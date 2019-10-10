9b9c686b75ba0a9b7218d88ac8e33c4ed3a99638
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-10-10 19:11:52 -0400

    change app module to api module

--- Deploy/terraform/environments/cms-ab2d-sbdemo/autoscaling-schedule.tf
+++ Deploy/terraform/environments/cms-ab2d-sbdemo/autoscaling-schedule.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_schedule" "night" {
   max_size               = 0
   desired_capacity       = 0
   recurrence             = "00 05 * * 1-5" #Mon-Fri at 12:00 AM Midnight EST
-  autoscaling_group_name = module.app.aws_autoscaling_group_name
+  autoscaling_group_name = module.api.aws_autoscaling_group_name
 }
 
 resource "aws_autoscaling_schedule" "morning" {
@@ -13,6 +13,6 @@ resource "aws_autoscaling_schedule" "morning" {
   max_size               = 2
   desired_capacity       = 2
   recurrence             = "00 12 * * 1-5" #Mon-Fri at 7AM EST
-  autoscaling_group_name = module.app.aws_autoscaling_group_name
+  autoscaling_group_name = module.api.aws_autoscaling_group_name
 }
 
