eaf935ecc8a38687759da4909fe69be27690e34d
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-10-15 01:27:05 -0400

    renamed

--- Deploy/terraform/environments/cms-ab2d-sbdemo/list-api-instances.sh
+++ Deploy/terraform/environments/cms-ab2d-sbdemo/list-api-instances.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script to list app instances in the environment
+# Script to list api instances in the environment
 
 export CMS_ENV="SBDEMO" #Examples: DEV, SBX, IMPL, PROD, SBDEMO
 
 echo "*******************************************"
-echo "App instances in $CMS_ENV environment"
+echo "API instances in $CMS_ENV environment"
 echo "*******************************************"
 
 aws --region us-east-1 ec2 describe-instances --output text \
