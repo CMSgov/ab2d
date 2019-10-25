d127f3ab3aeac35fbb3a2fffebd2f4795bed03ad
adaykin <daykinandy@gmail.com>
2019-10-25 14:21:51 -0500

    Removing configs/changing coverage

--- common/src/test/java/gov/cms/ab2d/common/SpringBootApp.java
+++ common/src/test/java/gov/cms/ab2d/common/SpringBootApp.java
@@ -2,12 +2,8 @@ package gov.cms.ab2d.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "gov.cms.ab2d.common")
-@EntityScan(basePackages = {"gov.cms.ab2d.common.model"})
-@EnableJpaRepositories(basePackages = "gov.cms.ab2d.common.repository")
+@SpringBootApplication
 public class SpringBootApp {
 
     public static void main(String [] args) {
