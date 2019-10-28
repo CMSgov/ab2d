467609a6506374ff0e25eb372c35a9394f2ce46c
Andy Daykin <daykinandy@gmail.com>
2019-10-28 08:52:23 -0500

    Feature/ab2d 270 refactor common classes (#24)
    Making several refactors to make code more reusable by moving service, repository, and util classes to module that will be shared by API and worker, and renaming to common. Moved code coverage checks to individual modules.

---
+++ common/src/test/java/gov/cms/ab2d/common/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package gov.cms.ab2d.common.service;
+
+import gov.cms.ab2d.common.SpringBootApp;
+import gov.cms.ab2d.common.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootApp.class)
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testUser() {
+        User user = userService.getCurrentUser();
+
+        assertEquals(null, user); // no authentication for now, so will be null
+    }
+}
