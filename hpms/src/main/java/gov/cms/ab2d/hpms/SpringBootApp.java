3ee1a9e95d018e188ec9748402829c7f30ca805f
adaykin <daykinandy@gmail.com>
2019-10-29 15:36:25 -0500

    Initial creation of HPMS module

---
+++ hpms/src/main/java/gov/cms/ab2d/hpms/SpringBootApp.java
@@ -0,0 +1,17 @@
+package gov.cms.ab2d.hpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.api"})
+@EntityScan(basePackages = {"gov.cms.ab2d.common.model"})
+@EnableJpaRepositories("gov.cms.ab2d.common.repository")
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+}
