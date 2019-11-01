9add11a041641461d384975244fb9fbead5a5576
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-01 18:14:30 -0400

    add spacing for sections

--- Deploy/python3/create-database-secret.py
+++ Deploy/python3/create-database-secret.py
@@ -4,6 +4,7 @@ import boto3
 import sys
 
 # Ensure command line arguments were passed
+
 command_line_arguments_count = sys.argv.__len__() - 1
 if command_line_arguments_count != 4:
     print("Try running the script like so:")
@@ -11,21 +12,26 @@ if command_line_arguments_count != 4:
     exit(1)
 
 # Eliminate double quotes from command line arguments
+
 environment = sys.argv[1].replace('"', '')
 secret_item = sys.argv[2].replace('"', '')
 kms_string = sys.argv[3].replace('"', '')
 date_time = sys.argv[4].replace('"', '')
 
 # Initialize the AWS Secrets Manager client
+
 client = boto3.client('secretsmanager')
 
 # Create sercret name
+
 secret_name = 'ab2d/' + environment + '/module/db/' + secret_item + '/' + date_time
 
 # Prompt for secret
+
 secret = input("Enter desired " + secret_item + ": ")
 
 # Create database user secret in AWS Secrets Manager
+
 response = client.create_secret(
     Name=secret_name,
     KmsKeyId=kms_string,
