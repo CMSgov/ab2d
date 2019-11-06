8d55511c60dab2121bca8143e8865429abe7d257
Denis Krylov <dkrylovsb@users.noreply.github.com>
2019-11-06 11:09:04 -0500

    Initial implementation of the BFD client (Credits: DPC).

---
+++ bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
@@ -0,0 +1,129 @@
+package gov.cms.ab2d.bfd.client;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.BeanInstantiationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+@Configuration
+@PropertySource("classpath:application.bfd.properties")
+/**
+ * Credits: most of the code in this class has been copied over from https://github
+ * .com/CMSgov/dpc-app
+ */
+public class BFDClientConfiguration {
+
+    private static final Log LOG = LogFactory.getLog(BFDClientConfiguration.class);
+
+    public static final String JKS = "JKS";
+
+    @Value("${bfd.keystore.location}")
+    private String keystorePath;
+
+    @Value("${bfd.keystore.password}")
+    private String keystorePassword;
+
+    @Value("${bfd.connectionTimeout}")
+    private int connectionTimeout;
+
+    @Value("${bfd.socketTimeout}")
+    private int socketTimeout;
+
+    @Value("${bfd.requestTimeout}")
+    private int requestTimeout;
+
+    @Value("${bfd.serverBaseUrl}")
+    private String serverBaseUrl;
+
+    @Bean
+    public IGenericClient bfdFhirRestClient(FhirContext fhirContext, HttpClient httpClient) {
+        fhirContext.getRestfulClientFactory().setHttpClient(httpClient);
+
+        return fhirContext.newRestfulGenericClient(serverBaseUrl);
+    }
+
+    @Bean
+    public FhirContext fhirContext() {
+        return FhirContext.forDstu3();
+    }
+
+    @Bean
+    public KeyStore bfdKeyStore() {
+        // Will need to support key stores that could be both in classpath AND in an external file
+        try (InputStream keyStoreStream = this.getClass().getResourceAsStream(keystorePath)) {
+            KeyStore keyStore = KeyStore.getInstance(JKS);
+            keyStore.load(keyStoreStream, keystorePassword.toCharArray());
+            return keyStore;
+        } catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException ex) {
+            LOG.error(ex.getMessage(), ex);
+            throw new BeanInstantiationException(KeyStore.class, ex.getMessage());
+        }
+    }
+
+    /**
+     * Borrowed from https://github.com/CMSgov/dpc-app
+     *
+     * @param keyStore
+     * @return
+     */
+    @Bean
+    public HttpClient bfdHttpClient(KeyStore keyStore) {
+        return buildMutualTlsClient(keyStore, keystorePassword.toCharArray());
+    }
+
+
+    /**
+     * Helper function to build a special {@link HttpClient} capable of authenticating with the
+     * Blue Button server using a client TLS certificate
+     * Borrowed from https://github.com/CMSgov/dpc-app.
+     *
+     * @param keyStore     {@link KeyStore} containing, at a minimum, the client tls certificate
+     *                     and private key
+     * @param keyStorePass password for keystore (default: "changeit")
+     * @return {@link HttpClient} compatible with HAPI FHIR TLS client
+     */
+    private HttpClient buildMutualTlsClient(KeyStore keyStore, char[] keyStorePass) {
+        final SSLContext sslContext;
+
+        try {
+            // BlueButton FHIR servers have a self-signed cert and require a client cert
+            sslContext = SSLContexts.custom()
+                    .loadKeyMaterial(keyStore, keyStorePass)
+                    .loadTrustMaterial(keyStore, new TrustSelfSignedStrategy())
+                    .build();
+
+        } catch (KeyManagementException | NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException ex) {
+            LOG.error(ex.getMessage());
+            throw new BeanInstantiationException(KeyStore.class, ex.getMessage());
+        }
+
+        // Configure the socket timeout for the connection, incl. ssl tunneling
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(connectionTimeout)
+                .setConnectionRequestTimeout(requestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setSSLContext(sslContext)
+                .build();
+    }
+
+}
