05b3a0f7d28b6b286a53260b17acb09b05161f62
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-07 23:45:42 -0500

    create target db

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e #Exit on first error
-set -x #Be verbose
+# set -x #Be verbose
 
 #
-# Configure
-#
-
 # Change to working directory
-cd "$(dirname "$0")"
+#
 
-# Configure environment setting
-export AWS_PROFILE="sbdemo"
-export CMS_ENV="SBDEMO"
+START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "${START_DIR}"
 
 #
 # Parse options
@@ -23,11 +19,19 @@ do
 case $i in
   --environment=*)
   ENVIRONMENT="${i#*=}"
-  CMS_ENV=$(echo $ENVIRONMENT | tr '[:lower:]' '[:upper:]')
+  CMS_ENV=$(echo $ENVIRONMENT | tr '[:upper:]' '[:lower:]')
   shift # past argument=value
   ;;
   --ami=*)
   AMI_ID="${i#*=}"
+  shift # past argument=value
+  ;;
+  --ssh-username=*)
+  SSH_USERNAME="${i#*=}"
+  shift # past argument=value
+  ;;
+  --database-secret-datetime=*)
+  DATABASE_SECRET_DATETIME=$(echo ${i#*=})
   shift # past argument=value
   ;;
   --auto-approve)
@@ -42,11 +46,31 @@ done
 #
 
 echo "Check vars are not empty before proceeding..."
-if [ -z "${ENVIRONMENT}" ]; then
+if [ -z "${ENVIRONMENT}" ] || [ -z "${DATABASE_SECRET_DATETIME}" ] || [ -z "${SSH_USERNAME}" ]; then
   echo "Try running the script like so:"
-  echo "./deploy.sh --environment=sbdemo"
+  echo "./deploy.sh --environment=dev --database-secret-datetime={YYYY-MM-DD-HH-MM-SS}"
   exit 1
 fi
+
+#
+# Set environment
+#
+
+export AWS_PROFILE="${CMS_ENV}"
+
+#
+# Get secrets
+#
+
+# Change to the "python3" directory
+
+cd "${START_DIR}"
+cd python3
+
+# Get database secrets
+
+DATABASE_USER=$(./get-database-secret.py $CMS_ENV database_user $DATABASE_SECRET_DATETIME)
+DATABASE_NAME=$(./get-database-secret.py $CMS_ENV database_name $DATABASE_SECRET_DATETIME)
 
 #
 # Get current known good ECS task definitions
@@ -90,6 +114,8 @@ fi
 #
 
 echo "Switch context to terraform environment..."
+
+cd "${START_DIR}"
 cd terraform/environments/ab2d-$ENVIRONMENT
 
 #
@@ -123,13 +149,72 @@ fi
 
 echo "Deploy new AMI out to AWS..."
 if [ -z "${AUTOAPPROVE}" ]; then
+    
   # Confirm with the caller prior to applying changes.
-  terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.api
-  terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.worker
+    
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_username=${DATABASE_USER}" \
+    --var "db_name=${DATABASE_NAME}" \
+    --target module.api
+
+  #
+  # TEMPORARILY COMMENTED OUT BEGIN
+  #
+  
+  # terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.worker
+
+  #
+  # TEMPORARILY COMMENTED OUT END
+  #
+
 else
+    
   # Apply the changes without prompting
-  terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.api --auto-approve
-  terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.worker --auto-approve
+    
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_username=${DATABASE_USER}" \
+    --var "db_name=${DATABASE_NAME}" \
+    --target module.api \
+    --auto-approve
+
+  #
+  # TEMPORARILY COMMENTED OUT BEGIN
+  #
+
+  # terraform apply --var "ami_id=$AMI_ID" --var "current_task_definition_arn=$API_TASK_DEFINITION" --target module.worker --auto-approve
+
+  #
+  # TEMPORARILY COMMENTED OUT END
+  #
+
+fi
+
+#
+# Create database
+#
+
+CONTROLLER_PUBLIC_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=ab2d-${CMS_ENV}-deployment-controller" --query="Reservations[*].Instances[?State.Name == 'running'].PublicIpAddress" --output text)
+
+DB_ENDPOINT=$(aws --region us-east-1 rds describe-db-instances --query="DBInstances[?DBInstanceIdentifier=='ab2d'].Endpoint.Address" --output=text)
+
+DB_NAME_IF_EXISTS=$(ssh -tt -i "~/.ssh/ab2d-${CMS_ENV}.pem" \
+  "${SSH_USERNAME}@${CONTROLLER_PUBLIC_IP}" \
+  "psql -t --host "${DB_ENDPOINT}" --username "${DATABASE_USER}" --dbname postgres --command='SELECT datname FROM pg_catalog.pg_database'" \
+  | grep "${DATABASE_NAME}" \
+  | sort \
+  | head -n 1 \
+  | xargs \
+  | tr -d '\r')
+
+if [ -n "${CONTROLLER_PUBLIC_IP}" ] && [ -n "${DB_ENDPOINT}" ] && [ "${DB_NAME_IF_EXISTS}" != "${DATABASE_NAME}" ]; then
+  echo "Creating database..."
+  ssh -tt -i "~/.ssh/ab2d-${CMS_ENV}.pem" \
+    "${SSH_USERNAME}@${CONTROLLER_PUBLIC_IP}" \
+    "createdb ${DATABASE_NAME} --host ${DB_ENDPOINT} --username ${DATABASE_USER}"
 fi
 
 #
@@ -157,23 +242,31 @@ else
 fi
 
 #
-# Ensure new autoscaling group is running containers
+# TEMPORARILY COMMENTED OUT BEGIN
 #
 
-echo "Ensure new autoscaling group is running containers..."
+# #
+# # Ensure new autoscaling group is running containers
+# #
 
-ACTUAL_API_COUNT=0
-RETRIES_API=0
+# echo "Ensure new autoscaling group is running containers..."
 
-while [ "$ACTUAL_API_COUNT" -lt "$EXPECTED_API_COUNT" ]; do
-  ACTUAL_API_COUNT=$(api_task_count)
-  echo "Running API Tasks: $ACTUAL_API_COUNT, Expected: $EXPECTED_API_COUNT"
-  if [ "$RETRIES_API" != "15" ]; then
-    echo "Retry in 60 seconds..."
-    sleep 60
-    RETRIES_API=$(expr $RETRIES_API + 1)
-  else
-    echo "Max retries reached. Exiting..."
-    exit 1
-  fi
-done
+# ACTUAL_API_COUNT=0
+# RETRIES_API=0
+
+# while [ "$ACTUAL_API_COUNT" -lt "$EXPECTED_API_COUNT" ]; do
+#   ACTUAL_API_COUNT=$(api_task_count)
+#   echo "Running API Tasks: $ACTUAL_API_COUNT, Expected: $EXPECTED_API_COUNT"
+#   if [ "$RETRIES_API" != "15" ]; then
+#     echo "Retry in 60 seconds..."
+#     sleep 60
+#     RETRIES_API=$(expr $RETRIES_API + 1)
+#   else
+#     echo "Max retries reached. Exiting..."
+#     exit 1
+#   fi
+# done
+
+#
+# TEMPORARILY COMMENTED OUT END
+#
