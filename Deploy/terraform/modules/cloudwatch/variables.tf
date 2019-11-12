e274f0d12f9692dfc4759686943b6fd4db8b074f
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-11 21:22:05 -0500

    cloudwatch module added

---
+++ Deploy/terraform/modules/cloudwatch/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "autoscaling_arn" {}
+# variable "sns_arn" {}
+variable "autoscaling_name" {}
+variable "controller_server_id" {}
+variable "s3_bucket_name" {}
+variable "db_name" {}
+variable "target_group_arn_suffix" {}
+variable "loadbalancer_arn_suffix" {}
