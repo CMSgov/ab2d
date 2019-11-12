2d31285f6585e6c08142793605e34c4d4bf802f2
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-12 01:53:35 -0500

    reenable script

--- Deploy/terraform/modules/api/outputs.tf
+++ Deploy/terraform/modules/api/outputs.tf
@@ -1,43 +1,35 @@
-#
-# TEMPORARILY COMMENTED OUT BEGIN
-#
+output "aws_autoscaling_group_name" {
+  value = aws_autoscaling_group.asg.name
+}
 
-# output "aws_autoscaling_group_name" {
-#   value = aws_autoscaling_group.asg.name
-# }
+output "application_security_group_id" {
+  value = aws_security_group.api.id
+}
 
-# output "application_security_group_id" {
-#   value = aws_security_group.api.id
-# }
+output "aws_autoscaling_policy_percent_capacity_arn" {
+  value = aws_autoscaling_policy.percent_capacity.arn
+}
 
-# output "aws_autoscaling_policy_percent_capacity_arn" {
-#   value = aws_autoscaling_policy.percent_capacity.arn
-# }
+output "aws_elastic_load_balancer_name" {
+  value = aws_lb.api.name
+}
 
-# output "aws_elastic_load_balancer_name" {
-#   value = aws_lb.api.name
-# }
+output "alb_target_group_arn_suffix" {
+  value = aws_lb_target_group.api.arn_suffix
+}
 
-# output "alb_target_group_arn_suffix" {
-#   value = aws_lb_target_group.api.arn_suffix
-# }
+output "alb_arn_suffix" {
+  value = aws_lb.api.arn_suffix
+}
 
-# output "alb_arn_suffix" {
-#   value = aws_lb.api.arn_suffix
-# }
+output "userdata" {
+  value = aws_launch_configuration.launch_config.user_data
+}
 
-# output "userdata" {
-#   value = aws_launch_configuration.launch_config.user_data
-# }
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.ab2d.id
+}
 
-# output "ecs_cluster_id" {
-#   value = aws_ecs_cluster.ab2d.id
-# }
-
-# output "launch_config_name" {
-#   value = aws_launch_configuration.launch_config.name
-# }
-
-#
-# TEMPORARILY COMMENTED OUT END
-#
+output "launch_config_name" {
+  value = aws_launch_configuration.launch_config.name
+}
