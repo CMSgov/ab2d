682f0508c87fd14c3a22051b055f50e15fe204da
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-12 01:54:08 -0500

    reenable script

--- Deploy/terraform/modules/api/variables.tf
+++ Deploy/terraform/modules/api/variables.tf
@@ -1,48 +1,41 @@
-#
-# TEMPORARILY COMMENTED OUT BEGIN
-#
+variable "env" {}
+variable "aws_account_number" {}
+variable "vpc_id" {}
+variable "db_sec_group_id" {}
+variable "controller_sec_group_id" {}
+variable "controller_subnet_ids" {type=list(string)}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "linux_user" {}
+variable "ssh_key_name" {}
+variable "node_subnet_ids" {type=list(string)}
+variable "logging_bucket" {}
+variable "healthcheck_url" {}
+variable "iam_instance_profile" {}
+variable "docker_repository_url" {}
+variable "iam_role_arn" {}
 
-# variable "env" {}
-# variable "aws_account_number" {}
-# variable "vpc_id" {}
-# variable "db_sec_group_id" {}
-# variable "controller_subnet_ids" {type=list(string)}
-# variable "ami_id" {}
-# variable "instance_type" {}
-# variable "linux_user" {}
-# variable "ssh_key_name" {}
-# variable "node_subnet_ids" {type=list(string)}
-# variable "logging_bucket" {}
-# variable "healthcheck_url" {}
-# variable "iam_instance_profile" {}
-# variable "docker_repository_url" {}
-# variable "iam_role_arn" {}
+# LSH BEGIN
+# variable "container_port" {default=3000}
+variable "container_port" {default=8080}
+# LSH END
 
-# # LSH BEGIN
-# # variable "container_port" {default=3000}
-# variable "container_port" {default=8080}
-# # LSH END
+variable "host_port" {default=80}
+variable "desired_instances" {}
+variable "min_instances" {}
+variable "max_instances" {}
+variable "autoscale_group_wait" {}
+variable "gold_disk_name" {}
+variable "override_task_definition_arn" {default=""}
 
-# variable "host_port" {default=80}
-# variable "desired_instances" {}
-# variable "min_instances" {}
-# variable "max_instances" {}
-# variable "autoscale_group_wait" {}
-# variable "gold_disk_name" {}
-# variable "override_task_definition_arn" {default=""}
+# LSH SKIP FOR NOW BEGIN
+# variable "enterprise-tools-sec-group-id" {}
+# variable "vpn-private-sec-group-id" {}
+# LSH SKIP FOR NOW END
 
-# # LSH SKIP FOR NOW BEGIN
-# # variable "enterprise-tools-sec-group-id" {}
-# # variable "vpn-private-sec-group-id" {}
-# # LSH SKIP FOR NOW END
+variable "percent_capacity_increase" {default="20"}
 
-# variable "percent_capacity_increase" {default="20"}
-
-# variable "db_host" {}
-# variable "db_name" {}
-# variable "db_username" {}
-# variable "db_password" {}
-
-#
-# TEMPORARILY COMMENTED OUT END
-#
+variable "db_host" {}
+variable "db_name" {}
+variable "db_username" {}
+variable "db_password" {}
