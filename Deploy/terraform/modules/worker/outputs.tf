53383a26e66c4b10afa202b601c92a001c090401
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-11-12 01:55:23 -0500

    reenable script

--- Deploy/terraform/modules/worker/outputs.tf
+++ Deploy/terraform/modules/worker/outputs.tf
@@ -1,15 +1,7 @@
-#
-# TEMPORARILY COMMENTED OUT BEGIN
-#
+output "aws_autoscaling_group_name" {
+  value = aws_autoscaling_group.asg.name
+}
 
-# output "aws_autoscaling_group_name" {
-#   value = aws_autoscaling_group.asg.name
-# }
-
-# output "aws_autoscaling_policy_percent_capacity_arn" {
-#   value = aws_autoscaling_policy.percent_capacity.arn
-# }
-
-#
-# TEMPORARILY COMMENTED OUT END
-#
+output "aws_autoscaling_policy_percent_capacity_arn" {
+  value = aws_autoscaling_policy.percent_capacity.arn
+}
