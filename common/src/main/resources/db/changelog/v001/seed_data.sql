409c1e4d980e4bb2d975076cb58126e6caa28657
adaykin <daykinandy@gmail.com>
2019-11-22 15:54:28 -0600

    Updating columns to be correct

--- common/src/main/resources/db/changelog/v001/seed_data.sql
+++ common/src/main/resources/db/changelog/v001/seed_data.sql
@@ -9,10 +9,10 @@ INSERT INTO sponsor (id, hpms_id, org_name, legal_name, parent_id) VALUES ((sele
 INSERT INTO sponsor (id, hpms_id, org_name, legal_name, parent_id) VALUES ((select nextval('hibernate_sequence')), 999, 'Kutch LLC', null,
                                                                            (select id from sponsor where hpms_id=1));
 
-INSERT INTO user_account (id, user_name, first_name, last_name, email, sponsor_id, enabled) VALUES ((select nextval('hibernate_sequence')),
+INSERT INTO user_account (id, username, first_name, last_name, email, sponsor_id, enabled) VALUES ((select nextval('hibernate_sequence')),
     'EileenCFrierson@example.com', 'Eileen', 'Frierson', 'EileenCFrierson@example.com', (select id from sponsor where hpms_id=999), true);
 
-INSERT INTO user_role (user_account_id, role_id) VALUES ((SELECT id FROM user_account WHERE user_name='EileenCFrierson@example.com'),
+INSERT INTO user_role (user_account_id, role_id) VALUES ((SELECT id FROM user_account WHERE username='EileenCFrierson@example.com'),
                                                          (SELECT id FROM role WHERE name='SPONSOR'));
 
 INSERT INTO contract (id, contract_number, contract_name, sponsor_id, attested_on) VALUES ((select nextval('hibernate_sequence')),'S0001', 'S0001',
