f5a3a26c8d6274d49f74a6ece0e5dbdba9928b33
spathiyil <satheesh.pathiyil@semanticbits.com>
2019-11-27 09:56:50 -0500

    create new module -optout- with quartz dependency

---
+++ optout/src/main/java/gov/cms/ab2d/optout/SpringBootApp.java
@@ -0,0 +1,18 @@
+package gov.cms.ab2d.optout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.optout"})
+@EntityScan(basePackages = {"gov.cms.ab2d.common.model"})
+@EnableJpaRepositories("gov.cms.ab2d.common.repository")
+@PropertySource("classpath:application.common.properties")
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
