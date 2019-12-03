d3bf9d093bfe3d0d0c33b10694fc7ffc10267058
adaykin <daykinandy@gmail.com>
2019-12-03 14:59:27 -0600

    Adding in a configurable cron job that will run by default every hour. The job will look for files older than 24 hours by default to delete to avoid taking up too much space.

---
+++ audit/src/test/java/gov/cms/ab2d/audit/job/FileDeletionJobTest.java
@@ -0,0 +1,102 @@
+package gov.cms.ab2d.audit.job;
+
+import gov.cms.ab2d.audit.SpringBootApp;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.quartz.*;
+import org.quartz.impl.JobExecutionContextImpl;
+import org.quartz.spi.OperableTrigger;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import static gov.cms.ab2d.audit.job.FileDeletionJob.AUDIT_FILES_TTL_HOURS;
+import static gov.cms.ab2d.audit.job.FileDeletionJob.EFS_MOUNT;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootApp.class)
+@TestPropertySource(locations = "/application.common.properties")
+public class FileDeletionJobTest {
+
+    @Value("${efs.mount}")
+    private String efsMount;
+
+    private static final String TEST_FILE = "testFile.txt";
+
+    private static final String TEST_FILE_NOT_DELETED = "testFileNotDeleted.txt";
+
+    private static final String TEST_DIRECTORY = "testDirectory";
+
+    private static final String TEST_FILE_NESTED = TEST_DIRECTORY + "/testFileInDirectory.txt";
+
+    // Change the creation time so that the file will be eligible for deletion
+    private void changeFileCreationDate(Path path) throws IOException {
+        BasicFileAttributeView attributes = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime time = FileTime.fromMillis(LocalDate.now().minus(2, ChronoUnit.DAYS).toEpochDay());
+        attributes.setTimes(time, time, time);
+    }
+
+    @Test
+    public void checkToEnsureFilesDeleted() throws IOException, URISyntaxException {
+        // Don't change the creation date on this file, but do so on the next two
+        Path destinationNotDeleted = Paths.get(efsMount, TEST_FILE_NOT_DELETED);
+        URL urlNotDeletedFile = this.getClass().getResource("/" + TEST_FILE_NOT_DELETED);
+        Path sourceNotDeleted = Paths.get(urlNotDeletedFile.toURI());
+        Files.move(sourceNotDeleted, destinationNotDeleted, StandardCopyOption.REPLACE_EXISTING);
+
+        Path destination = Paths.get(efsMount, TEST_FILE);
+        URL url = this.getClass().getResource("/" + TEST_FILE);
+        Path source = Paths.get(url.toURI());
+        Files.move(source, destination, StandardCopyOption.REPLACE_EXISTING);
+
+        changeFileCreationDate(destination);
+
+        File dir = new File(efsMount + TEST_DIRECTORY);
+        if (!dir.exists()) dir.mkdirs();
+
+        Path nestedFileDestination = Paths.get(efsMount, TEST_FILE_NESTED);
+        URL nestedFileUrl = this.getClass().getResource("/" + TEST_FILE_NESTED);
+        Path nestedFileSource = Paths.get(nestedFileUrl.toURI());
+        Files.move(nestedFileSource, nestedFileDestination, StandardCopyOption.REPLACE_EXISTING);
+
+        changeFileCreationDate(nestedFileDestination);
+
+        FileDeletionJob fileDeletionJob = new FileDeletionJob();
+        Scheduler scheduler = Mockito.mock(Scheduler.class);
+        TriggerFiredBundle triggerFiredBundle = Mockito.mock(TriggerFiredBundle.class);
+
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(EFS_MOUNT, efsMount);
+        jobDataMap.put(AUDIT_FILES_TTL_HOURS, 24);
+        OperableTrigger operableTrigger = Mockito.mock(OperableTrigger.class);
+        when(operableTrigger.getJobDataMap()).thenReturn(jobDataMap);
+        when(triggerFiredBundle.getTrigger()).thenReturn(operableTrigger);
+        JobDetail jobDetail = Mockito.mock(JobDetail.class);
+        when(jobDetail.getJobDataMap()).thenReturn(jobDataMap);
+        when(triggerFiredBundle.getJobDetail()).thenReturn(jobDetail);
+        JobExecutionContext jobExecutionContext = new JobExecutionContextImpl(scheduler, triggerFiredBundle, fileDeletionJob);
+
+        fileDeletionJob.execute(jobExecutionContext);
+
+        Assert.assertTrue(Files.notExists(destination));
+
+        Assert.assertTrue(Files.notExists(nestedFileDestination));
+
+        Assert.assertTrue(Files.exists(destinationNotDeleted));
+    }
+}
--- worker/src/main/java/gov/cms/ab2d/worker/config/AuditJobSetup.java
+++ worker/src/main/java/gov/cms/ab2d/worker/config/AuditJobSetup.java
@@ -1,39 +1,57 @@
 package gov.cms.ab2d.worker.config;
 
-import gov.cms.ab2d.audit.FileDeletionJob;
+import gov.cms.ab2d.audit.job.FileDeletionJob;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 @Slf4j
+@Component
 public class AuditJobSetup {
 
-    public void setupAuditJob() {
+    @Value("${audit-files-cron}")
+    private String auditFilesCron;
+
+    @Value("${efs.mount}")
+    private String efsMount;
+
+    @Value("${audit-files-ttl-hours}")
+    private int auditFilesTTLHours;
+
+    public static final String JOB_NAME = "DeleteAuditFilesJob";
+
+    public static final String CRON_GROUP = "FileDeletion";
+
+    public static final String TRIGGER_NAME = "DeleteAuditFilesTrigger";
+
+    @PostConstruct
+    public void setupAuditJob() throws SchedulerException {
         SchedulerFactory sf = new StdSchedulerFactory();
-        Scheduler sched = null;
-        try {
-            sched = sf.getScheduler();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
+        Scheduler scheduler = sf.getScheduler();
 
         JobDetail job = newJob(FileDeletionJob.class)
-                .withIdentity("job1", "group1")
+                .withIdentity(JOB_NAME, CRON_GROUP)
+                .storeDurably()
                 .build();
+        job.getJobDataMap().put(FileDeletionJob.EFS_MOUNT, efsMount);
+        job.getJobDataMap().put(FileDeletionJob.AUDIT_FILES_TTL_HOURS, auditFilesTTLHours);
 
         CronTrigger trigger = newTrigger()
-                .withIdentity("trigger1", "group1")
-                .withSchedule(cronSchedule("0/20 * * * * ?"))
+                .withIdentity(TRIGGER_NAME, CRON_GROUP)
+                .withSchedule(cronSchedule(auditFilesCron))
                 .build();
 
-        try {
-            sched.scheduleJob(job, trigger);
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
+        scheduler.scheduleJob(job, trigger);
+        scheduler.start();
+
+        log.info("Scheduler started with a cron of {}", auditFilesCron);
     }
 }
