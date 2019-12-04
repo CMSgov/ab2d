0c1cab779c02a69567df9caacb11c75cef63f4b8
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-12-03 19:55:48 -0500

    temporarily hardcode my public ip when connected to vpn; need to make the programmatical

--- Deploy/terraform/modules/controller/main.tf
+++ Deploy/terraform/modules/controller/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group_rule" "whitelist_lonnie" {
   from_port   = "22"
   to_port     = "22"
   protocol    = "TCP"
-  cidr_blocks = ["152.208.13.223/32"]
+  cidr_blocks = ["34.196.35.156/32"]
   security_group_id = aws_security_group.deployment_controller.id
 }
 
