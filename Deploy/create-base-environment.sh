d0e4405f6ef4129fc875dfbd6f7d6617d940bbe4
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-12-09 23:08:26 -0500

    merge deploy.sh into here and other changes related to passing env vars for docker container

--- Deploy/create-base-environment.sh
+++ Deploy/create-base-environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e #Exit on first error
-# set -x #Be verbose
+set -x #Be verbose
 
 #
 # Change to working directory
@@ -63,6 +63,10 @@ case $i in
   ;;
   --use-existing-images)
   USE_EXISTING_IMAGES="true"
+  shift # past argument=value
+  ;;
+  --auto-approve)
+  AUTOAPPROVE="true"
   shift # past argument=value
   ;;
 esac
@@ -163,8 +167,6 @@ echo "**************************************************************"
 echo "Initialize and validate terraform for the shared components..."
 echo "**************************************************************"
 
-export AWS_PROFILE="${CMS_SHARED_ENV}"
-
 cd "${START_DIR}"
 cd terraform/environments/ab2d-$CMS_SHARED_ENV
 
@@ -176,8 +178,6 @@ terraform validate
 echo "***************************************************************"
 echo "Initialize and validate terraform for the target environment..."
 echo "***************************************************************"
-
-export AWS_PROFILE="${CMS_ENV}"
 
 cd "${START_DIR}"
 cd terraform/environments/ab2d-$CMS_ENV
@@ -198,7 +198,6 @@ if [ -n "$KMS_KEY_ID" ]; then
   ./enable-kms-key.py $KMS_KEY_ID
 else
   echo "Deploying KMS..."
-  export AWS_PROFILE="${CMS_SHARED_ENV}"
   cd "${START_DIR}"
   cd terraform/environments/ab2d-$CMS_SHARED_ENV
   terraform apply \
@@ -946,8 +945,6 @@ fi
 # AMI Generation for Jenkins node
 #
 
-export AWS_PROFILE="${CMS_ENV}"
-
 # Set JENKINS_AMI_ID if it already exists for the deployment
 
 echo "Set JENKINS_AMI_ID if it already exists for the deployment..."
@@ -1000,6 +997,10 @@ if [ -z "${JENKINS_AMI_ID}" ]; then
     --tags "Key=Name,Value=ab2d-jenkins-ami"
 fi
 
+# Get deployer IP address
+
+DEPLOYER_IP_ADDRESS=$(curl ipinfo.io/ip)
+
 #
 # Create "auto.tfvars" files
 #
@@ -1009,7 +1010,9 @@ fi
 cd "${START_DIR}"
 cd terraform/environments/ab2d-$CMS_SHARED_ENV
 
-DB_ENDPOINT=$(aws --region us-east-1 rds describe-db-instances --query="DBInstances[?DBInstanceIdentifier=='ab2d'].Endpoint.Address" --output=text)
+DB_ENDPOINT=$(aws --region us-east-1 rds describe-db-instances \
+  --query="DBInstances[?DBInstanceIdentifier=='ab2d'].Endpoint.Address" \
+  --output=text)
 
 if [ -z "${DB_ENDPOINT}" ]; then
   echo 'vpc_id = "'$VPC_ID'"' \
@@ -1023,6 +1026,8 @@ if [ -z "${DB_ENDPOINT}" ]; then
   echo 'linux_user = "'$SSH_USERNAME'"' \
     >> $CMS_SHARED_ENV.auto.tfvars
   echo 'ami_id = "'$AMI_ID'"' \
+    >> $CMS_SHARED_ENV.auto.tfvars
+  echo 'deployer_ip_address = "'$DEPLOYER_IP_ADDRESS'"' \
     >> $CMS_SHARED_ENV.auto.tfvars
 else
   PRIVATE_SUBNETS_OUTPUT=$(aws ec2 describe-subnets \
@@ -1056,6 +1061,8 @@ else
     >> $CMS_SHARED_ENV.auto.tfvars
   echo 'ami_id = "'$AMI_ID'"' \
     >> $CMS_SHARED_ENV.auto.tfvars
+  echo 'deployer_ip_address = "'$DEPLOYER_IP_ADDRESS'"' \
+    >> $CMS_SHARED_ENV.auto.tfvars
 fi
 
 # Create ".auto.tfvars" file for the target environment
@@ -1073,6 +1080,8 @@ echo 'ec2_instance_type = "'$EC2_INSTANCE_TYPE'"' \
   >> $CMS_ENV.auto.tfvars
 echo 'linux_user = "'$SSH_USERNAME'"' \
   >> $CMS_ENV.auto.tfvars
+echo 'deployer_ip_address = "'$DEPLOYER_IP_ADDRESS'"' \
+  >> $CMS_ENV.auto.tfvars
 
 ##########################
 # Deploy shared components
@@ -1081,8 +1090,6 @@ echo 'linux_user = "'$SSH_USERNAME'"' \
 #
 # Configure shared environment
 #
-
-export AWS_PROFILE="${CMS_SHARED_ENV}"
 
 cd "${START_DIR}"
 cd terraform/environments/ab2d-$CMS_SHARED_ENV
@@ -1118,21 +1125,6 @@ cd terraform/environments/ab2d-$CMS_SHARED_ENV
 aws s3api put-bucket-policy \
   --bucket ab2d-cloudtrail \
   --policy file://ab2d-cloudtrail-bucket-policy.json
-
-#
-# Create "sbdemo-ab2d-website" bucket
-#
-
-echo "Creating "sbdemo-ab2d-website" bucket..."
-
-aws --region us-east-1 s3api create-bucket \
-  --bucket sbdemo-ab2d-website
-
-# Block public access on bucket
-
-aws --region us-east-1 s3api put-public-access-block \
-  --bucket sbdemo-ab2d-website \
-  --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
 
 #
 # Create dev S3 bucket
@@ -1214,8 +1206,6 @@ echo "${DB_ENDPOINT}:5432:postgres:${DATABASE_USER}:${DATABASE_PASSWORD}" > gene
 
 echo "Create or update controller..."
 
-DEPLOYER_IP_ADDRESS=$(curl ipinfo.io/ip)
-
 terraform apply \
   --var "db_username=${DATABASE_USER}" \
   --var "db_password=${DATABASE_PASSWORD}" \
@@ -1227,8 +1217,6 @@ terraform apply \
 ######################################
 # Deploy target environment components
 ######################################
-
-export AWS_PROFILE="${CMS_ENV}"
 
 cd "${START_DIR}"
 cd terraform/environments/ab2d-$CMS_ENV
@@ -1250,37 +1238,468 @@ if [ -z "${EFS_FS_ID}" ]; then
 fi
 
 #
-# Temporarily stop here
+# Create database
 #
 
-exit 0
+cd "${START_DIR}"
+
+# Get the public ip address of the controller
+
+CONTROLLER_PUBLIC_IP=$(aws --region us-east-1 ec2 describe-instances \
+  --filters "Name=tag:Name,Values=ab2d-deployment-controller" \
+  --query="Reservations[*].Instances[?State.Name == 'running'].PublicIpAddress" \
+  --output text)
+
+# Determine if the database for the environment exists
+
+DB_NAME_IF_EXISTS=$(ssh -tt -i "~/.ssh/ab2d-${CMS_SHARED_ENV}.pem" \
+  "${SSH_USERNAME}@${CONTROLLER_PUBLIC_IP}" \
+  "psql -t --host "${DB_ENDPOINT}" --username "${DATABASE_USER}" --dbname postgres --command='SELECT datname FROM pg_catalog.pg_database'" \
+  | grep "${DATABASE_NAME}" \
+  | sort \
+  | head -n 1 \
+  | xargs \
+  | tr -d '\r')
+
+# Create the database for the environment if it doesn't exist
+
+if [ -n "${CONTROLLER_PUBLIC_IP}" ] && [ -n "${DB_ENDPOINT}" ] && [ "${DB_NAME_IF_EXISTS}" != "${DATABASE_NAME}" ]; then
+  echo "Creating database..."
+  ssh -tt -i "~/.ssh/ab2d-${CMS_SHARED_ENV}.pem" \
+    "${SSH_USERNAME}@${CONTROLLER_PUBLIC_IP}" \
+    "createdb ${DATABASE_NAME} --host ${DB_ENDPOINT} --username ${DATABASE_USER}"
+fi
 
 #
 # Deploy AWS application modules
 #
 
-cd "${START_DIR}"
-
 if [ -n "${BUILD_NEW_IMAGES}" ]; then
-    
-  ./deploy.sh \
-    --environment="${CMS_ENV}" \
-    --shared-environment="${CMS_SHARED_ENV}" \
-    --ami="${AMI_ID}" \
-    --ssh-username="${SSH_USERNAME}" \
-    --database-secret-datetime="${DATABASE_SECRET_DATETIME}" \
-    --build-new-images \
-    --auto-approve
 
+  # Build and push API and worker to ECR
+
+  echo "Build and push API and worker to ECR..."
+  
+  # Log on to ECR
+      
+  read -sra cmd < <(aws ecr get-login --no-include-email)
+  pass="${cmd[5]}"
+  unset cmd[4] cmd[5]
+  "${cmd[@]}" --password-stdin <<< "$pass"
+
+  # Build API and worker (if creating a new image)
+
+  cd "${START_DIR}"
+  cd ..
+  make docker-build
+  sleep 5
+
+  # Build API docker image
+
+  cd "${START_DIR}"
+  cd ../api
+  docker build \
+    --tag "ab2d_api:latest" .
+
+  # Build worker docker image
+
+  cd "${START_DIR}"
+  cd ../worker
+  docker build \
+    --tag "ab2d_worker:latest" .
+
+  # Tag and push API docker image to ECR
+
+  API_ECR_REPO_URI=$(aws --region us-east-1 ecr describe-repositories \
+    --query "repositories[?repositoryName == 'ab2d_api'].repositoryUri" \
+    --output text)
+  if [ -z "${API_ECR_REPO_URI}" ]; then
+    aws --region us-east-1 ecr create-repository \
+        --repository-name "ab2d_api"
+    API_ECR_REPO_URI=$(aws --region us-east-1 ecr describe-repositories \
+      --query "repositories[?repositoryName == 'ab2d_api'].repositoryUri" \
+      --output text)
+  fi
+  docker tag "ab2d_api:latest" "${API_ECR_REPO_URI}:latest"
+  docker push "${API_ECR_REPO_URI}:latest"
+
+  # Tag and push worker docker image to ECR
+
+  WORKER_ECR_REPO_URI=$(aws --region us-east-1 ecr describe-repositories \
+    --query "repositories[?repositoryName == 'ab2d_worker'].repositoryUri" \
+    --output text)
+  if [ -z "${WORKER_ECR_REPO_URI}" ]; then
+    aws --region us-east-1 ecr create-repository \
+      --repository-name "ab2d_worker"
+    WORKER_ECR_REPO_URI=$(aws --region us-east-1 ecr describe-repositories \
+      --query "repositories[?repositoryName == 'ab2d_worker'].repositoryUri" \
+      --output text)
+  fi
+  docker tag "ab2d_worker:latest" "${WORKER_ECR_REPO_URI}:latest"
+  docker push "${WORKER_ECR_REPO_URI}:latest"
+   
 else # use existing images
 
-  ./deploy.sh \
-    --environment="${CMS_ENV}" \
-    --shared-environment="${CMS_SHARED_ENV}" \
-    --ami="${AMI_ID}" \
-    --ssh-username="${SSH_USERNAME}" \
-    --database-secret-datetime="${DATABASE_SECRET_DATETIME}" \
-    --use-existing-images \
+  echo "Using existing images..."
+    
+fi
+
+#
+# Switch context to terraform environment
+#
+
+echo "Switch context to terraform environment..."
+
+cd "${START_DIR}"
+cd terraform/environments/ab2d-$CMS_ENV
+
+#
+# Get current known good ECS task definitions
+#
+
+echo "Get current known good ECS task definitions..."
+CLUSTER_ARNS=$(aws --region us-east-1 ecs list-clusters \
+  --query 'clusterArns' \
+  --output text \
+  | grep "/ab2d-${CMS_ENV}-api" \
+  | xargs \
+  | tr -d '\r')
+if [ -z "${CLUSTER_ARNS}" ]; then
+  echo "Skipping getting current ECS task definitions, since there are no existing clusters"
+else
+  echo "TEST"
+  API_TASK_DEFINITION=$(aws --region us-east-1 ecs describe-services \
+    --services "ab2d-${CMS_ENV}-api" \
+    --cluster "ab2d-${CMS_ENV}-api" \
+    | grep "taskDefinition" \
+    | head -1)
+  API_TASK_DEFINITION=$(echo $API_TASK_DEFINITION | awk -F'": "' '{print $2}' | tr -d '"' | tr -d ',')
+fi
+
+#
+# Get ECS task counts before making any changes
+#
+
+echo "Get ECS task counts before making any changes..."
+
+# Define api_task_count
+api_task_count() { aws --region us-east-1 ecs list-tasks --cluster "ab2d-${CMS_ENV}-api" | grep "\:task\/"|wc -l|tr -d ' '; }
+
+# Get old api task count (if exists)
+if [ -z "${CLUSTER_ARNS}" ]; then
+  echo "Skipping setting OLD_API_TASK_COUNT, since there are no existing clusters"
+else
+  OLD_API_TASK_COUNT=$(api_task_count)
+fi
+
+# set expected api task count
+
+# LSH BEGIN 2019-11-20
+# if [ -z "${CLUSTER_ARNS}" ]; then
+#   EXPECTED_API_COUNT="2"
+# else
+#   EXPECTED_API_COUNT="$((OLD_API_TASK_COUNT*2))"
+# fi
+EXPECTED_API_COUNT="2"
+# LSH END 2019-11-20
+
+#
+# Ensure Old Autoscaling Groups and containers are around to service requests
+#
+
+echo "Ensure Old Autoscaling Groups and containers are around to service requests..."
+
+if [ -z "${CLUSTER_ARNS}" ]; then
+  echo "Skipping setting OLD_API_ASG, since there are no existing clusters"
+else
+  OLD_API_ASG=$(terraform show|grep :autoScalingGroup:|awk -F" = " '{print $2}' | grep ab2d-$CMS_ENV)
+fi
+
+if [ -z "${CLUSTER_ARNS}" ]; then
+  echo "Skipping removing autosclaing group and launch configuration, since there are no existing clusters"
+else
+  terraform state rm module.app.aws_autoscaling_group.asg
+  terraform state rm module.app.aws_launch_configuration.launch_config
+fi
+
+if [ -z "${CLUSTER_ARNS}" ]; then
+  echo "Skipping removing autosclaing group and launch configuration, since there are no existing clusters"
+else
+  OLD_API_CONTAINER_INSTANCES=$(aws --region us-east-1 ecs list-container-instances \
+    --cluster "ab2d-${CMS_ENV}-api" \
+    | grep container-instance)
+fi
+
+#
+# Deploy API and Worker
+#
+
+echo "Deploy API and Worker..."
+
+# Change to the "python3" directory
+
+cd "${START_DIR}"
+cd python3
+
+# Create or get database host secret
+
+DATABASE_HOST=$(./get-database-secret.py $CMS_ENV database_host $DATABASE_SECRET_DATETIME)
+if [ -z "${DATABASE_HOST}" ]; then
+  aws secretsmanager create-secret \
+    --name "ab2d/${CMS_ENV}/module/db/database_host/${DATABASE_SECRET_DATETIME}" \
+    --secret-string "${DB_ENDPOINT}"
+fi
+
+# Create or get database port secret
+
+DB_PORT=$(aws --region us-east-1 rds describe-db-instances \
+  --query="DBInstances[?DBInstanceIdentifier=='ab2d'].Endpoint.Port" \
+  --output=text)
+
+DATABASE_PORT=$(./get-database-secret.py $CMS_ENV database_port $DATABASE_SECRET_DATETIME)
+if [ -z "${DATABASE_PORT}" ]; then
+  aws secretsmanager create-secret \
+    --name "ab2d/${CMS_ENV}/module/db/database_port/${DATABASE_SECRET_DATETIME}" \
+    --secret-string "${DB_PORT}"
+fi
+
+# Get database secret manager ARNs
+
+DATABASE_HOST_SECRET_ARN=$(aws secretsmanager describe-secret \
+  --secret-id "ab2d/${CMS_ENV}/module/db/database_host/${DATABASE_SECRET_DATETIME}" \
+  --query "ARN" \
+  --output text)
+
+DATABASE_PORT_SECRET_ARN=$(aws secretsmanager describe-secret \
+  --secret-id "ab2d/${CMS_ENV}/module/db/database_port/${DATABASE_SECRET_DATETIME}" \
+  --query "ARN" \
+  --output text)
+
+DATABASE_USER_SECRET_ARN=$(aws secretsmanager describe-secret \
+  --secret-id "ab2d/${CMS_ENV}/module/db/database_user/${DATABASE_SECRET_DATETIME}" \
+  --query "ARN" \
+  --output text)
+
+DATABASE_PASSWORD_SECRET_ARN=$(aws secretsmanager describe-secret \
+  --secret-id "ab2d/${CMS_ENV}/module/db/database_password/${DATABASE_SECRET_DATETIME}" \
+  --query "ARN" \
+  --output text)
+
+DATABASE_NAME_SECRET_ARN=$(aws secretsmanager describe-secret \
+  --secret-id "ab2d/${CMS_ENV}/module/db/database_name/${DATABASE_SECRET_DATETIME}" \
+  --query "ARN" \
+  --output text)
+
+# Change to the target terraform environment
+
+cd "${START_DIR}"
+cd terraform/environments/ab2d-$CMS_ENV
+
+if [ -z "${AUTOAPPROVE}" ]; then
+    
+  # Confirm with the caller prior to applying changes.
+
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_host=$DATABASE_HOST" \
+    --var "db_port=$DATABASE_PORT" \
+    --var "db_username=$DATABASE_USER" \
+    --var "db_password=$DATABASE_PASSWORD" \
+    --var "db_name=$DATABASE_NAME" \
+    --var "db_host_secret_arn=$DATABASE_HOST_SECRET_ARN" \
+    --var "db_port_secret_arn=$DATABASE_PORT_SECRET_ARN" \
+    --var "db_user_secret_arn=$DATABASE_USER_SECRET_ARN" \
+    --var "db_password_secret_arn=$DATABASE_PASSWORD_SECRET_ARN" \
+    --var "db_name_secret_arn=$DATABASE_NAME_SECRET_ARN" \
+    --var "deployer_ip_address=$DEPLOYER_IP_ADDRESS" \
+    --target module.api
+  
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_host=$DATABASE_HOST" \
+    --var "db_port=$DATABASE_PORT" \
+    --var "db_username=$DATABASE_USER" \
+    --var "db_password=$DATABASE_PASSWORD" \
+    --var "db_name=$DATABASE_NAME" \
+    --var "db_host_secret_arn=$DATABASE_HOST_SECRET_ARN" \
+    --var "db_port_secret_arn=$DATABASE_PORT_SECRET_ARN" \
+    --var "db_user_secret_arn=$DATABASE_USER_SECRET_ARN" \
+    --var "db_password_secret_arn=$DATABASE_PASSWORD_SECRET_ARN" \
+    --var "db_name_secret_arn=$DATABASE_NAME_SECRET_ARN" \
+    --target module.worker
+
+else
+    
+  # Apply the changes without prompting
+
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_host=$DATABASE_HOST" \
+    --var "db_port=$DATABASE_PORT" \
+    --var "db_username=$DATABASE_USER" \
+    --var "db_password=$DATABASE_PASSWORD" \
+    --var "db_name=$DATABASE_NAME" \
+    --var "db_host_secret_arn=$DATABASE_HOST_SECRET_ARN" \
+    --var "db_port_secret_arn=$DATABASE_PORT_SECRET_ARN" \
+    --var "db_user_secret_arn=$DATABASE_USER_SECRET_ARN" \
+    --var "db_password_secret_arn=$DATABASE_PASSWORD_SECRET_ARN" \
+    --var "db_name_secret_arn=$DATABASE_NAME_SECRET_ARN" \
+    --var "deployer_ip_address=$DEPLOYER_IP_ADDRESS" \
+    --target module.api \
     --auto-approve
 
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --var "db_host=$DATABASE_HOST" \
+    --var "db_port=$DATABASE_PORT" \
+    --var "db_username=$DATABASE_USER" \
+    --var "db_password=$DATABASE_PASSWORD" \
+    --var "db_name=$DATABASE_NAME" \
+    --var "db_host_secret_arn=$DATABASE_HOST_SECRET_ARN" \
+    --var "db_port_secret_arn=$DATABASE_PORT_SECRET_ARN" \
+    --var "db_user_secret_arn=$DATABASE_USER_SECRET_ARN" \
+    --var "db_password_secret_arn=$DATABASE_PASSWORD_SECRET_ARN" \
+    --var "db_name_secret_arn=$DATABASE_NAME_SECRET_ARN" \
+    --target module.worker \
+    --auto-approve
+
+fi
+
+#
+# Apply schedule autoscaling if applicable
+#
+
+echo "Apply schedule autoscaling if applicable..."
+if [ -f ./autoscaling-schedule.tf ]; then
+    
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --target=aws_autoscaling_schedule.morning \
+    --auto-approve
+
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --target=aws_autoscaling_schedule.night \
+    --auto-approve
+
+fi
+
+#
+# Push authorized_keys file to deployment_controller
+#
+
+cd "${START_DIR}"
+cd terraform/environments/ab2d-$CMS_SHARED_ENV
+
+echo "Push authorized_keys file to deployment_controller..."
+terraform taint \
+  --allow-missing null_resource.authorized_keys_file
+if [ -z "${AUTOAPPROVE}" ]; then
+  # Confirm with the caller prior to applying changes.
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --target null_resource.authorized_keys_file
+else
+  # Apply the changes without prompting
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --var "current_task_definition_arn=$API_TASK_DEFINITION" \
+    --target null_resource.authorized_keys_file \
+    --auto-approve
+fi
+
+#
+# Ensure new autoscaling group is running containers
+#
+
+echo "Ensure new autoscaling group is running containers..."
+
+ACTUAL_API_COUNT=0
+RETRIES_API=0
+
+while [ "$ACTUAL_API_COUNT" -lt "$EXPECTED_API_COUNT" ]; do
+  ACTUAL_API_COUNT=$(api_task_count)
+  echo "Running API Tasks: $ACTUAL_API_COUNT, Expected: $EXPECTED_API_COUNT"
+  if [ "$RETRIES_API" != "15" ]; then
+    echo "Retry in 60 seconds..."
+    sleep 60
+    RETRIES_API=$(expr $RETRIES_API + 1)
+  else
+    echo "Max retries reached. Exiting..."
+    exit 1
+  fi
+done
+
+#
+# Deploy CloudWatch
+#
+
+cd "${START_DIR}"
+cd terraform/environments/ab2d-$CMS_ENV
+
+echo "Deploy CloudWatch..."
+if [ -z "${AUTOAPPROVE}" ]; then
+
+  terraform apply \
+    --var "ami_id=$AMI_ID" \
+    --target module.cloudwatch
+
+else
+    
+  # Apply the changes without prompting
+
+  terraform apply \
+    --target module.cloudwatch \
+    --var "ami_id=$AMI_ID" \
+    --auto-approve
+
+fi
+
+#
+# Deploy AWS WAF
+#
+
+if [ -z "${AUTOAPPROVE}" ]; then
+    
+  terraform apply \
+    --target module.waf
+  
+else
+
+  # Apply the changes without prompting
+    
+  terraform apply \
+    --target module.waf \
+    --auto-approve
+  
+fi
+
+#
+# Apply AWS Shield standard to the application load balancer
+#
+
+# Note that no change is actually made since AWS shield standard is automatically applied to
+# the application load balancer. This section may be needed later if AWS Shield Advanced is
+# applied instead.
+
+if [ -z "${AUTOAPPROVE}" ]; then
+    
+  terraform apply \
+    --target module.shield
+  
+else
+
+  # Apply the changes without prompting
+    
+  terraform apply \
+    --target module.shield \
+    --auto-approve
+  
 fi
