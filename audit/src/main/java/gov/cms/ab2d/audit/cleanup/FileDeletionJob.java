6ec139257065d2258e6f68a13a697a442a24f7ff
satheesh pathiyil <56698881+spathiyil@users.noreply.github.com>
2019-12-18 11:28:22 -0500

    prevent concurrent execution of quartz job in worker (#90)
    * prevent concurrent execution of quartz job
    
    * adjust quartz setup in audit to DisallowConcurrentExecution

--- audit/src/main/java/gov/cms/ab2d/audit/cleanup/FileDeletionJob.java
+++ audit/src/main/java/gov/cms/ab2d/audit/cleanup/FileDeletionJob.java
@@ -2,11 +2,13 @@ package gov.cms.ab2d.audit.cleanup;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 @Slf4j
 @RequiredArgsConstructor
+@DisallowConcurrentExecution
 public class FileDeletionJob extends QuartzJobBean {
 
     private final FileDeletionService fileDeletionService;
