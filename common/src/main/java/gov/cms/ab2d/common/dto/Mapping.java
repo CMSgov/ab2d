eac3bfabb0801a44b9699d775f3b90ecab0ced53
adaykin <daykinandy@gmail.com>
2019-12-18 09:50:46 -0600

    WIP

--- common/src/main/java/gov/cms/ab2d/common/dto/Mapping.java
+++ common/src/main/java/gov/cms/ab2d/common/dto/Mapping.java
@@ -3,8 +3,8 @@ package gov.cms.ab2d.common.dto;
 import gov.cms.ab2d.common.model.Sponsor;
 import gov.cms.ab2d.common.model.User;
 import gov.cms.ab2d.common.service.SponsorService;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,15 +17,15 @@ public class Mapping {
 
     public Mapping() {
         modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
+        /*modelMapper.getConfiguration()
                 .setMatchingStrategy(MatchingStrategies.STRICT);
-        PropertyMap<UserDTO, User> userMap = new PropertyMap<>() {
-            protected void configure() {
-                Sponsor sponsor = sponsorService.findSponsorById(source.getSponsorId());
-                map().setSponsor(sponsor);
-            }
+        Converter<UserDTO, User> converter = context -> {
+            Sponsor sponsor = sponsorService.findSponsorById(context.getSource().getSponsorId());
+            context.getDestination().setSponsor(sponsor);
+            //return context.getDestination();
         };
-        modelMapper.addMappings(userMap);
+        modelMapper.createTypeMap(UserDTO.class, User.class)
+                .setPreConverter(converter);*/
     }
 
     public ModelMapper getModelMapper() {
