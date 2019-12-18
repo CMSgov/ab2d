4ffcbe9fe112b7fe4307b73564013e4d17414d06
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2019-12-18 15:42:09 -0500

    create scripts for sbx env

---
+++ Deploy/terraform/environments/ab2d-sbx/list-api-instances.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to list api instances in the environment
+
+# Note that the same values are used for both sbdemo and CMS AWS accounts
+export CMS_ENV="dev" #Examples: dev, sbx, impl, prod
+
+echo "*******************************************"
+echo "API instances in $CMS_ENV environment"
+echo "*******************************************"
+
+aws --region us-east-1 ec2 describe-instances --output text \
+  --filters "Name=tag:Name,Values=ab2d-$CMS_ENV-api" \
+  --query "Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]"
