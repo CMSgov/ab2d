cae1968d0015434690c3d6e5d008f9bcfb45d9c7
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-01-03 11:47:47 -0500

    remove unused imports

--- worker/src/main/java/gov/cms/ab2d/worker/stuckjob/StuckJobQuartzSetup.java
+++ worker/src/main/java/gov/cms/ab2d/worker/stuckjob/StuckJobQuartzSetup.java
@@ -8,7 +8,6 @@ import org.quartz.TriggerBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class StuckJobQuartzSetup {
