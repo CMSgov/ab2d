0c019dfdd2fbf3cee2db86939a2809d1e57d18a5
adaykin <daykinandy@gmail.com>
2020-01-06 14:13:01 -0600

    Adding transactional

--- common/src/main/java/gov/cms/ab2d/common/service/UserServiceImpl.java
+++ common/src/main/java/gov/cms/ab2d/common/service/UserServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 @Slf4j
 /**
