5c19f604b63ad49c17022f139ab58144dccae6d5
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-01-08 13:41:25 -0500

    reset in progress jobs to submitted status on Shutdown

--- worker/src/main/java/gov/cms/ab2d/worker/service/WorkerServiceImpl.java
+++ worker/src/main/java/gov/cms/ab2d/worker/service/WorkerServiceImpl.java
@@ -6,6 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This class is responsible for actually processing the job and preparing bulk downloads for users.
  */
@@ -16,17 +21,37 @@ public class WorkerServiceImpl implements WorkerService {
 
     private final JobPreProcessor jobPreprocessor;
     private final JobProcessor jobProcessor;
+    private final ShutDownService shutDownService;
+
+    private static List<String> jobsInProgres = Collections.synchronizedList(new ArrayList<>());
 
 
     @Override
     public void process(String jobUuid) {
+
+        jobsInProgres.add(jobUuid);
+
         jobPreprocessor.preprocess(jobUuid);
         log.info("Job was put in progress");
 
         jobProcessor.process(jobUuid);
         log.info("Job was processed");
+
+        jobsInProgres.remove(jobUuid);
     }
 
+
+
+    @PreDestroy
+    public void resetInProgressJobs() {
+        log.info("Shutdown in progress ... Do house cleaning ...");
+
+        if (!jobsInProgres.isEmpty()) {
+            shutDownService.resetInProgressJobs(jobsInProgres);
+        }
+
+        log.info("House cleaning done - Shutting down");
+    }
 
 
 }
