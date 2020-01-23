e6d684b98e8eff121293126dc9ae6ff8e9133fdd
satheesh pathiyil <56698881+spathiyil@users.noreply.github.com>
2020-01-23 09:07:49 -0500

    Feature/ab2d 839 update job progress during batch processing (#130)
    update job progress periodically through the batch process.
    A new property reportProgressDbFrequency was added to manage this periodicity.
    At the end of processHandles(), if the number of records processed since the last time 
    is greater than reportProgressDbFrequency AND the percentage is greater than the one
    saved last time, then the current percentage is calculated 
    and updated in the job table in the database.
    
    There is another new property named reportProgressLogFrequency which is used
    to manage the periodicity of logging progress in the log file. This is set to a higher number.
    
    * refactor - update progress in txn, log exception & cleanup
    
    * review comment - switch to HQL from nativeQuery
    * review comment - adjust property to report progress. Add a separate one for logging
    * review comments & refactoring
    * add & update junit tests

---
+++ worker/src/main/java/gov/cms/ab2d/worker/processor/domainmodel/ProgressTracker.java
@@ -0,0 +1,61 @@
+package gov.cms.ab2d.worker.processor.domainmodel;
+
+import gov.cms.ab2d.worker.adapter.bluebutton.GetPatientsByContractResponse;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Singular;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ProgressTracker {
+
+    private final String jobUuid;
+
+    @Singular
+    private final List<GetPatientsByContractResponse> patientsByContracts;
+    private int totalCount;
+    private int processedCount;
+
+    @Setter
+    private int lastDbUpdateCount;
+
+    @Setter
+    private int lastLogUpdateCount;
+
+    @Setter
+    private int lastUpdatedPercentage;
+
+    public void incrementProcessedCount() {
+        ++processedCount;
+    }
+
+
+    public int getTotalCount() {
+        if (totalCount == 0) {
+            totalCount = patientsByContracts.stream()
+                    .mapToInt(patientsByContract -> patientsByContract.getPatients().size())
+                    .sum();
+        }
+
+        return totalCount;
+    }
+
+    public boolean isTimeToUpdateDatabase(int reportProgressFrequency) {
+        return processedCount - lastDbUpdateCount >= reportProgressFrequency;
+    }
+
+    public boolean isTimeToLog(int reportProgressLogFrequency) {
+        return processedCount - lastLogUpdateCount >= reportProgressLogFrequency;
+    }
+
+    public int getPercentageCompleted() {
+        final int percentCompleted = (processedCount * 100) / getTotalCount();
+        lastDbUpdateCount = processedCount;
+        return percentCompleted;
+    }
+
+
+}
