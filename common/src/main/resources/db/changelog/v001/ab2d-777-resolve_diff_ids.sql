77607dfb647ddaed0bb2b52a7b5d3be52040262c
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-01-29 10:02:06 -0500

    Added liquibase annotations

--- common/src/main/resources/db/changelog/v001/ab2d-777-resolve_diff_ids.sql
+++ common/src/main/resources/db/changelog/v001/ab2d-777-resolve_diff_ids.sql
@@ -1,3 +1,7 @@
+--liquibase formatted sql
+--  -------------------------------------------------------------------------------------------------------------------
+
+--changeset lsharshar:ab2d-777-opt-out-resolve-ben-id-mbi failOnError:true
 ALTER TABLE opt_out ADD COLUMN mbi VARCHAR(255);
 ALTER TABLE opt_out ADD COLUMN ccw_id VARCHAR(255);
 
