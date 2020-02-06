11e876c907fa497e09efcb5ea3f94b014ef100d6
Denis Krylov <dkrylovsb@users.noreply.github.com>
2020-02-06 08:14:54 -0500

    AB2D-966: Changed the BFD Client to request data in JSON format, since the default is XML. (#153)
    * Introduced additional (but optional) environment variables to be able to fine-tune the app without having to do code changes. Reduced the "Retry After" from 30 to 5 seconds.
    
    * Moved some classes to the "right" package. Tuned thread pool configs a bit; also introduced environment variables to control the pool parameters without having to make code mods.
    
    * Moved PatientClaimsProcessorUnitTest.java to the right package.
    
    * Added the new properties to application.properties under "test" as well.
    
    * The Retry-After change had to be reflected in the e2e TestRunner.java.
    
    * Fixed "AB2D-914: Larger patient processor pool sizes lead to ConnectionPoolTimeoutException somewhere within the BFD Client" by increasing the connection pool size in Apache HTTP Client configuration. Also bumped the "PCP" pool size to 50 by default to help with job processing times.
    
    * Forgot to add the new properties to application.bfd.properties under "test".
    
    * Bumped `bfd.retry.maxAttempts` to 6 to be more conservative.
    
    * Modified the Contract-to-Bene mock to account for the fact that the synthetic patient IDs are negated in BFD SBX & Prod.
    
    * Added a log statement to FileDeletionServiceImpl.java to produce an indication that the job actually kicked off.
    
    * Changed the defaults for the PCP pool.
    
    * Typo fix.
    
    * Increased Hikari DB pool size.
    
    * Changed the BFD Client to request data in JSON format, since the default is XML.

--- bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
+++ bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
@@ -1,6 +1,7 @@
 package gov.cms.ab2d.bfd.client;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.RequestFormatParamStyleEnum;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.HttpClient;
@@ -59,8 +60,9 @@ public class BFDClientConfiguration {
     @Bean
     public IGenericClient bfdFhirRestClient(FhirContext fhirContext, HttpClient httpClient) {
         fhirContext.getRestfulClientFactory().setHttpClient(httpClient);
-
-        return fhirContext.newRestfulGenericClient(serverBaseUrl);
+        IGenericClient client = fhirContext.newRestfulGenericClient(serverBaseUrl);
+        client.setFormatParamStyle(RequestFormatParamStyleEnum.SHORT);
+        return client;
     }
 
     @Bean
