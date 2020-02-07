92a2434811a2ee298a61d1973fa88ee5b7489fdb
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-02-07 14:29:25 -0500

    Added Postgresql container to do test

--- common/src/test/java/gov/cms/ab2d/common/health/DatabaseAvailableTest.java
+++ common/src/test/java/gov/cms/ab2d/common/health/DatabaseAvailableTest.java
@@ -1,12 +1,16 @@
 package gov.cms.ab2d.common.health;
 
 import gov.cms.ab2d.common.SpringBootApp;
+import gov.cms.ab2d.common.util.AB2DPostgresqlContainer;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 
@@ -20,11 +24,15 @@ import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest(classes = SpringBootApp.class)
 @TestPropertySource(locations = "/application.common.properties")
+@Testcontainers
 class DatabaseAvailableTest {
     @Autowired
     private DataSource dataSource;
     @Mock
     private DataSource bogusDS;
+
+    @Container
+    private static final PostgreSQLContainer postgreSQLContainer= new AB2DPostgresqlContainer();
 
     @Test
     public void testDatasource() throws SQLException {
