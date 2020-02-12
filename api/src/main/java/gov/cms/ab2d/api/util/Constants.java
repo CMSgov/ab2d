7f67565ca3490a7f5e63dffff81bc0617b4a2fbf
Andy Daykin <daykinandy@gmail.com>
2020-02-12 08:43:49 -0600

    Refactoring to change to base url, adding health check for API. (#165)
    Adding an E2E test to hit the health endpoint, and then check the status to make sure it's a 200. Refactor to use a base url, instead of the API url.

--- api/src/main/java/gov/cms/ab2d/api/util/Constants.java
+++ api/src/main/java/gov/cms/ab2d/api/util/Constants.java
@@ -1,5 +1,8 @@
 package gov.cms.ab2d.api.util;
 
+import static gov.cms.ab2d.common.util.Constants.API_PREFIX;
+import static gov.cms.ab2d.common.util.Constants.FHIR_PREFIX;
+
 public final class Constants {
 
     public static final String GENERIC_FHIR_ERR_MSG = "The body will contain a FHIR " +
@@ -7,12 +10,6 @@ public final class Constants {
             "https://www.hl7.org/fhir/operationoutcome.html " +
             "Please refer to the body of the response for " +
             "details.";
-
-    public static final String API_PREFIX = "/api/v1";
-
-    public static final String FHIR_PREFIX = "/fhir";
-
-    public static final String ADMIN_PREFIX = "/admin";
 
     public static final String ADMIN_ROLE = "ADMIN";
 
