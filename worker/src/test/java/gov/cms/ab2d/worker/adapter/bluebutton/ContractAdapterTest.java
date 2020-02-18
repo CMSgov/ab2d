17c32dff3709709566dbb01322774fa371cab9b2
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-02-18 11:26:11 -0500

    add junit test cases

--- worker/src/test/java/gov/cms/ab2d/worker/adapter/bluebutton/ContractAdapterTest.java
+++ worker/src/test/java/gov/cms/ab2d/worker/adapter/bluebutton/ContractAdapterTest.java
@@ -4,22 +4,23 @@ import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import gov.cms.ab2d.bfd.client.BFDClient;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.dstu3.model.Bundle.BundleLinkComponent;
 import org.hl7.fhir.dstu3.model.Identifier;
 import org.hl7.fhir.dstu3.model.Patient;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
+import java.time.Month;
 
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -36,31 +37,180 @@ class ContractAdapterTest {
 
     private ContractAdapter cut;
     private String contractNumber = "S0000";
-    private int currentMonth;
+    private int currentMonth = Month.MARCH.getValue();
+    private Bundle bundle;
 
 
     @BeforeEach
     void setUp() {
         cut = new ContractAdapterImpl(client);
-
-        currentMonth = LocalDate.now().getMonthValue();
-        var bundle = new Bundle();
-        var entries = bundle.getEntry();
-        entries.add(createBundleEntry("ccw_patient_000"));
-        entries.add(createBundleEntry("ccw_patient_001"));
-
+        bundle = createBundle();
         when(client.requestPartDEnrolleesFromServer(anyString(), anyInt())).thenReturn(bundle);
     }
-
-
 
     @Test
     @DisplayName("given contractNumber, get patients from BFD API")
     void GivenContractNumber_ShouldReturnPatients() {
         var response = cut.getPatients(contractNumber, currentMonth);
 
-        Assert.assertThat(response, notNullValue());
-        Assert.assertThat(response.getContractNumber(), is(contractNumber));
+        assertThat(response, notNullValue());
+        assertThat(response.getContractNumber(), is(contractNumber));
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(1));
+    }
+
+    @Test
+    void GivenPatientActiveInJanuary_ShouldReturnOneRowInDateRangesUnderContract() {
+        var response = cut.getPatients(contractNumber, Month.JANUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(1));
+
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(1));
+    }
+
+    @Test
+    void GivenPatientActiveInJanAndFeb_ShouldReturnTwoRowsInDateRangesUnderContract() {
+        var response = cut.getPatients(contractNumber, Month.FEBRUARY.getValue());
+
+        var patient0 = response.getPatients().get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(2));
+    }
+
+    @Test
+    void GivenSecondPatientJoinsInFeb_ShouldReturnOnlyOneRowsForThatPatientInDateRangesUnderContract() {
+        var bundle1 = bundle.copy();
+
+        // add 2nd patient
+        var entries = bundle1.getEntry();
+        entries.add(createBundleEntry("ccw_patient_001"));
+
+        when(client.requestPartDEnrolleesFromServer(anyString(), anyInt()))
+                .thenReturn(bundle, bundle1);
+
+        var response = cut.getPatients(contractNumber, Month.FEBRUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(2));
+
+        //1st patient has 2 rows in date ranges under contract
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(2));
+
+        //2nd patient has 1 row in date ranges under contract
+        var patient1 = patients.get(1);
+        assertThat(patient1.getPatientId(), is("ccw_patient_001"));
+        assertThat(patient1.getDateRangesUnderContract().size(), is(1));
+    }
+
+
+    @Test
+    void GivenAPatientLeavesInFeb_ShouldReturnOnlyOneRowsForThatPatientInDateRangesUnderContract() {
+        var bundle1 = bundle.copy();
+
+        //bundle1 has 2 patients in January
+        var entries = bundle1.getEntry();
+        entries.add(createBundleEntry("ccw_patient_001"));
+
+        //bundle2  has 1 patient in February, coz patient_001 left and is no longer active in contract
+        var bundle2 = bundle.copy();
+
+        when(client.requestPartDEnrolleesFromServer(anyString(), anyInt()))
+                .thenReturn(bundle1, bundle2);
+
+        var response = cut.getPatients(contractNumber, Month.FEBRUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(2));
+
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(2));
+
+        var patient1 = patients.get(1);
+        assertThat(patient1.getPatientId(), is("ccw_patient_001"));
+        assertThat(patient1.getDateRangesUnderContract().size(), is(1));
+    }
+
+
+    @Test
+    void GivenTwoPatientsActiveInJanAndFeb_ShouldReturnTwoPatientRowsEachWithTwoRowsInDateRangesUnderContract() {
+        var entries = bundle.getEntry();
+        entries.add(createBundleEntry("ccw_patient_001"));
+
+        var response = cut.getPatients(contractNumber, Month.FEBRUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(2));
+
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(2));
+
+        var patient1 = patients.get(1);
+        assertThat(patient1.getPatientId(), is("ccw_patient_001"));
+        assertThat(patient1.getDateRangesUnderContract().size(), is(2));
+    }
+
+
+    @Test
+    void GivenMultiplePages_ShouldProcessAllPages() {
+        var bundle1 = bundle.copy();
+
+        var entries = bundle1.getEntry();
+        entries.add(createBundleEntry("ccw_patient_001"));
+        bundle1.addLink(addNextLink());
+
+        var bundle2 = createBundle("ccw_patient_002");
+
+        when(client.requestPartDEnrolleesFromServer(anyString(), anyInt())).thenReturn(bundle1);
+        when(client.requestNextBundleFromServer(Mockito.any(Bundle.class))).thenReturn(bundle2);
+
+        var response = cut.getPatients(contractNumber, Month.JANUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(3));
+
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(1));
+
+        var patient1 = patients.get(1);
+        assertThat(patient1.getPatientId(), is("ccw_patient_001"));
+        assertThat(patient1.getDateRangesUnderContract().size(), is(1));
+
+        var patient2 = patients.get(2);
+        assertThat(patient2.getPatientId(), is("ccw_patient_002"));
+        assertThat(patient2.getDateRangesUnderContract().size(), is(1));
+    }
+
+
+    @Test
+    void GivenDuplicatePatientRowsFromBFD_ShouldEliminateDuplicates() {
+        var entries = bundle.getEntry();
+        entries.add(createBundleEntry("ccw_patient_001"));
+        //simulate duplicate patient record coming from BDF
+        entries.add(createBundleEntry("ccw_patient_001"));
+
+        when(client.requestPartDEnrolleesFromServer(anyString(), anyInt())).thenReturn(bundle);
+
+        var response = cut.getPatients(contractNumber, Month.JANUARY.getValue());
+
+        var patients = response.getPatients();
+        assertThat(patients.size(), is(2));
+
+        var patient0 = patients.get(0);
+        assertThat(patient0.getPatientId(), is("ccw_patient_000"));
+        assertThat(patient0.getDateRangesUnderContract().size(), is(1));
+
+        var patient1 = patients.get(1);
+        assertThat(patient1.getPatientId(), is("ccw_patient_001"));
+        assertThat(patient1.getDateRangesUnderContract().size(), is(1));
     }
 
 
@@ -77,8 +227,16 @@ class ContractAdapterTest {
     }
 
 
+    private Bundle createBundle() {
+        return createBundle("ccw_patient_000");
+    }
 
-
+    private Bundle createBundle(final String patientId) {
+        var bundle = new Bundle();
+        var entries = bundle.getEntry();
+        entries.add(createBundleEntry(patientId));
+        return bundle;
+    }
 
     private BundleEntryComponent createBundleEntry(String patientId) {
         var component = new BundleEntryComponent();
@@ -97,6 +255,12 @@ class ContractAdapterTest {
         identifier.setSystem(BENEFICIARY_ID);
         identifier.setValue(patientId);
         return identifier;
+    }
+
+    private BundleLinkComponent addNextLink() {
+        var linkComponent = new BundleLinkComponent();
+        linkComponent.setRelation(Bundle.LINK_NEXT);
+        return linkComponent;
     }
 
 }
