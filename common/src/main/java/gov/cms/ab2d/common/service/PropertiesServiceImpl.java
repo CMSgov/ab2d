c176618e2c5fc8506e3855310287f17eaa9c29de
adaykin <daykinandy@gmail.com>
2020-02-18 14:59:30 -0600

    Fixing tests

--- common/src/main/java/gov/cms/ab2d/common/service/PropertiesServiceImpl.java
+++ common/src/main/java/gov/cms/ab2d/common/service/PropertiesServiceImpl.java
@@ -5,10 +5,12 @@ import gov.cms.ab2d.common.repository.PropertiesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @Slf4j
 public class PropertiesServiceImpl implements PropertiesService {
 
