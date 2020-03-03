80fac2c4d498a5f4c6b512cea016468f4b7f9a46
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-03-03 09:09:23 -0500

    add liquibase changeset - default value for zipSupport is off

---
+++ common/src/main/resources/db/changelog/v001/add_zip_support_toggle_property.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--  -------------------------------------------------------------------------------------------------------------------
+
+
+--changeset spathiyil:ab2d_1110_add_zip_support_toggle_in_property_table failOnError:true
+INSERT INTO properties (id, key, value) VALUES((select nextval('hibernate_sequence')), 'ZipSupportOn', 'false');
+
