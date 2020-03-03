ade69b44f2bc6042bdc6729476585192bcfc7035
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-03-03 12:33:27 -0500

    liquibase changeset

--- common/src/main/resources/db/changelog/v001/add_zip_support_toggle_property.sql
+++ common/src/main/resources/db/changelog/v001/add_zip_support_toggle_property.sql
@@ -5,3 +5,5 @@
 --changeset spathiyil:ab2d_1110_add_zip_support_toggle_in_property_table failOnError:true
 INSERT INTO properties (id, key, value) VALUES((select nextval('hibernate_sequence')), 'ZipSupportOn', 'false');
 
+--rollback DELETE FROM properties WHERE key = 'ZipSupportOn';
+
