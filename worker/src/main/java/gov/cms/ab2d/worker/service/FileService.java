bb3bfbb94782eb3565fe12596f9948229124320f
Andy Daykin <daykinandy@gmail.com>
2020-03-11 10:34:50 -0500

    Feature/ab2d 1146 status file response (#229)
    Create a migration so that checksum and file length columns are added to the database. Set the checksum and file length from the worker when the job is completed so that when a user makes a Bulk Status call that has a 200 status, the checksum and file length will be displayed to the user.

--- worker/src/main/java/gov/cms/ab2d/worker/service/FileService.java
+++ worker/src/main/java/gov/cms/ab2d/worker/service/FileService.java
@@ -1,8 +1,11 @@
 package gov.cms.ab2d.worker.service;
 
+import java.io.File;
 import java.nio.file.Path;
 
 public interface FileService {
 
     Path createDirectory(Path outputDir);
+
+    String generateChecksum(File file);
 }
