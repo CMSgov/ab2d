76c0337ad55b7af709c8792db8ff4b4b8c6c67de
spathiyil <satheesh.pathiyil@semanticbits.com>
2020-03-18 11:49:16 -0400

    use lombok

--- worker/src/main/java/gov/cms/ab2d/worker/processor/domainmodel/PatientClaimsRequest.java
+++ worker/src/main/java/gov/cms/ab2d/worker/processor/domainmodel/PatientClaimsRequest.java
@@ -1,43 +1,20 @@
 package gov.cms.ab2d.worker.processor.domainmodel;
 
 import com.newrelic.api.agent.Token;
-import gov.cms.ab2d.worker.adapter.bluebutton.GetPatientsByContractResponse;
+import gov.cms.ab2d.worker.adapter.bluebutton.GetPatientsByContractResponse.PatientDTO;
 import gov.cms.ab2d.worker.processor.StreamHelper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.OffsetDateTime;
 
+@Getter
+@AllArgsConstructor
 public class PatientClaimsRequest {
-    private final GetPatientsByContractResponse.PatientDTO patientDTO;
+    private final PatientDTO patientDTO;
     private final StreamHelper helper;
     private final OffsetDateTime attTime;
     private final OffsetDateTime sinceTime;
     private final Token token;
 
-    public PatientClaimsRequest(GetPatientsByContractResponse.PatientDTO patientDTO, StreamHelper helper, OffsetDateTime attTime, OffsetDateTime sinceTime, Token token) {
-        this.patientDTO = patientDTO;
-        this.helper = helper;
-        this.attTime = attTime;
-        this.sinceTime = sinceTime;
-        this.token = token;
-    }
-
-    public GetPatientsByContractResponse.PatientDTO getPatientDTO() {
-        return patientDTO;
-    }
-
-    public StreamHelper getHelper() {
-        return helper;
-    }
-
-    public OffsetDateTime getAttTime() {
-        return attTime;
-    }
-
-    public OffsetDateTime getSinceTime() {
-        return sinceTime;
-    }
-
-    public Token getToken() {
-        return token;
-    }
 }
