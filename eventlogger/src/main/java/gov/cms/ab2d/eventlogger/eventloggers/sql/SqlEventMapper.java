63a4c65fc2692da5b952efc6da674d251ca57068
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-04-02 10:47:56 -0400

    Added comment to see if Travis won't rebuild (I can't see to do it manually)

--- eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/sql/SqlEventMapper.java
+++ eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/sql/SqlEventMapper.java
@@ -3,6 +3,10 @@ package gov.cms.ab2d.eventlogger.eventloggers.sql;
 import gov.cms.ab2d.eventlogger.LoggableEvent;
 import org.springframework.jdbc.core.RowMapper;
 
+/**
+ * Defines what an SQL mapper should have - you should be able to log
+ * and you should be able to read a the event from a result set
+ */
 public abstract class SqlEventMapper implements RowMapper {
     abstract void log(LoggableEvent event);
 }
