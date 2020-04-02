2d32cb5995772cf5ae90d86a0feb35157427e19e
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-04-02 14:28:20 -0400

    Added transactional information and surround with try/catch

--- eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/sql/SqlEventLogger.java
+++ eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/sql/SqlEventLogger.java
@@ -3,9 +3,13 @@ package gov.cms.ab2d.eventlogger.eventloggers.sql;
 import gov.cms.ab2d.eventlogger.EventLogger;
 import gov.cms.ab2d.eventlogger.EventLoggingException;
 import gov.cms.ab2d.eventlogger.LoggableEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class SqlEventLogger implements EventLogger {
     private final SqlMapperConfig mapperConfig;
 
@@ -14,11 +18,16 @@ public class SqlEventLogger implements EventLogger {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void log(LoggableEvent event) {
-        SqlEventMapper mapper = mapperConfig.getMapper(event.getClass());
-        if (mapper == null) {
-            throw new EventLoggingException("Can't find logger for " + event.getClass().toString());
+        try {
+            SqlEventMapper mapper = mapperConfig.getMapper(event.getClass());
+            if (mapper == null) {
+                throw new EventLoggingException("Can't find logger for " + event.getClass().toString());
+            }
+            mapper.log(event);
+        } catch (Exception ex) {
+            log.error("Error in logging event " + event.toString());
         }
-        mapper.log(event);
     }
 }
