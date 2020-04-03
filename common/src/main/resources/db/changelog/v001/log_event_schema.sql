80c49735e261610ad2c7717b93ad13da33c3013d
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-04-03 13:32:57 -0400

    Increased size of request id

--- common/src/main/resources/db/changelog/v001/log_event_schema.sql
+++ common/src/main/resources/db/changelog/v001/log_event_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE event_api_request
         url VARCHAR(2048),
         ip_address VARCHAR(32),
         token_hash VARCHAR(255),
-        request_id VARCHAR(32)
+        request_id VARCHAR(255)
 );
 ALTER TABLE event_api_request ADD CONSTRAINT "pk_api_request_event" PRIMARY KEY (id);
 CREATE INDEX "ix_api_request_user" ON event_api_request (user_id);
