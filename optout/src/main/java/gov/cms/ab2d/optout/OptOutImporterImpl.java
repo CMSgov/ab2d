682e0c0b89ac813e5f228012d7a2e45736c4c2e8
Laila Sharshar <laila.sharshar@semanticbits.com>
2020-04-09 16:45:01 -0400

    Unused import

--- optout/src/main/java/gov/cms/ab2d/optout/OptOutImporterImpl.java
+++ optout/src/main/java/gov/cms/ab2d/optout/OptOutImporterImpl.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
