bd344199444b3ab490e5637bce8b599cacd0e1aa
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-04-21 11:47:27 -0400

    updated script to use CloudTamer API

--- Deploy/bash/get-logs.sh
+++ Deploy/bash/get-logs.sh
@@ -11,37 +11,15 @@ START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${START_DIR}"
 
 #
-# Define functions
-#
-
-aws_credentials_parser ()
-{
-    ini="$(<$1)"                # read the file
-    ini="${ini//[/\[}"          # escape [
-    ini="${ini//]/\]}"          # escape ]
-    IFS=$'\n' && ini=( ${ini} ) # convert to line-array
-    ini=( ${ini[*]//;*/} )      # remove comments with ;
-    ini=( ${ini[*]/\    =/=} )  # remove tabs before =
-    ini=( ${ini[*]/=\   /=} )   # remove tabs after =
-    ini=( ${ini[*]/\ =\ /=} )   # remove anything with a space around =
-    ini=( ${ini[*]/#\\[/\}$'\n'aws_credentials.section.} ) # set section prefix
-    ini=( ${ini[*]/%\\]/ \(} )    # convert text2function (1)
-    ini=( ${ini[*]/=/=\( } )    # convert item to array
-    ini=( ${ini[*]/%/ \)} )     # close array parenthesis
-    ini=( ${ini[*]/%\\ \)/ \\} ) # the multiline trick
-    ini=( ${ini[*]/%\( \)/\(\) \{} ) # convert text2function (2)
-    ini=( ${ini[*]/%\} \)/\}} ) # remove extra parenthesis
-    ini[0]="" # remove first element
-    ini[${#ini[*]} + 1]='}'    # add the last brace
-    eval "$(echo "${ini[*]}")" # eval the result
-}
-
-#
 # Retrieve user input
 #
 
 # Ask user of chooose an environment
 
+echo ""
+echo "--------------------------"
+echo "Choose desired AWS account"
+echo "--------------------------"
 echo ""
 PS3='Please enter your choice: '
 options=("Dev AWS account" "Sbx AWS account" "Impl AWS account" "Quit")
@@ -49,17 +27,20 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Dev AWS account")
-	    export AWS_PROFILE=ab2d-dev
+	    export AWS_ACCOUNT_NUMBER=349849222861
+	    export CMS_ENV=ab2d-dev
 	    SSH_PRIVATE_KEY=ab2d-dev.pem
 	    break
             ;;
         "Sbx AWS account")
-	    export AWS_PROFILE=ab2d-sbx-sandbox
+	    export AWS_ACCOUNT_NUMBER=777200079629
+	    export CMS_ENV=ab2d-sbx-sandbox
 	    SSH_PRIVATE_KEY=ab2d-sbx-sandbox.pem
 	    break
             ;;
         "Impl AWS account")
-	    export AWS_PROFILE=ab2d-east-impl
+	    export AWS_ACCOUNT_NUMBER=330810004472
+	    export CMS_ENV=ab2d-east-impl
 	    SSH_PRIVATE_KEY=ab2d-east-impl.pem
 	    break
             ;;
@@ -74,34 +55,73 @@ if [ $REPLY -eq 4 ]; then
   exit 0
 fi
 
-echo ""
-echo "Retreiving logs from option $REPLY ($opt)..."
-echo ""
+#
+# Acquire temporary credentials via CloudTamer API
+#
 
-# Verify AWS credentials exist
-
-if [ -f "${HOME}/.aws/credentials" ]; then
-  echo "Verified AWS credentials exist..."
+if [ -z $CLOUDTAMER_USER_NAME ] || [ -z $CLOUDTAMER_PASSWORD ]; then
   echo ""
-else
-  echo "***********************************"
-  echo "ERROR: AWS credentials do not exist"
-  echo "***********************************"
-  echo ""
-  exit 1
+  echo "----------------------------"
+  echo "Enter CloudTamer credentials"
+  echo "----------------------------"
 fi
 
-# Verify AWS profile exists
-
-aws_credentials_parser "${HOME}/.aws/credentials"
-aws_credentials.section.ab2d-dev
-
-if [ -n "${aws_access_key_id}" ] && [ -n "${aws_secret_access_key}" ]; then
-  echo "AWS credentials exist for the ${AWS_PROFILE} AWS profile..."
+if [ -z $CLOUDTAMER_USER_NAME ]; then
   echo ""
-else
+  echo "Enter your CloudTamer user name (EUA ID):"
+  read CLOUDTAMER_USER_NAME
+fi
+
+if [ -z $CLOUDTAMER_PASSWORD ]; then
+  echo ""
+  echo "Enter your CloudTamer password:"
+  read CLOUDTAMER_PASSWORD
+fi
+
+echo ""
+echo "--------------------"
+echo "Getting bearer token"
+echo "--------------------"
+echo ""
+
+BEARER_TOKEN=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v2/token' \
+  --header 'Accept: application/json' \
+  --header 'Accept-Language: en-US,en;q=0.5' \
+  --header 'Content-Type: application/json' \
+  --data-raw "{\"username\":\"${CLOUDTAMER_USER_NAME}\",\"password\":\"${CLOUDTAMER_PASSWORD}\",\"idms\":{\"id\":2}}" \
+  | jq --raw-output ".data.access.token")
+
+echo ""
+echo "-----------------------------"
+echo "Getting temporary credentials"
+echo "-----------------------------"
+echo ""
+
+JSON_OUTPUT=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v3/temporary-credentials' \
+  --header 'Accept: application/json' \
+  --header 'Accept-Language: en-US,en;q=0.5' \
+  --header 'Content-Type: application/json' \
+  --header "Authorization: Bearer ${BEARER_TOKEN}" \
+  --header 'Content-Type: application/json' \
+  --data-raw "{\"account_number\":\"${AWS_ACCOUNT_NUMBER}\",\"iam_role_name\":\"ab2d-spe-developer\"}" \
+  | jq --raw-output ".data")
+
+# Get temporary AWS credentials
+
+export AWS_ACCESS_KEY_ID=$(echo $JSON_OUTPUT | jq --raw-output ".access_key")
+export AWS_SECRET_ACCESS_KEY=$(echo $JSON_OUTPUT | jq --raw-output ".secret_access_key")
+
+# Get AWS session token (required for temporary credentials)
+
+export AWS_SESSION_TOKEN=$(echo $JSON_OUTPUT | jq --raw-output ".session_token")
+
+# Verify temporary AWS credentials
+
+if [ -z "${AWS_ACCESS_KEY_ID}" ] \
+    || [ -z "${AWS_SECRET_ACCESS_KEY}" ] \
+    || [ -z "${AWS_SESSION_TOKEN}" ]; then
   echo "**********************************************************************"
-  echo "ERROR: AWS credentials do not exist for the ${AWS_PROFILE} AWS profile"
+  echo "ERROR: AWS credentials do not exist for the ${CMS_ENV} AWS account"
   echo "**********************************************************************"
   echo ""
   exit 1
@@ -109,12 +129,9 @@ fi
 
 # Verify SSH private key exists
 
-if [ -f "${HOME}/.ssh/${SSH_PRIVATE_KEY}" ]; then
-  echo "SSH private key for ${AWS_PROFILE} exists..."
-  echo ""
-else
+if [ ! -f "${HOME}/.ssh/${SSH_PRIVATE_KEY}" ]; then
   echo "**********************************************************************"
-  echo "ERROR: SSH private key for ${AWS_PROFILE} do not exist..."
+  echo "ERROR: SSH private key for ${CMS_ENV} do not exist..."
   echo "**********************************************************************"
   echo ""
   exit 1
@@ -123,40 +140,33 @@ fi
 # Get the private IP addresses of API nodes
 
 aws --region us-east-1 ec2 describe-instances \
-  --filters "Name=tag:Name,Values=${AWS_PROFILE}-api" \
+  --filters "Name=tag:Name,Values=${CMS_ENV}-api" \
   --query="Reservations[*].Instances[?State.Name == 'running'].PrivateIpAddress" \
   --output text \
-  > "/tmp/${AWS_PROFILE}-api-nodes.txt"
+  > "/tmp/${CMS_ENV}-api-nodes.txt"
 
 # Get the private IP addresses of worker nodes
 
 aws --region us-east-1 ec2 describe-instances \
-  --filters "Name=tag:Name,Values=${AWS_PROFILE}-worker" \
+  --filters "Name=tag:Name,Values=${CMS_ENV}-worker" \
   --query="Reservations[*].Instances[?State.Name == 'running'].PrivateIpAddress" \
   --output text \
-  > "/tmp/${AWS_PROFILE}-worker-nodes.txt"
+  > "/tmp/${CMS_ENV}-worker-nodes.txt"
 
 # Delete old logs
-
-echo ""
-echo "Delete existing API and worker logs in the 'Downloads' directory..."
 
 rm -f ${HOME}/Downloads/messages-api-node-*.txt
 rm -f ${HOME}/Downloads/messages-worker-node-*.txt
 
 # Get logs from API nodes
 
-input="/tmp/${AWS_PROFILE}-api-nodes.txt"
+input="/tmp/${CMS_ENV}-api-nodes.txt"
 COUNTER=0
 while IFS= read -r IP_ADDRESS
 do
-  echo "IP_ADDRESS=${IP_ADDRESS}"
   COUNTER=$((COUNTER+1))
   nc -v -z -G 5 "${IP_ADDRESS}" 22 &> /dev/null
-  if [ $? -eq 0 ]; then
-    echo ""
-    echo "VPN access to API node $COUNTER ($IP_ADDRESS) verified..."
-  else
+  if [ $? -ne 0 ]; then
     echo ""
     echo "**************************************************************************"
     echo "ERROR: VPN access to API node $COUNTER ($IP_ADDRESS) could not be verified"
@@ -165,7 +175,10 @@ do
     exit 1
   fi
   echo ""
-  echo "Getting log from API node $COUNTER ($IP_ADDRESS)..."
+  echo "-------------------------------------------------------"
+  echo "Getting log from API node $COUNTER ($IP_ADDRESS)"
+  echo "-------------------------------------------------------"
+  echo ""
   ssh -i "~/.ssh/${SSH_PRIVATE_KEY}" ec2-user@"${IP_ADDRESS}" \
     sudo cp /var/log/messages /home/ec2-user \
     < /dev/null
@@ -181,17 +194,13 @@ API_COUNT=$COUNTER
 
 # Get logs from worker nodes
 
-input="/tmp/${AWS_PROFILE}-worker-nodes.txt"
+input="/tmp/${CMS_ENV}-worker-nodes.txt"
 COUNTER=0
 while IFS= read -r IP_ADDRESS
 do
-  echo "IP_ADDRESS=${IP_ADDRESS}"
   COUNTER=$((COUNTER+1))
   nc -v -z -G 5 "${IP_ADDRESS}" 22 &> /dev/null
-  if [ $? -eq 0 ]; then
-    echo ""
-    echo "VPN access to worker node $COUNTER ($IP_ADDRESS) verified..."
-  else
+  if [ $? -ne 0 ]; then
     echo ""
     echo "*****************************************************************************"
     echo "ERROR: VPN access to worker node $COUNTER ($IP_ADDRESS) could not be verified"
@@ -200,7 +209,10 @@ do
     exit 1
   fi
   echo ""
-  echo "Getting log from worker node $COUNTER ($IP_ADDRESS)..."
+  echo "-------------------------------------------------------"
+  echo "Getting log from Worker node $COUNTER ($IP_ADDRESS)"
+  echo "-------------------------------------------------------"
+  echo ""
   ssh -i "~/.ssh/${SSH_PRIVATE_KEY}" ec2-user@"${IP_ADDRESS}" \
     sudo cp /var/log/messages /home/ec2-user \
     < /dev/null
@@ -230,7 +242,8 @@ if [ $REPLY -lt 4 ]; then
   echo "**********************************************"
   echo "ENVIRONMENT SUMMARY"
   echo "**********************************************"
-  echo "AWS_PROFILE=${AWS_PROFILE}"
+  echo ""
+  echo "CMS_ENV=${CMS_ENV}"
   echo "SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}"
   echo "API_COUNT=${API_COUNT}"
   echo "WORKER_COUNT=${WORKER_COUNT}"
@@ -240,6 +253,7 @@ if [ $REPLY -lt 4 ]; then
   echo "**********************************************"
   echo "LOGS DOWNLOADED"
   echo "**********************************************"
+  echo ""
   if [ $API_COUNT -eq 0 ] && [ $WORKER_COUNT -eq 0 ]; then
     echo "NONE"
   fi
