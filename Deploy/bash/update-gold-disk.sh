67a2409b84c27bc11940bae40f1de468bb598440
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-04-23 19:11:57 -0400

    update script for CloudTamer

--- Deploy/bash/update-gold-disk.sh
+++ Deploy/bash/update-gold-disk.sh
@@ -11,6 +11,22 @@ START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${START_DIR}"
 
 #
+# Check vars are not empty before proceeding
+#
+
+echo "Check vars are not empty before proceeding..."
+if [ -z "${CMS_ENV_PARAM}" ] \
+    || [ -z "${DEBUG_LEVEL_PARAM}" ] \
+    || [ -z "${EC2_INSTANCE_TYPE_PACKER_PARAM}" ] \
+    || [ -z "${OWNER_PARAM}" ] \
+    || [ -z "${REGION_PARAM}" ] \
+    || [ -z "${SSH_USERNAME_PARAM}" ] \
+    || [ -z "${VPC_ID_PARAM}" ]; then
+  echo "ERROR: All parameters must be set."
+  exit 1
+fi
+
+#
 # Set parameters
 #
 
@@ -27,6 +43,118 @@ REGION="${REGION_PARAM}"
 SSH_USERNAME="${SSH_USERNAME_PARAM}"
 
 VPC_ID="${VPC_ID_PARAM}"
+
+#
+# Set AWS account numbers
+#
+
+CMS_ECR_REPO_ENV_AWS_ACCOUNT_NUMBER=653916833532
+
+if [ "${CMS_ENV}" == "ab2d-dev" ]; then
+  CMS_ENV_AWS_ACCOUNT_NUMBER=349849222861
+elif [ "${CMS_ENV}" == "ab2d-sbx-sandbox" ]; then
+  CMS_ENV_AWS_ACCOUNT_NUMBER=777200079629
+elif [ "${CMS_ENV}" == "ab2d-east-impl" ]; then
+  CMS_ENV_AWS_ACCOUNT_NUMBER=330810004472
+else
+  echo "ERROR: 'CMS_ENV' environment is unknown."
+  exit 1  
+fi
+
+#
+# Define functions
+#
+
+get_temporary_aws_credentials ()
+{
+  # Set AWS account number
+
+  AWS_ACCOUNT_NUMBER="$1"
+
+  # Verify that CloudTamer user name and password environment variables are set
+
+  if [ -z $CLOUDTAMER_USER_NAME ] || [ -z $CLOUDTAMER_PASSWORD ]; then
+    echo ""
+    echo "----------------------------"
+    echo "Enter CloudTamer credentials"
+    echo "----------------------------"
+  fi
+
+  if [ -z $CLOUDTAMER_USER_NAME ]; then
+    echo ""
+    echo "Enter your CloudTamer user name (EUA ID):"
+    read CLOUDTAMER_USER_NAME
+  fi
+
+  if [ -z $CLOUDTAMER_PASSWORD ]; then
+    echo ""
+    echo "Enter your CloudTamer password:"
+    read CLOUDTAMER_PASSWORD
+  fi
+
+  # Get bearer token
+
+  echo ""
+  echo "--------------------"
+  echo "Getting bearer token"
+  echo "--------------------"
+  echo ""
+
+  BEARER_TOKEN=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v2/token' \
+    --header 'Accept: application/json' \
+    --header 'Accept-Language: en-US,en;q=0.5' \
+    --header 'Content-Type: application/json' \
+    --data-raw "{\"username\":\"${CLOUDTAMER_USER_NAME}\",\"password\":\"${CLOUDTAMER_PASSWORD}\",\"idms\":{\"id\":2}}" \
+    | jq --raw-output ".data.access.token")
+
+  # Get json output for temporary AWS credentials
+
+  echo ""
+  echo "-----------------------------"
+  echo "Getting temporary credentials"
+  echo "-----------------------------"
+  echo ""
+
+  JSON_OUTPUT=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v3/temporary-credentials' \
+    --header 'Accept: application/json' \
+    --header 'Accept-Language: en-US,en;q=0.5' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer ${BEARER_TOKEN}" \
+    --header 'Content-Type: application/json' \
+    --data-raw "{\"account_number\":\"${AWS_ACCOUNT_NUMBER}\",\"iam_role_name\":\"ab2d-spe-developer\"}" \
+    | jq --raw-output ".data")
+
+  # Set default AWS region
+
+  export AWS_DEFAULT_REGION=us-east-1
+
+  # Get temporary AWS credentials
+
+  export AWS_ACCESS_KEY_ID=$(echo $JSON_OUTPUT | jq --raw-output ".access_key")
+  export AWS_SECRET_ACCESS_KEY=$(echo $JSON_OUTPUT | jq --raw-output ".secret_access_key")
+
+  # Get AWS session token (required for temporary credentials)
+
+  export AWS_SESSION_TOKEN=$(echo $JSON_OUTPUT | jq --raw-output ".session_token")
+
+  # Verify AWS credentials
+
+  if [ -z "${AWS_ACCESS_KEY_ID}" ] \
+      || [ -z "${AWS_SECRET_ACCESS_KEY}" ] \
+      || [ -z "${AWS_SESSION_TOKEN}" ]; then
+    echo "**********************************************************************"
+    echo "ERROR: AWS credentials do not exist for the ${CMS_ENV} AWS account"
+    echo "**********************************************************************"
+    echo ""
+    exit 1
+  fi
+}
+
+#
+# Set AWS target environment
+#
+
+get_temporary_aws_credentials "${CMS_ENV_AWS_ACCOUNT_NUMBER}"
 
 #
 # AMI Generation
