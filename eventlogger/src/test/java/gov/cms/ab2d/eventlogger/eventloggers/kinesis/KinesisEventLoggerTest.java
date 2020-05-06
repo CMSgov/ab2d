5fdd9b8463ec6d222d1808aeeffafdfe3cae70b0
Laila Sharshar <sharshar@gmail.com>
2020-05-06 10:49:16 -0400

    Feature/ab2d 1459 make kinesis asynchronous (#316)
    * Make Kinesis asynchronous threads. The only constraint is that if you log both DB & Kinesis, you have to do it in a blocking way since otherwise, it's hard to get the aws id to update the record in the LogManager
    
    * Fix tests to support asynchronous
    
    * Fixed comments from PR.
    
    * Checkstyle issue
    
    Co-authored-by: Laila Sharshar <laila.sharshar@semanticbits.com>
    Co-authored-by: Andy Daykin <daykinandy@gmail.com>

--- eventlogger/src/test/java/gov/cms/ab2d/eventlogger/eventloggers/kinesis/KinesisEventLoggerTest.java
+++ eventlogger/src/test/java/gov/cms/ab2d/eventlogger/eventloggers/kinesis/KinesisEventLoggerTest.java
@@ -1,5 +1,8 @@
 package gov.cms.ab2d.eventlogger.eventloggers.kinesis;
 
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
+import com.amazonaws.services.kinesisfirehose.model.PutRecordResult;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -10,7 +13,9 @@ import gov.cms.ab2d.eventlogger.SpringBootApp;
 import gov.cms.ab2d.eventlogger.events.BeneficiarySearchEvent;
 import gov.cms.ab2d.eventlogger.events.ErrorEvent;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -18,8 +23,13 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest(classes = SpringBootApp.class)
 @Testcontainers
@@ -28,9 +38,28 @@ class KinesisEventLoggerTest {
     private static final PostgreSQLContainer postgreSQLContainer = new AB2DPostgresqlContainer();
 
     @Autowired
+    private KinesisConfig config;
+
+    @Mock
+    private AmazonKinesisFirehose firehose;
+
     private KinesisEventLogger logger;
 
-    /*
+    @BeforeEach
+    void init() {
+        logger = new KinesisEventLogger(config, firehose);
+        doReturn(generateRandomResult()).when(firehose).putRecord(any());
+    }
+
+    private PutRecordResult generateRandomResult() {
+        PutRecordResult putRecordResult = new PutRecordResult();
+        Map<String, String> values = new HashMap<>();
+        values.put("AWS_REQUEST_ID", UUID.randomUUID().toString());
+        ResponseMetadata data = new ResponseMetadata(values);
+        putRecordResult.setSdkResponseMetadata(data);
+        return putRecordResult;
+    }
+
     @Test
     public void sendEvent() {
         ErrorEvent e = new ErrorEvent();
@@ -40,10 +69,9 @@ class KinesisEventLoggerTest {
         e.setJobId("JOB123");
         e.setTimeOfEvent(OffsetDateTime.now());
         e.setUser("ME");
-        logger.log(e);
+        logger.log(e, true);
         assertNotNull(e.getAwsId());
     }
-    */
 
     @Test
     public void sendOnlyBeneSearch() throws JsonProcessingException, JSONException {
@@ -57,7 +85,7 @@ class KinesisEventLoggerTest {
         assertEquals("contract1", se.getContractNum());
         assertEquals(now, se.getTimeOfEvent());
         assertEquals(now2, se.getResponseDate());
-        String json = KinesisEventLogger.getJsonString(se);
+        String json = KinesisEventProcessor.getJsonString(se);
         System.out.println(json);
         ObjectMapper mapper = new ObjectMapper()
                 .registerModule(new ParameterNamesModule())
@@ -70,5 +98,23 @@ class KinesisEventLoggerTest {
         assertEquals("contract1", resultBene.getContractNum());
         assertEquals(now.getNano(), resultBene.getTimeOfEvent().getNano());
         assertEquals(now2.getNano(), resultBene.getResponseDate().getNano());
+    }
+
+    @Test
+    public void testThreadPool() throws InterruptedException {
+        ErrorEvent e = new ErrorEvent();
+        e.setDescription("Test Error 2");
+        e.setErrorType(ErrorEvent.ErrorType.UNAUTHORIZED_CONTRACT);
+        e.setId(1L);
+        e.setJobId("JOB123");
+        e.setTimeOfEvent(OffsetDateTime.now());
+        e.setUser("ME");
+        for (int i = 0; i < 20; i++) {
+            logger.log(e, false);
+        }
+        while (!logger.isFinished()) {
+            Thread.sleep(1000);
+        }
+        assertNotNull(e.getAwsId());
     }
 }
