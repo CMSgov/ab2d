86c806f5d2e3146f2e0fd97142d2978a5fd529a1
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-05-06 18:18:01 -0400

    add lifecycle to create before destroy and append a unique if to target group name

--- Deploy/terraform/modules/api/main.tf
+++ Deploy/terraform/modules/api/main.tf
@@ -206,11 +206,15 @@ resource "aws_lb" "api" {
 }
 
 resource "aws_lb_target_group" "api" {
-  name = "${lower(var.env)}-api-tg"
+  name = "${lower(var.env)}-api-tg-${substr(uuid(),0, 3)}"
   port = var.host_port
   protocol = "HTTPS"
   vpc_id = var.vpc_id
 
+  lifecycle {
+    create_before_destroy = true
+  }
+    
   health_check {
     healthy_threshold = 5
     unhealthy_threshold = 2
