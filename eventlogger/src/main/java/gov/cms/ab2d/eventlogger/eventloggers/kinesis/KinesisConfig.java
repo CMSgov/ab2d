a2d65223c155e8f139c3b0350ed66fd3182dccde
Laila Sharshar <sharshar@gmail.com>
2020-05-06 10:49:16 -0400

    Feature/ab2d 1459 make kinesis asynchronous (#316)
    * Make Kinesis asynchronous threads. The only constraint is that if you log both DB & Kinesis, you have to do it in a blocking way since otherwise, it's hard to get the aws id to update the record in the LogManager
    
    * Fix tests to support asynchronous
    
    * Fixed comments from PR.
    
    * Checkstyle issue
    
    Co-authored-by: Laila Sharshar <laila.sharshar@semanticbits.com>
    Co-authored-by: Andy Daykin <daykinandy@gmail.com>

--- eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/kinesis/KinesisConfig.java
+++ eventlogger/src/main/java/gov/cms/ab2d/eventlogger/eventloggers/kinesis/KinesisConfig.java
@@ -6,16 +6,35 @@ import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class KinesisConfig {
     @Value("${eventlogger.kinesis.region:US_EAST_1}")
     private String region;
 
+    @Value("${eventlogger.core.pool.size:5}")
+    private int jobCorePoolSize;
+
+    @Value("${eventlogger.max.pool.size:10}")
+    private int jobMaxPoolSize;
+
     @Bean
     AmazonKinesisFirehose getProducer() {
         return AmazonKinesisFirehoseClient.builder()
                 .withRegion(Regions.valueOf(region))
                 .build();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor kinesisLogProcessingPool() {
+        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(jobCorePoolSize);
+        taskExecutor.setMaxPoolSize(jobMaxPoolSize);
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        taskExecutor.setThreadNamePrefix("kin-");
+        return taskExecutor;
     }
 }
