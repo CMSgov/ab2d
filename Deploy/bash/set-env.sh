51e17779b62b1e283a7845ab839d9c5963e0d816
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-06-04 21:22:25 -0400

    use sourced cloudtamer api credentials function

--- Deploy/bash/set-env.sh
+++ Deploy/bash/set-env.sh
@@ -31,7 +31,17 @@ if [ "$0" != "-bash" ]; then
   exit 1
 fi
 
+#
+# Define functions
+#
+
+# Import the "get temporary AWS credentials via CloudTamer API" function
+
+source "${START_DIR}/functions/fn_get_temporary_aws_credentials_via_cloudtamer_api.sh"
+
+#
 # Ask user of chooose an environment
+#
 
 echo ""
 echo "--------------------------"
@@ -86,126 +96,7 @@ if [ $REPLY -eq 6 ]; then
 fi
 
 #
-# Acquire temporary credentials via CloudTamer API
+# Get Temporary AWS credentials via CloudTamer API
 #
 
-if [ -z $CLOUDTAMER_USER_NAME ] || [ -z $CLOUDTAMER_PASSWORD ]; then
-  echo ""
-  echo "----------------------------"
-  echo "Enter CloudTamer credentials"
-  echo "----------------------------"
-fi
-
-if [ -z $CLOUDTAMER_USER_NAME ]; then
-  echo ""
-  echo "Enter your CloudTamer user name (EUA ID):"
-  read CLOUDTAMER_USER_NAME
-fi
-
-if [ -z $CLOUDTAMER_PASSWORD ]; then
-  echo ""
-  echo "Enter your CloudTamer password:"
-  read CLOUDTAMER_PASSWORD
-fi
-
-echo ""
-echo "--------------------"
-echo "Getting bearer token"
-echo "--------------------"
-echo ""
-
-BEARER_TOKEN=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v2/token' \
-  --header 'Accept: application/json' \
-  --header 'Accept-Language: en-US,en;q=0.5' \
-  --header 'Content-Type: application/json' \
-  --data-raw "{\"username\":\"${CLOUDTAMER_USER_NAME}\",\"password\":\"${CLOUDTAMER_PASSWORD}\",\"idms\":{\"id\":2}}" \
-  | jq --raw-output ".data.access.token")
-
-BEARER_TOKEN=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v2/token' \
-  --header 'Accept: application/json' \
-  --header 'Accept-Language: en-US,en;q=0.5' \
-  --header 'Content-Type: application/json' \
-  --data-raw "{\"username\":\"${CLOUDTAMER_USER_NAME}\",\"password\":\"${CLOUDTAMER_PASSWORD}\",\"idms\":{\"id\":2}}" \
-  | jq --raw-output ".data.access.token")
-
-if [ "${BEARER_TOKEN}" == "null" ]; then
-  echo "**********************************************************************************************"
-  echo "ERROR: Retrieval of bearer token failed."
-  echo ""
-  echo "Do you need to update your "CLOUDTAMER_PASSWORD" environment variable?"
-  echo ""
-  echo "Have you been locked out due to the failed password attempts?"
-  echo ""
-  echo "If you have gotten locked out due to failed password attempts, do the following:"
-  echo "1. Go to this site:"
-  echo "   https://jiraent.cms.gov/servicedesk/customer/portal/13"
-  echo "2. Select 'CMS Cloud Access Request'"
-  echo "3. Configure page as follows:"
-  echo "   - Summary: CloudTamer & CloudVPN account password reset for {your eua id}"
-  echo "   - CMS Business Unit: OEDA"
-  echo "   - Project Name: Project 058 BCDA"
-  echo "   - Types of Access/Resets: Cisco AnyConnect and AWS Console Password Resets [not MFA]"
-  echo "   - Approvers: Stephen Walter"
-  echo "   - Description"
-  echo "     I am locked out of VPN access due to failed password attempts."
-  echo "     Can you reset my CloudTamer & CloudVPN account password?"
-  echo "     EUA: {your eua id}"
-  echo "     email: {your email}"
-  echo "     cell phone: {your cell phone number}"
-  echo "4. After you submit your ticket, call the following number and give them your ticket number."
-  echo "   888-533-4777"
-  echo "**********************************************************************************************"
-  echo ""
-  exit 1
-fi
-
-echo ""
-echo "-----------------------------"
-echo "Getting temporary credentials"
-echo "-----------------------------"
-echo ""
-
-JSON_OUTPUT=$(curl --location --request POST 'https://cloudtamer.cms.gov/api/v3/temporary-credentials' \
-  --header 'Accept: application/json' \
-  --header 'Accept-Language: en-US,en;q=0.5' \
-  --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer ${BEARER_TOKEN}" \
-  --header 'Content-Type: application/json' \
-  --data-raw "{\"account_number\":\"${AWS_ACCOUNT_NUMBER}\",\"iam_role_name\":\"ab2d-spe-developer\"}" \
-  | jq --raw-output ".data")
-
-echo ""
-
-# Set default AWS region
-
-export AWS_DEFAULT_REGION=us-east-1
-
-# Get temporary AWS credentials
-
-export AWS_ACCESS_KEY_ID=$(echo $JSON_OUTPUT | jq --raw-output ".access_key")
-export AWS_SECRET_ACCESS_KEY=$(echo $JSON_OUTPUT | jq --raw-output ".secret_access_key")
-
-# Get AWS session token (required for temporary credentials)
-
-export AWS_SESSION_TOKEN=$(echo $JSON_OUTPUT | jq --raw-output ".session_token")
-
-# Verify AWS credentials
-
-if [ -z "${AWS_ACCESS_KEY_ID}" ] \
-    || [ -z "${AWS_SECRET_ACCESS_KEY}" ] \
-    || [ -z "${AWS_SESSION_TOKEN}" ]; then
-  echo "**********************************************************************"
-  echo "ERROR: AWS credentials do not exist for the ${CMS_ENV} AWS account"
-  echo "**********************************************************************"
-  echo ""
-  return
-fi
-
-# Summary
-
-echo "*******************************************************************************"
-echo "$opt environment variables have been set."
-echo "-------------------------------------------------------------------------------"
-echo "NOTE: These credentials expire in 1 hour."
-echo "*******************************************************************************"
-echo ""
+fn_get_temporary_aws_credentials_via_cloudtamer_api "${AWS_ACCOUNT_NUMBER}" "${CMS_ENV}"
