ad59f3348d79ae069c9b3374f3d01875dc4e264b
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-06-09 20:49:11 -0400

    set ecs container memory to 120000

--- Deploy/terraform/modules/worker/main.tf
+++ Deploy/terraform/modules/worker/main.tf
@@ -74,6 +74,7 @@ resource "aws_ecs_task_definition" "worker" {
       "image": "${var.ecr_repo_aws_account}.dkr.ecr.us-east-1.amazonaws.com/ab2d_worker:${lower(var.env)}-latest",
       "essential": true,
       "cpu": 32,
+      "memory": 125000,
       "mountPoints": [
         {
 	  "containerPath": "/mnt/efs",
