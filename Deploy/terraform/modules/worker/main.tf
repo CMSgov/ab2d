13f3856b618c6fde1af24798b282827b821e4264
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-06-10 22:24:23 -0400

    calculate full cpu and 90% of memory for ecs container

--- Deploy/terraform/modules/worker/main.tf
+++ Deploy/terraform/modules/worker/main.tf
@@ -73,8 +73,8 @@ resource "aws_ecs_task_definition" "worker" {
       "name": "${lower(var.env)}-worker",
       "image": "${var.ecr_repo_aws_account}.dkr.ecr.us-east-1.amazonaws.com/ab2d_worker:${lower(var.env)}-latest",
       "essential": true,
-      "cpu": 32768,
-      "memory": 125953,
+      "cpu": ${var.ecs_task_def_cpu},
+      "memory": ${var.ecs_task_def_memory},
       "mountPoints": [
         {
 	  "containerPath": "/mnt/efs",
@@ -164,9 +164,6 @@ resource "aws_ecs_task_definition" "worker" {
 JSON
   requires_compatibilities = ["EC2"]
   network_mode = "bridge"
-  # cpu = var.ecs_task_def_cpu
-  # cpu = 10240
-  # memory = var.ecs_task_def_memory
   execution_role_arn = "arn:aws:iam::${var.aws_account_number}:role/Ab2dInstanceRole"
 }
 
