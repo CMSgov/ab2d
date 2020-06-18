d923e6c90c222ed74b834dcefbe2f45f8bf271ef
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-06-18 14:32:48 -0400

    completed and tested script to create new static website in Akamai

--- Deploy/bash/create-or-update-website-akamai.sh
+++ Deploy/bash/create-or-update-website-akamai.sh
@@ -4,6 +4,46 @@ set -e #Exit on first error
 # set -x #Be verbose
 
 #
+# Change to working directory
+#
+
+START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "${START_DIR}"
+
+#
+# Check vars are not empty before proceeding
+#
+
+echo "Check vars are not empty before proceeding..."
+if [ -z "${AKAMAI_RSYNC_DOMAIN_PARAM}" ] \
+   || [ -z "${AKAMAI_UPLOAD_DIRECTORY_PARAM}" ] \
+   || [ -z "${NETSTORAGE_SSH_KEY_PARAM}" ] \
+   || [ -z "${WEBSITE_DIRECTORY_PARAM}" ]; then
+  echo "ERROR: All parameters must be set."
+  exit 1
+fi
+
+#
+# Set variables
+#
+
+AKAMAI_RSYNC_DOMAIN="${AKAMAI_RSYNC_DOMAIN_PARAM}"
+
+AKAMAI_UPLOAD_DIRECTORY="${AKAMAI_UPLOAD_DIRECTORY_PARAM}"
+
+NETSTORAGE_SSH_KEY="${NETSTORAGE_SSH_KEY_PARAM}"
+
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+
+WEBSITE_DIRECTORY="${WEBSITE_DIRECTORY_PARAM}"
+
+#
+# Change to website parent directory
+#
+
+cd "${WEBSITE_DIRECTORY}/.."
+
+#
 # Check that there is a local "_site" directory in the current directory
 #
 
@@ -15,11 +55,32 @@ if [ ! -d "_site" ]; then
   echo "Change to the directory that has the desired '_site' directory."
   echo "***************************************************************"
   echo ""
+  exit 1
 fi
 
 #
-# Change to working directory
+# Upload a timestamped website directory backup to the target Akamai Upload Directory
 #
 
-START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "${START_DIR}"
+rsync \
+  --progress \
+  --partial \
+  --archive \
+  --verbose \
+  --rsh="ssh -v -oStrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-dss -i ${NETSTORAGE_SSH_KEY}" \
+  _site/* \
+  "sshacs@${AKAMAI_RSYNC_DOMAIN}:/${AKAMAI_UPLOAD_DIRECTORY}/_site_${TIMESTAMP}"
+
+#
+# Create or update the website directory in the target Akamai Upload Directory
+#
+
+rsync \
+  --progress \
+  --partial \
+  --archive \
+  --verbose \
+  --force \
+  --rsh="ssh -v -oStrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-dss -i ${NETSTORAGE_SSH_KEY}" \
+  _site/* \
+  "sshacs@${AKAMAI_RSYNC_DOMAIN}:/${AKAMAI_UPLOAD_DIRECTORY}/_site"
