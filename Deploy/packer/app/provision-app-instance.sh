d6f941203e2696f0ed45f08df0a16946799119a2
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-06-21 17:56:39 -0400

    Note that -l was added to the first line to make bash a login shell; this causes .bash_profile and .bashrc to be sourced which is needed for ruby items

--- Deploy/packer/app/provision-app-instance.sh
+++ Deploy/packer/app/provision-app-instance.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
+# Note: that "-l" was added to the first line to make bash a login shell
+# - this causes .bash_profile and .bashrc to be sourced which is needed for ruby items
+
 # Purpose: Shell script to provision image for app instances
 
 set -x # Be verbose
