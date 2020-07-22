3497782556171670ee61e9a2d973fe5daaa3fdfc
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-07-21 21:23:43 -0400

    greenfield migration updates

--- Deploy/packer/jenkins_master/provision-jenkins-master.sh
+++ Deploy/packer/jenkins_master/provision-jenkins-master.sh
@@ -40,6 +40,8 @@ sudo yum -y install \
   vim \
   tmux \
   yum-utils \
+  device-mapper-persistent-data \
+  lvm2 \
   python-pip \
   telnet \
   nc
@@ -49,7 +51,6 @@ sudo yum -y install wget
 sudo yum -y install mlocate
 # LSH Testing environment END
 
-# sudo pip install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -60,9 +61,11 @@ sudo service ds_agent stop
 # LSH END: Comment this out if running within sbdemo environment
 
 # Remove tty requirement for sudo in scripts and ssh calls
+
 sudo sed -i.bak '/Defaults    requiretty/d' /etc/sudoers
 
 # Prevent trendmicro from killing our docker network
+
 sudo touch /etc/use_dsa_with_iptables
 sudo chmod 755 /etc/use_dsa_with_iptables
 
@@ -80,30 +83,28 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 # Docker compose
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod 755 /usr/local/bin/docker-compose
 
 # Change docker daemon configs
+
 sudo mv /tmp/docker-daemon.json /etc/docker/daemon.json
 sudo chmod 755 /etc/docker/daemon.json
 
-#
-# LSH BEGIN
-#
-
 # Install Jenkins dependencies
+
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 
 # Get the Jenkins yum repo
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 
 # RPM import the Jenkins key
+
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 
 # Install Jenkins
-sudo yum install jenkins -y
 
-#
-# LSH END
-#
+sudo yum install jenkins -y
