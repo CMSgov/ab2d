3268f6ded20ef998a31e4194142c5e6bd53b330a
nflearl <earl@nolans.us>
2020-08-07 11:28:16 -0700

    Added a test to verify that if workers are disengaged, then jobs are not processed.

--- worker/src/main/java/gov/cms/ab2d/worker/service/WorkerDrive.java
+++ worker/src/main/java/gov/cms/ab2d/worker/service/WorkerDrive.java
@@ -1,8 +1,18 @@
 package gov.cms.ab2d.worker.service;
 
 public enum WorkerDrive {
-    IN_GEAR, // default - worker is ready to process jobs
-    NEUTRAL;  // out of gear in order to allow for something else
+    IN_GEAR("engaged"), // default - worker is ready to process jobs
+    NEUTRAL("idle");  // out of gear in order to allow for something else
+
+    private String serialValue;
+
+    WorkerDrive(String propValue) {
+        serialValue = propValue;
+    }
+
+    public String getSerialValue() {
+        return serialValue;
+    }
 
     // Defaults to being IN_GEAR.
     public static WorkerDrive fromString(String rawValue) {
