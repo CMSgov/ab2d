5f9f2655264984002db79f88ace1dd6aba61ba31
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-08-11 18:01:30 -0400

    add script to verify attestor

---
+++ Deploy/bash/verify-attestor.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+set -e #Exit on first error
+set -x #Be verbose
+
+#
+# Change to working directory
+#
+
+START_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "${START_DIR}"
+
+# Check vars are not empty before proceeding
+#
+
+echo "Check vars are not empty before proceeding..."
+if [ -z "${CMS_ENV_PARAM}" ] \
+    || [ -z "${REGION_PARAM}" ] \
+    || [ -z "${VPC_ID_PARAM}" ] \
+    || [ -z "${DATABASE_SECRET_DATETIME_PARAM}" ] \
+    || [ -z "${JOB_ID_PARAM}" ] \
+    || [ -z "${DEBUG_LEVEL_PARAM}" ] \
+    || [ -z "${CLOUD_TAMER_PARAM}" ]; then
+  echo "ERROR: All parameters must be set."
+  exit 1
+fi
+
+#
+# Set variables
+#
+
+CMS_ENV="${CMS_ENV_PARAM}"
+
+REGION="${REGION_PARAM}"
+
+VPC_ID="${VPC_ID_PARAM}"
+
+DATABASE_SECRET_DATETIME="${DATABASE_SECRET_DATETIME_PARAM}"
+
+JOB_ID="${JOB_ID_PARAM}"
+
+export DEBUG_LEVEL="${DEBUG_LEVEL_PARAM}"
+
+# Set whether CloudTamer API should be used
+
+if [ "${CLOUD_TAMER_PARAM}" != "false" ] && [ "${CLOUD_TAMER_PARAM}" != "true" ]; then
+  echo "ERROR: CLOUD_TAMER_PARAM parameter must be true or false"
+  exit 1
+else
+  CLOUD_TAMER="${CLOUD_TAMER_PARAM}"
+fi
+
+#
+# Get secrets
+#
+
+# Get database host secret
+
+DATABASE_HOST=$(./get-database-secret.py $CMS_ENV database_host $DATABASE_SECRET_DATETIME)
+
+if [ -z "${DATABASE_HOST}" ]; then
+  echo "***************************************************"
+  echo "ERROR: database host secret not found."
+  echo "***************************************************"
+  exit 1
+fi
+
+# Get database port secret
+
+DATABASE_PORT=$(./get-database-secret.py $CMS_ENV database_port $DATABASE_SECRET_DATETIME)
+
+if [ -z "${DATABASE_PORT}" ]; then
+  echo "***************************************************"
+  echo "ERROR: database port secret not found."
+  echo "***************************************************"
+  exit 1
+fi
+
+# Get database user secret
+
+DATABASE_USER=$(./get-database-secret.py $CMS_ENV database_user $DATABASE_SECRET_DATETIME)
+
+if [ -z "${DATABASE_USER}" ]; then
+  echo "**************************************"
+  echo "ERROR: database user secret not found."
+  echo "**************************************"
+  exit 1
+fi
+
+# Get database password secret
+
+DATABASE_PASSWORD=$(./get-database-secret.py $CMS_ENV database_password $DATABASE_SECRET_DATETIME)
+
+if [ -z "${DATABASE_PASSWORD}" ]; then
+  echo "******************************************"
+  echo "ERROR: database password secret not found."
+  echo "******************************************"
+  exit 1
+fi
+
+# Get database name secret
+
+DATABASE_NAME=$(./get-database-secret.py $CMS_ENV database_name $DATABASE_SECRET_DATETIME)
+
+if [ -z "${DATABASE_NAME}" ]; then
+  echo "***************************************"
+  echo "ERROR: database name secret not found."
+  echo "**************************************"
+  exit 1
+fi
+
+# Determine if customer export was successful
+
+RESULT=$(psql \
+  --host "${DATABASE_HOST}" \
+  --username "${DATABASE_USER}" \
+  --dbname "${DATABASE_NAME} \
+  --tuples-only \
+  --command "select status from job where job_uuid = '${JOB_ID}';")
+
+RESULT=$(echo $RESULT)
+
+if [ "${RESULT}" != "SUCCESSFUL" ]; then
+  echo "Export was not successful!"
+  echo "Actual result: ${RESULT}"
+  exit 1
+fi
