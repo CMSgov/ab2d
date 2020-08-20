5334d2c683417dc73f606665e42b1e2bc2215009
Andy Daykin <daykinandy@gmail.com>
2020-08-20 12:53:06 -0500

    Feature/ab2d 1907 hapi replace (#420)
    Replace the HAPI FHIR library with a custom one that will improve performance by eliminating abstractions we don't need. Should be no different in terms of functionality compared to old code, since HttpClient stays the same.

---
+++ bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDSearchImpl.java
@@ -0,0 +1,74 @@
+package gov.cms.ab2d.bfd.client;
+
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.time.OffsetDateTime;
+
+@Component
+@Slf4j
+public class BFDSearchImpl implements BFDSearch {
+
+    private final HttpClient httpClient;
+
+    private final IParser parser;
+
+    private final Environment environment;
+
+    @Value("${bfd.serverBaseUrl}")
+    private String serverBaseUrl;
+
+    public BFDSearchImpl(HttpClient httpClient, IParser iParser, Environment environment) {
+        this.httpClient = httpClient;
+        this.parser = iParser;
+        this.environment = environment;
+    }
+
+    @Override
+    public Bundle searchEOB(String patientId, OffsetDateTime since, int pageSize) throws IOException {
+        StringBuilder url = new StringBuilder(serverBaseUrl + "ExplanationOfBenefit?patient=" + patientId + "&excludeSAMHSA=true");
+
+        if (since != null) {
+            url.append("&_lastUpdated=ge").append(since);
+        }
+
+        if (pageSize > 0) {
+            url.append("&_count=").append(pageSize);
+        }
+
+        HttpGet request = new HttpGet(url.toString());
+        // No active profiles means use JSON
+        if (environment.getActiveProfiles().length == 0) {
+            request.addHeader("Accept", "application/fhir+json;q=1.0, application/json+fhir;q=0.9");
+        }
+
+        request.addHeader(HttpHeaders.ACCEPT, "gzip");
+        request.addHeader(HttpHeaders.ACCEPT_CHARSET, "utf-8");
+
+        try (CloseableHttpResponse response = (CloseableHttpResponse) httpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            if (status >= HttpStatus.SC_OK && status < HttpStatus.SC_MULTIPLE_CHOICES) {
+                try (InputStream instream = response.getEntity().getContent()) {
+                    return parser.parseResource(Bundle.class, instream);
+                }
+            } else if (status == HttpStatus.SC_NOT_FOUND) {
+                throw new ResourceNotFoundException("Patient " + patientId + " was not found");
+            } else {
+                throw new RuntimeException("Server error occurred");
+            }
+        }
+    }
+}
