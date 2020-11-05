9ee12f5ee4af0cac8e9f66b91e93b50fa0fbfc57
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-11-05 11:24:20 -0500

    query to list jobs in descending order

---
+++ Deploy/sql/list-jobs-in-descending-order.sql
@@ -0,0 +1,7 @@
+SELECT created_at, completed_at, org_name, status, progress, user_account_id
+FROM job
+INNER JOIN user_account
+ON job.user_account_id = user_account.id
+INNER JOIN sponsor
+ON user_account.sponsor_id = sponsor.id
+ORDER BY 1 DESC;
