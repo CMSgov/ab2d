37cd6d60f7d5764b3b98ae2db6459027fe0dc2b2
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-12-01 21:11:21 -0500

    add script to make db changes related to mapping credentials directly to contract

---
+++ common/src/main/resources/db/changelog/v001/ab2d-2701-map-credentials-directly-to-contracts.sql
@@ -0,0 +1,83 @@
+-- Set notifications to 'warning or greater
+
+SET client_min_messages TO WARNING;
+
+-- Drop uc_user_account_email constraint on user_account table
+
+ALTER TABLE user_account DROP CONSTRAINT IF EXISTS uc_user_account_email;
+
+-- Add contract_id to user_account table
+
+ALTER TABLE user_account ADD COLUMN IF NOT EXISTS contract_id bigint;
+
+-- Update existing user_account records with their correct sponsor id
+
+UPDATE public.user_account h
+SET sponsor_id = a.id
+FROM public.sponsor a
+INNER JOIN public.contract d
+ON a.id = d.sponsor_id
+INNER JOIN public.user_account e
+ON a.id = e.sponsor_id OR a.parent_id = e.sponsor_id
+WHERE h.id = e.id;
+
+-- Update existing user_account records with their correct contract_id
+
+UPDATE public.user_account h
+SET contract_id = d.id
+FROM public.sponsor a
+INNER JOIN public.contract d
+ON a.id = d.sponsor_id
+INNER JOIN public.user_account e
+ON a.id = e.sponsor_id
+WHERE h.id = e.id;
+
+-- Delete user role records where user accounts have no contract record
+
+DELETE FROM user_role c
+WHERE c.user_account_id IN (
+  SELECT b.user_account_id
+  FROM user_role b
+  INNER JOIN user_account a
+  ON b.user_account_id = a.id
+  WHERE sponsor_id NOT IN (
+    SELECT sponsor_id
+    FROM contract
+  )
+);
+
+-- Delete user accounts that have no contract record
+
+DELETE FROM user_account
+WHERE sponsor_id NOT IN (
+  SELECT sponsor_id
+  FROM contract
+);
+
+-- Add 'not null' constraint to contract_id column of the user_account table
+
+ALTER TABLE user_account
+ALTER COLUMN contract_id SET NOT NULL;
+
+-- Add foreign key to the user_account table that directly references the contract associated with the user account mapping
+
+ALTER TABLE user_account
+ADD CONSTRAINT "fk_user_account_to_contract"
+FOREIGN KEY (contract_id)
+REFERENCES contract (id);
+
+--------
+-- Audit
+--------
+
+-- S2893 OK
+-- S3285 OK
+-- S3875 MISSING
+-- S4501 OK
+-- S5540 OK
+-- S5596 NEW
+-- S5609 NEW
+-- S6946 OK
+-- S7126 OK
+-- S8067 OK
+-- S9701 OK
