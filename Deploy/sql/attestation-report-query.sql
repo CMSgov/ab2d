144a98b2c07026459ab7e094c9c5056830f0bab2
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-12-01 21:09:48 -0500

    eliminate or condition based on db changes

--- Deploy/sql/attestation-report-query.sql
+++ Deploy/sql/attestation-report-query.sql
@@ -14,7 +14,7 @@ FROM public.sponsor a
 INNER JOIN public.contract d
 ON a.id = d.sponsor_id
 INNER JOIN public.user_account e
-ON a.id = e.sponsor_id OR a.parent_id = e.sponsor_id
+ON a.id = e.sponsor_id
 WHERE d.attested_on is not null
 AND d.contract_number NOT LIKE 'Z%'
 ORDER BY d.contract_number, e.username;
