3c265e1235d0760f4d9d172600b07d2d255b07d1
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2020-12-01 21:08:52 -0500

    sql scripts for testing and reconciliation

---
+++ Deploy/sql/list-jobs-in-descending-order.sql
@@ -0,0 +1,14 @@
+SELECT
+  f.created_at,
+  f.completed_at,
+  a.org_name,
+  f.status,
+  f.progress,
+  f.job_uuid,
+  f.user_account_id
+FROM job f
+INNER JOIN user_account e
+ON f.user_account_id = e.id
+INNER JOIN sponsor a
+ON e.sponsor_id = a.id
+ORDER BY 1 DESC;
