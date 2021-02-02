6900e00ef87cf39cd59887f4f4d88a757818d7a5
sharshar-sb <laila.sharshar@semanticbits.com>
2021-02-02 17:37:44 -0500

    DB fixes, moved file

--- common/src/main/resources/db/changelog/v2021/add_fhir_version.sql
+++ common/src/main/resources/db/changelog/v2021/add_fhir_version.sql
@@ -4,4 +4,5 @@
 --changeset lsharshar:add_fhir_version failOnError:true
 
 ALTER TABLE job ADD COLUMN fhir_version varchar(255);
-UPDATE job SET fhir_version = 'R3' WHERE fhir_version IS NULL;
+UPDATE job SET fhir_version = 'STU3' WHERE fhir_version IS NULL;
+ALTER TABLE job ALTER COLUMN fhir_version SET NOT NULL;
