076d5c048a8601ab2209fcbcfcc02308963ff9d0
Lonnie Hanekamp <lonnie.hanekamp@semanticbits.com>
2021-02-23 19:35:24 -0500

    eliminate unused components

--- Deploy/packer/app/node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systemctl start docker
-sleep 60
-wget -O vars.sh $MASTER_IP:777
-eval $(cat vars.sh)
-kubeadm join $MASTER_IP:6443 --token $TOKEN --discovery-token-ca-cert-hash $HASH
