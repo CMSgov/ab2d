79e4d31e92dacd71ac73bf5bad45cc078f46a10b
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-03-04 11:58:20 -0600

    change to use client_id column

--- common/src/main/resources/db/changelog/v2021/remove_current_admin.sql
+++ common/src/main/resources/db/changelog/v2021/remove_current_admin.sql
@@ -4,7 +4,7 @@
 --changeset wnyffenegger:remove_current_admin failOnError:true
 
 DELETE FROM user_role
-WHERE user_account_id = (SELECT id FROM user_account WHERE username = '0oa32rfir2xLtx7s2297');
+WHERE user_account_id = (SELECT id FROM user_account WHERE client_id = '0oa32rfir2xLtx7s2297');
 
 DELETE FROM user_account
-WHERE username = '0oa32rfir2xLtx7s2297';
+WHERE client_id = '0oa32rfir2xLtx7s2297';
