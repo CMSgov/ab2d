70a262a9d020a22b4031c6bac7a81f9742ab9632
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-06-16 10:11:30 -0500

    revert job handler changes

--- worker/src/main/java/gov/cms/ab2d/worker/config/JobHandler.java
+++ worker/src/main/java/gov/cms/ab2d/worker/config/JobHandler.java
@@ -1,5 +1,7 @@
 package gov.cms.ab2d.worker.config;
 
+import gov.cms.ab2d.common.model.Job;
+import gov.cms.ab2d.common.model.JobStatus;
 import gov.cms.ab2d.common.service.FeatureEngagement;
 import gov.cms.ab2d.common.service.ResourceNotFoundException;
 import gov.cms.ab2d.worker.service.WorkerService;
@@ -50,6 +52,10 @@ public class JobHandler implements MessageHandler {
 
         final List<Map<String, Object>> payload = (List<Map<String, Object>>) message.getPayload();
 
+        if (!payload.isEmpty()) {
+            log.info("iterating over {} submitted jobs to attempt to find one to start", payload.size());
+        }
+
         for (Map<String, Object> submittedJob : payload) {
 
             final String jobId = getJobId(submittedJob);
@@ -66,7 +72,11 @@ public class JobHandler implements MessageHandler {
 
                     // Attempt to start (mark an eob job as in progress) an eob job.
                     // A job may not be started if the workers are busy or if coverage metadata needs an update.
-                    workerService.process(jobId);
+                    Job job = workerService.process(jobId);
+                    if (job.getStatus() == JobStatus.IN_PROGRESS) {
+                        log.info("{} job has been started so exiting loop", jobId);
+                        break;
+                    }
 
                 } catch (ResourceNotFoundException rnfe) {
                     throw new MessagingException("could not find job in database for " + jobId + " job uuid", rnfe);
