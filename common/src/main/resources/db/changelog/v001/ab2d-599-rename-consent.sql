5688e2586aed20654fad1d7b6a1b700007859279
Leonardo Carreon <leonardo.carreon@semanticbits.com>
2021-08-05 10:06:50 -0500

    back out sql changes for hicn

--- common/src/main/resources/db/changelog/v001/ab2d-599-rename-consent.sql
+++ common/src/main/resources/db/changelog/v001/ab2d-599-rename-consent.sql
@@ -5,6 +5,7 @@
 --changeset spathiyil:ab2d-599-rename-consent-table-to-opt_out failOnError:true
 ALTER TABLE consent RENAME TO opt_out;
 ALTER TABLE opt_out RENAME CONSTRAINT "pk_consent" TO "pk_opt_out";
+ALTER INDEX "ix_consent_hicn" RENAME TO "ix_opt_out_hicn";
 
 --rollback  ALTER TABLE opt_out RENAME TO consent;
 --rollback  ALTER TABLE consent RENAME CONSTRAINT "pk_opt_out" TO "pk_consent";
