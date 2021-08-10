e045cdcee4d3b8ba7f7244c2a79a94410e13768c
sb-benohe <71290292+sb-benohe@users.noreply.github.com>
2021-08-10 11:19:54 -0400

    Revert "Delete ab2d_1167_add_column_to_opt_out_table.sql"
    This reverts commit 6d6a30a18ad0e69bb37e7687f7e70aef77993fbc.

---
+++ common/src/main/resources/db/changelog/v001/ab2d_1167_add_column_to_opt_out_table.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+--  -------------------------------------------------------------------------------------------------------------------
+
+
+--changeset spathiyil:ab2d_1167_add_column_to_opt_out_table failOnError:true
+ALTER TABLE opt_out
+ ADD COLUMN filename        VARCHAR(255)    NOT NULL;
+
+--rollback  ALTER TABLE opt_out DROP COLUMN filename;
+
+
+--changeset spathiyil:ab2d_1167_add_indexes_on_opt_out_table failOnError:true
+CREATE INDEX "ix_opt_out_ccw_id"        ON opt_out (ccw_id);
+CREATE INDEX "ix_opt_out_ccw_id_hicn"   ON opt_out (ccw_id, hicn);
+
+--rollback DROP INDEX "ix_opt_out_ccw_id";
+--rollback DROP INDEX "ix_opt_out_ccw_id_hicn";	
