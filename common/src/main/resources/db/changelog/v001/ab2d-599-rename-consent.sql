7fe4c29325e4ff354972bbf0cbecb9d487907d09
Leonardo Carreon <leonardo.carreon@semanticbits.com>
2021-08-11 10:54:17 -0500

    re-upload files from master

--- common/src/main/resources/db/changelog/v001/ab2d-599-rename-consent.sql
+++ common/src/main/resources/db/changelog/v001/ab2d-599-rename-consent.sql
@@ -11,3 +11,5 @@ ALTER INDEX "ix_consent_hicn" RENAME TO "ix_opt_out_hicn";
 --rollback  ALTER TABLE consent RENAME CONSTRAINT "pk_opt_out" TO "pk_consent";
 --rollback  ALTER INDEX "ix_opt_out_hicn" RENAME TO "ix_consent_hicn";
 --  -------------------------------------------------------------------------------------------------------------------
+
+
