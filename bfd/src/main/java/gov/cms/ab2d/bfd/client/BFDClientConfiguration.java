0fa3fcda81a8776c429614928ee203e4b9df8c8d
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-09-21 15:47:48 -0500

    unused imports

--- bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
+++ bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
@@ -10,11 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.util.ResourceUtils;
 
 import javax.net.ssl.SSLContext;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
