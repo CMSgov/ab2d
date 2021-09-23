a38979f3f0eca926b5cabf4e9e7edb9cff2c8df5
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-09-23 13:31:27 -0500

    make sure started_by is not null

--- common/src/main/resources/db/changelog/v2021/add_started_by_column_to_job.sql
+++ common/src/main/resources/db/changelog/v2021/add_started_by_column_to_job.sql
@@ -3,6 +3,6 @@
 
 --changeset wnyffenegger:add_started_by_column_to_job failOnError:true
 
-ALTER TABLE job ADD COLUMN started_by VARCHAR(32) DEFAULT 'PDP';
+ALTER TABLE job ADD COLUMN started_by VARCHAR(32) NOT NULL DEFAULT 'PDP';
 
 --rollback  ALTER TABLE job DROP COLUMN manual
