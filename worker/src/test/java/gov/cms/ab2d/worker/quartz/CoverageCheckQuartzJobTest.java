fe014bdf6dfcc13b9f7303bc5932eabc4aef5ffa
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-10-06 09:24:00 -0500

    provide tests for worker quartz jobs

---
+++ worker/src/test/java/gov/cms/ab2d/worker/quartz/CoverageCheckQuartzJobTest.java
@@ -0,0 +1,114 @@
+package gov.cms.ab2d.worker.quartz;
+
+import gov.cms.ab2d.common.service.PropertiesService;
+import gov.cms.ab2d.eventlogger.LogManager;
+import gov.cms.ab2d.worker.processor.coverage.CoverageDriver;
+import gov.cms.ab2d.worker.processor.coverage.CoverageVerificationException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.quartz.JobExecutionException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CoverageCheckQuartzJobTest {
+
+    @Mock
+    private PropertiesService propertiesService;
+
+    @Mock
+    private CoverageDriver coverageDriver;
+
+    @Mock
+    private LogManager logManager;
+
+
+    @AfterEach
+    void tearDown() {
+        reset(propertiesService);
+        reset(coverageDriver);
+        reset(logManager);
+    }
+
+    @DisplayName("Skip job when in maintenance mode")
+    @Test
+    void whenMaintenanceMode_skipJob() {
+
+        when(propertiesService.isInMaintenanceMode()).thenReturn(true);
+
+        CoverageCheckQuartzJob job = new CoverageCheckQuartzJob(logManager, coverageDriver, propertiesService);
+
+        try {
+            job.executeInternal(null);
+        } catch (JobExecutionException executionException) {
+            fail("job should not execute in maintenance mode");
+        }
+
+        verify(propertiesService, times(1)).isInMaintenanceMode();
+    }
+
+    @DisplayName("Execute normally when not in maintenance mode")
+    @Test
+    void whenNotMaintenanceMode_executeJob() {
+
+        when(propertiesService.isInMaintenanceMode()).thenReturn(false);
+
+        CoverageCheckQuartzJob job = new CoverageCheckQuartzJob(logManager, coverageDriver, propertiesService);
+
+        try {
+            job.executeInternal(null);
+        } catch (JobExecutionException executionException) {
+            fail("job should execute successfully if no exception thrown");
+        }
+
+        verify(propertiesService, times(1)).isInMaintenanceMode();
+        verify(coverageDriver, times(1)).verifyCoverage();
+    }
+
+    @DisplayName("Report issues when verification detects issues")
+    @Test
+    void whenVerificationException_alertWithIssues() {
+
+        when(propertiesService.isInMaintenanceMode()).thenReturn(false);
+
+        doThrow(new CoverageVerificationException("testing123", List.of("alertalert")))
+                .when(coverageDriver).verifyCoverage();
+
+        CoverageCheckQuartzJob job = new CoverageCheckQuartzJob(logManager, coverageDriver, propertiesService);
+
+        JobExecutionException exception = assertThrows(JobExecutionException.class, () -> job.executeInternal(null));
+        assertTrue(exception.getMessage().contains("testing123"));
+
+        verify(propertiesService, times(1)).isInMaintenanceMode();
+        verify(coverageDriver, times(1)).verifyCoverage();
+        verify(logManager, times(1)).alert(
+                argThat(alert -> alert.contains("alertalert") && alert.contains("Coverage verification failed")), anyList());
+    }
+
+    @DisplayName("Report failure to run verification as an alert")
+    @Test
+    void whenVerificationFailsToRun_alert() {
+
+        when(propertiesService.isInMaintenanceMode()).thenReturn(false);
+
+        doThrow(new RuntimeException("testing123"))
+                .when(coverageDriver).verifyCoverage();
+
+        CoverageCheckQuartzJob job = new CoverageCheckQuartzJob(logManager, coverageDriver, propertiesService);
+
+        JobExecutionException exception = assertThrows(JobExecutionException.class, () -> job.executeInternal(null));
+        assertTrue(exception.getMessage().contains("testing123"));
+
+        verify(propertiesService, times(1)).isInMaintenanceMode();
+        verify(coverageDriver, times(1)).verifyCoverage();
+        verify(logManager, times(1)).alert(contains("could not verify coverage due"), anyList());
+    }
+
+}
--- worker/src/test/java/gov/cms/ab2d/worker/quartz/CoveragePeriodQuartzJobTest.java
+++ worker/src/test/java/gov/cms/ab2d/worker/quartz/CoveragePeriodQuartzJobTest.java
@@ -3,9 +3,16 @@ package gov.cms.ab2d.worker.quartz;
 import gov.cms.ab2d.common.model.Properties;
 import gov.cms.ab2d.common.service.PropertiesService;
 import gov.cms.ab2d.common.util.Constants;
+import gov.cms.ab2d.eventlogger.LogManager;
+import gov.cms.ab2d.worker.processor.coverage.CoverageDriver;
 import gov.cms.ab2d.worker.processor.coverage.CoverageDriverStub;
+import gov.cms.ab2d.worker.processor.coverage.CoverageProcessor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.quartz.JobExecutionException;
 
 import java.time.DayOfWeek;
@@ -15,16 +22,29 @@ import static gov.cms.ab2d.common.util.Constants.COVERAGE_SEARCH_OVERRIDE;
 import static gov.cms.ab2d.common.util.DateUtil.AB2D_ZONE;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CoveragePeriodQuartzJobTest {
+
+    @Mock
+    private PropertiesService propertiesService;
+
+    @Mock
+    private CoverageDriver coverageDriverMock;
+
+    @Mock
+    private LogManager logManager;
+
+    @AfterEach
+    void tearDown() {
+        reset(propertiesService);
+        reset(logManager);
+    }
 
     @DisplayName("Disengaging coverage search works")
     @Test
     void disengageSearchWorks() {
-
-        PropertiesService propertiesService = mock(PropertiesService.class);
 
         when(propertiesService.getPropertiesByKey(eq(Constants.COVERAGE_SEARCH_DISCOVERY))).thenAnswer((arg) -> {
             Properties engaged = new Properties();
@@ -47,15 +67,15 @@ class CoveragePeriodQuartzJobTest {
             return override;
         });
 
-        CoverageDriverStub coverageProcessor = new CoverageDriverStub();
+        CoverageDriverStub coverageDriverStub = new CoverageDriverStub();
 
-        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageProcessor, propertiesService);
+        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageDriverStub, propertiesService, logManager);
 
         try {
             quartzJob.executeInternal(null);
 
-            assertFalse(coverageProcessor.discoveryCalled);
-            assertFalse(coverageProcessor.queueingCalled);
+            assertFalse(coverageDriverStub.discoveryCalled);
+            assertFalse(coverageDriverStub.queueingCalled);
 
         } catch (JobExecutionException jobExecutionException) {
             // Exception never thrown by stubbed method
@@ -89,15 +109,15 @@ class CoveragePeriodQuartzJobTest {
             return override;
         });
 
-        CoverageDriverStub coverageProcessor = new CoverageDriverStub();
-        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageProcessor, propertiesService);
+        CoverageDriverStub coverageDriverStub = new CoverageDriverStub();
+        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageDriverStub, propertiesService, logManager);
 
         try {
 
             quartzJob.executeInternal(null);
 
-            assertTrue(coverageProcessor.discoveryCalled);
-            assertTrue(coverageProcessor.queueingCalled);
+            assertTrue(coverageDriverStub.discoveryCalled);
+            assertTrue(coverageDriverStub.queueingCalled);
 
         } catch (JobExecutionException jobExecutionException) {
             fail("could not execute normally", jobExecutionException);
@@ -131,26 +151,65 @@ class CoveragePeriodQuartzJobTest {
             return override;
         });
 
-        CoverageDriverStub coverageProcessor = new CoverageDriverStub();
-        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageProcessor, propertiesService);
+        CoverageDriverStub coverageDriverStub = new CoverageDriverStub();
+        CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageDriverStub, propertiesService, logManager);
 
         try {
 
             quartzJob.executeInternal(null);
 
-            assertTrue(coverageProcessor.discoveryCalled);
+            assertTrue(coverageDriverStub.discoveryCalled);
 
 
             OffsetDateTime date = OffsetDateTime.now(AB2D_ZONE);
 
             if (date.getDayOfWeek() == DayOfWeek.TUESDAY && date.getHour() == 0) {
-                assertTrue(coverageProcessor.queueingCalled);
+                assertTrue(coverageDriverStub.queueingCalled);
             } else {
-                assertFalse(coverageProcessor.queueingCalled);
+                assertFalse(coverageDriverStub.queueingCalled);
             }
 
         } catch (JobExecutionException jobExecutionException) {
             fail("could not execute normally", jobExecutionException);
+        }
+    }
+
+
+    @DisplayName("Failing searches trigger alerts")
+    @Test
+    void failingSearchesTriggerAlerts() {
+
+        PropertiesService propertiesService = mock(PropertiesService.class);
+
+        when(propertiesService.getPropertiesByKey(eq(Constants.COVERAGE_SEARCH_DISCOVERY))).thenAnswer((arg) -> {
+            Properties engaged = new Properties();
+            engaged.setKey(Constants.COVERAGE_SEARCH_DISCOVERY);
+            engaged.setValue("engaged");
+            return engaged;
+        });
+
+        when(propertiesService.getPropertiesByKey(eq(COVERAGE_SEARCH_OVERRIDE))).thenAnswer((arg) -> {
+            Properties override = new Properties();
+            override.setKey(COVERAGE_SEARCH_OVERRIDE);
+            override.setValue("true");
+            return override;
+        });
+
+
+        try {
+            doThrow(new RuntimeException("testing123")).when(coverageDriverMock).discoverCoveragePeriods();
+            CoveragePeriodQuartzJob quartzJob = new CoveragePeriodQuartzJob(coverageDriverMock, propertiesService, logManager);
+
+            JobExecutionException exception =
+                    assertThrows(JobExecutionException.class, () -> quartzJob.executeInternal(null));
+
+            assertTrue(exception.getMessage().contains("testing123"));
+
+            verify(logManager, times(1))
+                    .alert(eq("coverage period updates could not be conducted"), anyList());
+
+        } catch (Exception ex) {
+            fail("could not execute test due to interruption", ex);
         }
     }
 }
