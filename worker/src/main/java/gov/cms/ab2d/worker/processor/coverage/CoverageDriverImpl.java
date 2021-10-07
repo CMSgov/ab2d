5b50206fe141b75827c3a45cc97aa83326a54b62
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-10-07 12:40:06 -0500

    ignore all synthetic contracts in verification

--- worker/src/main/java/gov/cms/ab2d/worker/processor/coverage/CoverageDriverImpl.java
+++ worker/src/main/java/gov/cms/ab2d/worker/processor/coverage/CoverageDriverImpl.java
@@ -505,6 +505,7 @@ public class CoverageDriverImpl implements CoverageDriver {
         List<String> issues = new ArrayList<>();
 
         List<Contract> enabledContracts = pdpClientService.getAllEnabledContracts().stream()
+                .filter(contract -> !contract.getContractNumber().startsWith("Z"))
                 .filter(contract -> contractNotBeingUpdated(issues, contract))
                 .collect(toList());
 
