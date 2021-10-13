b34ec8bea9d1f649260c8be39de1a9778c0e3339
wnyffenegger <will.nyffenegger@semanticbits.com>
2021-10-13 15:44:17 -0500

    split claims processor into request and results
    Claims processor mixed logic for requesting claims from BFD and performing filtering of the resultant claims. Splitting claims into two classes and encapsulate the filtering logic.
    
    This allows us to test the filtering alone without mocking or exposing other functionality. The filtering object can be reused within the claims request for subsequent bundles.
    
    Simplifying the claims processor exposed a mistake made where subsequent requests for pages of claims from BFD would be made without a bulk id job which would interfere with BFD logging.

--- worker/src/main/java/gov/cms/ab2d/worker/processor/PatientClaimsProcessorImpl.java
+++ worker/src/main/java/gov/cms/ab2d/worker/processor/PatientClaimsProcessorImpl.java
@@ -1,6 +1,5 @@
 package gov.cms.ab2d.worker.processor;
 
-import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Token;
 import com.newrelic.api.agent.Trace;
 import gov.cms.ab2d.bfd.client.BFDClient;
@@ -8,12 +7,10 @@ import gov.cms.ab2d.common.model.CoverageSummary;
 import gov.cms.ab2d.eventlogger.LogManager;
 import gov.cms.ab2d.eventlogger.events.BeneficiarySearchEvent;
 import gov.cms.ab2d.fhir.BundleUtils;
-import gov.cms.ab2d.fhir.EobUtils;
-import gov.cms.ab2d.filter.ExplanationOfBenefitTrimmer;
-import gov.cms.ab2d.common.util.FilterOutByDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hl7.fhir.instance.model.api.*;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
@@ -21,10 +18,12 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.*;
-import java.util.*;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import static gov.cms.ab2d.common.util.Constants.SINCE_EARLIEST_DATE;
 import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
@@ -39,16 +38,11 @@ public class PatientClaimsProcessorImpl implements PatientClaimsProcessor {
 
     @Value("${claims.skipBillablePeriodCheck}")
     private boolean skipBillablePeriodCheck;
+
     @Value("${bfd.earliest.data.date:01/01/2020}")
-    private String startDate;
-    @Value("${bfd.earliest.data.date.special.contracts}")
-    private String startDateSpecialContracts;
-    @Value("#{'${bfd.special.contracts}'.split(',')}")
-    private List<String> specialContracts;
+    private String earliestDataDate;
 
     private static final OffsetDateTime START_CHECK = OffsetDateTime.parse(SINCE_EARLIEST_DATE, ISO_DATE_TIME);
-
-    private static final String EOB_REQUEST_EVENT = "EobBundleRequests";
 
     /**
      * Process the retrieval of patient explanation of benefit objects and write them
@@ -72,11 +66,15 @@ public class PatientClaimsProcessorImpl implements PatientClaimsProcessor {
     }
 
     private List<IBaseResource> getEobBundleResources(PatientClaimsRequest request) {
+
+        OffsetDateTime requestStartTime = OffsetDateTime.now();
+
+        Date earliestDate = getEarliestDataDate();
+        PatientClaimsCollector collector = new PatientClaimsCollector(request, skipBillablePeriodCheck, earliestDate);
+
         CoverageSummary patient = request.getCoverageSummary();
         long beneficiaryId = patient.getIdentifiers().getBeneficiaryId();
-        OffsetDateTime attTime = request.getAttTime();
 
-        OffsetDateTime start = OffsetDateTime.now();
         IBaseBundle eobBundle;
 
         OffsetDateTime sinceTime = getSinceTime(request);
@@ -85,37 +83,23 @@ public class PatientClaimsProcessorImpl implements PatientClaimsProcessor {
 
             BFDClient.BFD_BULK_JOB_ID.set(request.getJob());
             eobBundle = bfdClient.requestEOBFromServer(request.getVersion(), patient.getIdentifiers().getBeneficiaryId(), sinceTime);
-            logSuccessful(request, beneficiaryId, start);
+            collector.filterAndAddEntries(eobBundle);
 
+            while (BundleUtils.getNextLink(eobBundle) != null) {
+                eobBundle = bfdClient.requestNextBundleFromServer(request.getVersion(), eobBundle);
+                collector.filterAndAddEntries(eobBundle);
+            }
+
+            logSuccessful(request, beneficiaryId, requestStartTime);
+            collector.logBundleEvent(sinceTime);
+
+            return collector.getEobs();
         } catch (Exception ex) {
-            logError(request, beneficiaryId, start, ex);
+            logError(request, beneficiaryId, requestStartTime, ex);
             throw ex;
         } finally {
             BFDClient.BFD_BULK_JOB_ID.remove();
         }
-
-        final List<IBaseBackboneElement> entries = BundleUtils.getEntries(eobBundle);
-        int bundles = 1;
-
-        final List<IBaseResource> resources = extractResources(request.getContractNum(), entries, patient.getDateRanges(), attTime);
-
-        while (BundleUtils.getNextLink(eobBundle) != null) {
-            eobBundle = bfdClient.requestNextBundleFromServer(request.getVersion(), eobBundle);
-
-            final List<IBaseBackboneElement> nextEntries = BundleUtils.getEntries(eobBundle);
-            bundles += 1;
-
-            resources.addAll(extractResources(request.getContractNum(), nextEntries, patient.getDateRanges(), attTime));
-        }
-
-        // Record details of EOB request for analysis
-        Map<String, Object> bundleEvent = bundleEvent(request, bundles,
-                BundleUtils.getTotal(eobBundle), entries.size(), sinceTime);
-        NewRelic.getAgent().getInsights().recordCustomEvent(EOB_REQUEST_EVENT, bundleEvent);
-
-        log.debug("Bundle - Total: {} - Entries: {} ", BundleUtils.getTotal(eobBundle), entries.size());
-
-        return resources;
     }
 
     /**
@@ -162,70 +146,16 @@ public class PatientClaimsProcessorImpl implements PatientClaimsProcessor {
                         "ERROR: " + ex.getMessage()));
     }
 
-    private Date getStartDate(String contract) {
+    private Date getEarliestDataDate() {
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 
-        String dateToUse = startDate;
-        if (isContractSpecial(contract)) {
-            dateToUse = startDateSpecialContracts;
-        }
         Date date;
         try {
-            date = sdf.parse(dateToUse);
+            date = sdf.parse(earliestDataDate);
         } catch (ParseException e) {
             LocalDateTime d = LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0);
             date = new Date(d.toInstant(ZoneOffset.UTC).toEpochMilli());
         }
         return date;
-    }
-
-    private boolean isContractSpecial(String contract) {
-        return this.specialContracts != null && !this.specialContracts.isEmpty() && specialContracts.contains(contract);
-    }
-
-    @Trace
-    List<IBaseResource> extractResources(String contractNum,
-                                     List<IBaseBackboneElement> entries,
-                                     final List<FilterOutByDate.DateRange> dateRanges,
-                                     OffsetDateTime attTime) {
-        if (attTime == null) {
-            return new ArrayList<>();
-        }
-        long epochMilli = attTime.toInstant().toEpochMilli();
-        Date attDate = new Date(epochMilli);
-        final Date earliestDate = getStartDate(contractNum);
-        return BundleUtils.getEobResources(entries).stream()
-                // Filter by date
-                .filter(resource -> skipBillablePeriodCheck || FilterOutByDate.valid(resource, attDate, earliestDate, dateRanges))
-                // filter it
-                .map(resource -> ExplanationOfBenefitTrimmer.getBenefit(resource))
-                // Remove any empty values
-                .filter(Objects::nonNull)
-                // Remove Plan D
-                .filter(resource -> !EobUtils.isPartD(resource))
-                // compile the list
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Create custom NewRelic event
-     * @param request patient claims request
-     * @param bundles number of bundles pulled from BFD
-     * @param rawEobs number of eobs returned before filtering
-     * @param filteredEobs number of eobs returned after filtering
-     * @param since since date used if in use
-     * @return custom NewRelic event as a map
-     */
-    private Map<String, Object> bundleEvent(PatientClaimsRequest request, int bundles, int rawEobs, int filteredEobs, OffsetDateTime since) {
-        Map<String, Object> event = new HashMap<>();
-        event.put("organization", request.getOrganization());
-        event.put("contract", request.getContractNum());
-        event.put("since", since);
-        event.put("jobid", request.getJob());
-        event.put("bundles", bundles);
-        event.put("raweobs", rawEobs);
-        event.put("eobs", filteredEobs);
-
-        return event;
     }
 }
