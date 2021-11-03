4c8d0659256d11fe0468791b455f0cb2e15b66a6
earl.nolan <earl.nolan@semanticbits.com>
2021-11-03 16:22:48 -0700

    AB2D-3825: Added logging in order to verify if the rewritten method is even called.

--- worker/src/main/java/gov/cms/ab2d/worker/service/JobChannelServiceImpl.java
+++ worker/src/main/java/gov/cms/ab2d/worker/service/JobChannelServiceImpl.java
@@ -5,14 +5,18 @@ import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import gov.cms.ab2d.worker.processor.JobMeasure;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JobChannelServiceImpl implements JobChannelService {
 
     @Override
     public void sendUpdate(String jobUuid, JobMeasure measure, long value) {
+        log.info("Sending message to SQS from JobChannelService");
+
         final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
         String queueUrl = sqs.getQueueUrl("ab2d-job-tracking").getQueueUrl();
 
@@ -21,5 +25,6 @@ public class JobChannelServiceImpl implements JobChannelService {
                 .withMessageBody("hello world")
                 .withDelaySeconds(1);
         sqs.sendMessage(sendMessageRequest);
+        log.info("jobChannelService sendUpdate is done");
     }
 }
