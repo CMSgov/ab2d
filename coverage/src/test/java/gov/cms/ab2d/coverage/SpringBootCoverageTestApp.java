0ef1c582ab68d7dc3987733160b48d0d112bfe98
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-01-13 11:11:39 -0800

    Remove test code

--- coverage/src/test/java/gov/cms/ab2d/coverage/SpringBootCoverageTestApp.java
+++ coverage/src/test/java/gov/cms/ab2d/coverage/SpringBootCoverageTestApp.java
@@ -2,14 +2,13 @@ package gov.cms.ab2d.coverage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = {
         "gov.cms.ab2d.common",
         "gov.cms.ab2d.coverage",
         "gov.cms.ab2d.eventlogger"
 })
-@PropertySource("classpath:application.common.properties")
+//@PropertySource("classpath:application.common.properties")
 public class SpringBootCoverageTestApp {
 
     public static void main(String [] args) {
