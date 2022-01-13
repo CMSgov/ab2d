8e30eac209ee7f22c8693d6ea5957856c69dad29
nflearl <earl.nolan@semanticbits.com>
2022-01-13 10:17:47 -0800

    AB2D-4266 Initial checkin
    Added comments to liquibase.

--- common/src/main/resources/db/changelog/v2022/job_contract_bus_key.sql
+++ common/src/main/resources/db/changelog/v2022/job_contract_bus_key.sql
@@ -3,6 +3,7 @@
 
 --changeset enolan:contract_bus_key failOnError:true
 
+-- Create and populate the contract business key
 ALTER TABLE job ADD COLUMN contract_number VARCHAR(255);
 UPDATE job
 SET contract_number = c.contract_number
@@ -12,9 +13,10 @@ WHERE contract_id is not null and contract_id = c.id;
 UPDATE job
 SET contract_number = 'UNKNOWN'
 WHERE contract_id is null;
+-- Enforce that a saved job has a contract number
 ALTER TABLE job ALTER "contract_number" SET NOT NULL;
 
-
+-- Foreign key is no longer needed.  The business key will be used going forward
 ALTER TABLE job DROP CONSTRAINT fk_job_contract;
 ALTER TABLE job DROP COLUMN contract_id;
 
