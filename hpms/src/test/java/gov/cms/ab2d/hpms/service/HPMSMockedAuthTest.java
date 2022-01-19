4da097f07cd3ecac2388285892699655e28c9260
Keane McGough <keane.mcgough@semanticbits.com>
2022-01-19 09:42:55 -0600

    fixed failing tests

--- hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSMockedAuthTest.java
+++ hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSMockedAuthTest.java
@@ -1,21 +1,13 @@
 package gov.cms.ab2d.hpms.service;
 
-import gov.cms.ab2d.hpms.SpringBootTestApp;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.*;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
 
 import java.net.URI;
@@ -24,18 +16,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = SpringBootTestApp.class)
-@TestPropertySource(locations = "/application.hpms.properties")
-@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class HPMSMockedAuthTest {
 
-    @Autowired
-    HPMSAuthServiceImpl authService;
+    HPMSAuthServiceImpl authService = new HPMSAuthServiceImpl();
 
-    @MockBean
+    @Mock
     private WebClient.Builder webClientBuilder;
 
-    @MockBean
+    @Mock
     private WebClient mockedWebClient;
 
     @Mock
@@ -44,37 +33,42 @@ class HPMSMockedAuthTest {
     @Mock
     WebClient.RequestHeadersSpec headersSpec;
 
-
-    @Mock
-    MediaType mediaType;
-
     @Mock
     WebClient.RequestBodySpec requestBodySpec;
+
     @Mock
     Flux flux;
 
     @Mock
     WebClient.ResponseSpec responseSpec;
 
+    @BeforeEach
+    public void initMock() {
+        //let's not drag the spring context into this
+        MockitoAnnotations.openMocks(this);
+    }
+
 
     @Test
     void cookies() {
-        Mockito.when(webClientBuilder.exchangeStrategies(Mockito.any(ExchangeStrategies.class))).thenReturn(webClientBuilder);
-        Mockito.when(webClientBuilder.clientConnector(Mockito.any(ReactorClientHttpConnector.class))).thenReturn(webClientBuilder);
-        Mockito.when(webClientBuilder.build()).thenReturn(mockedWebClient);
-        final MockedStatic<WebClient> webClientStatic = Mockito.mockStatic(WebClient.class);
-        webClientStatic.when(WebClient::create).thenReturn(mockedWebClient);
-        when(mockedWebClient.post()).thenReturn(requestBodyUriSpec);
-        when(requestBodySpec.contentType(any())).thenReturn(requestBodySpec);
-        when(requestBodyUriSpec.uri(any(URI.class))).thenReturn(requestBodySpec);
-        when(requestBodySpec.header(any(), any())).thenReturn(requestBodySpec);
-        when(requestBodySpec.bodyValue(any())).thenReturn(headersSpec);
-        when(headersSpec.accept(any())).thenReturn(headersSpec);
-        when(headersSpec.exchangeToFlux(any())).thenReturn(flux);
-        when(responseSpec.bodyToFlux(ArgumentMatchers.<Class<String>>notNull()))
-                .thenReturn(Flux.just("resp"));
-        assertThrows(RuntimeException.class, () -> {
-            authService.buildAuthHeaders(new HttpHeaders());
-        });
+        try (MockedStatic<WebClient> webClientStatic = Mockito.mockStatic(WebClient.class)) {
+            Mockito.when(webClientBuilder.exchangeStrategies(Mockito.any(ExchangeStrategies.class))).thenReturn(webClientBuilder);
+            Mockito.when(webClientBuilder.clientConnector(Mockito.any(ReactorClientHttpConnector.class))).thenReturn(webClientBuilder);
+            Mockito.when(webClientBuilder.build()).thenReturn(mockedWebClient);
+            webClientStatic.when(WebClient::create).thenReturn(mockedWebClient);
+            when(mockedWebClient.post()).thenReturn(requestBodyUriSpec);
+            when(requestBodySpec.contentType(any())).thenReturn(requestBodySpec);
+            when(requestBodyUriSpec.uri(any(URI.class))).thenReturn(requestBodySpec);
+            when(requestBodySpec.header(any(), any())).thenReturn(requestBodySpec);
+            when(requestBodySpec.bodyValue(any())).thenReturn(headersSpec);
+            when(headersSpec.accept(any())).thenReturn(headersSpec);
+            when(headersSpec.exchangeToFlux(any())).thenReturn(flux);
+            when(responseSpec.bodyToFlux(ArgumentMatchers.<Class<String>>notNull()))
+                    .thenReturn(Flux.just("resp"));
+            HttpHeaders headers = new HttpHeaders();
+            assertThrows(RuntimeException.class, () -> {
+                authService.buildAuthHeaders(headers);
+            });
+        }
     }
 }
