a9702483f1381fac2009616ff86bdfb74de75a56
nflearl <earl.nolan@semanticbits.com>
2022-01-28 10:09:07 -0800

    AB2D-4328 Converted solution to builder pattern that does not use deprecated methods.

--- hpms/src/main/java/gov/cms/ab2d/hpms/config/WebClientConfig.java
+++ hpms/src/main/java/gov/cms/ab2d/hpms/config/WebClientConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -15,10 +14,9 @@ public class WebClientConfig {
     @Bean
     public WebClient buildWebClient() {
         final int timeout = 90;
-        TcpClient tcpClient = TcpClient.newConnection();
-        HttpClient httpClient = HttpClient.from(tcpClient)
-                .tcpConfiguration(client -> client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout * 1000)
-                        .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(timeout))));
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout * 1000)
+                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(timeout)));
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
