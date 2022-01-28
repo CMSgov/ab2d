13a4cdbfde4c424e9ce43bd00ed58a252d040303
nflearl <earl.nolan@semanticbits.com>
2022-01-28 13:25:54 -0800

    AB2D-4328 Missed creating a new connection provider which is what the deprecated version was doing.

--- hpms/src/main/java/gov/cms/ab2d/hpms/config/WebClientConfig.java
+++ hpms/src/main/java/gov/cms/ab2d/hpms/config/WebClientConfig.java
@@ -1,22 +1,18 @@
 package gov.cms.ab2d.hpms.config;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient buildWebClient() {
-        final int timeout = 90;
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout * 1000)
-                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(timeout)));
+        HttpClient httpClient = HttpClient.create(ConnectionProvider.newConnection());
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
