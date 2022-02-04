922c1abb4edc5b65d83f93fabe66b53e9ca52123
nflearl <earl.nolan@semanticbits.com>
2022-02-04 15:01:36 -0800

    AB2D-4376: Added additional testing to make Sonar happier.

--- hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSManualModeTest.java
+++ hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSManualModeTest.java
@@ -4,7 +4,7 @@ import gov.cms.ab2d.common.model.Contract;
 import gov.cms.ab2d.common.repository.ContractRepository;
 import gov.cms.ab2d.common.util.AB2DPostgresqlContainer;
 import gov.cms.ab2d.hpms.SpringBootTestApp;
-import org.junit.jupiter.api.BeforeEach;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,6 +18,7 @@ import java.time.OffsetDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = SpringBootTestApp.class)
@@ -37,22 +38,41 @@ public class HPMSManualModeTest {
     @Autowired
     private AttestationUpdaterServiceImpl aus;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void manualLeftAlone() {
+        Contract contract = buildContract(Contract.UpdateMode.MANUAL);
+        Contract savedContract = contractRepository.save(contract);
+
+        aus.pollOrganizations();
+        Optional<Contract> contractOpt = contractRepository.findContractByContractNumber(TEST_CONTRACT_NUMBER);
+        assertTrue(contractOpt.isPresent());
+        Contract contractResult = contractOpt.get();
+        assertNotNull(contractResult.getAttestedOn());
+
+        contractRepository.delete(savedContract);
+    }
+
+    @Test
+    public void cleardOut() {
+        Contract contract = buildContract(Contract.UpdateMode.AUTOMATIC);
+        Contract savedContract = contractRepository.save(contract);
+
+        aus.pollOrganizations();
+        Optional<Contract> contractOpt = contractRepository.findContractByContractNumber(TEST_CONTRACT_NUMBER);
+        assertTrue(contractOpt.isPresent());
+        Contract contractResult = contractOpt.get();
+        assertNull(contractResult.getAttestedOn());
+
+        contractRepository.delete(savedContract);
+    }
+
+    @NotNull
+    private Contract buildContract(Contract.UpdateMode updateMode) {
         Contract contract = new Contract();
         contract.setContractNumber(TEST_CONTRACT_NUMBER);
         contract.setContractName("Manual Mode Test");
         contract.setAttestedOn(OffsetDateTime.now());
-        contract.setUpdateMode(Contract.UpdateMode.MANUAL);
-        contractRepository.save(contract);
-    }
-
-    @Test
-    public void manualLeftAlone() {
-        aus.pollOrganizations();
-        Optional<Contract> contractOpt = contractRepository.findContractByContractNumber(TEST_CONTRACT_NUMBER);
-        assertTrue(contractOpt.isPresent());
-        Contract contract = contractOpt.get();
-        assertNotNull(contract.getAttestedOn());
+        contract.setUpdateMode(updateMode);
+        return contract;
     }
 }
