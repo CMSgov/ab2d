bf5cc1d5aa610f0abdc56798f4c18f7f7c94d97d
nflearl <earl.nolan@semanticbits.com>
2022-02-17 08:45:01 -0800

    AB2D-4218: Unused import cleanup.

--- api/src/main/java/gov/cms/ab2d/api/util/Constants.java
+++ api/src/main/java/gov/cms/ab2d/api/util/Constants.java
@@ -1,8 +1,5 @@
 package gov.cms.ab2d.api.util;
 
-import static gov.cms.ab2d.common.util.Constants.API_PREFIX_V1;
-import static gov.cms.ab2d.common.util.Constants.FHIR_PREFIX;
-
 public final class Constants {
 
     public static final String GENERIC_FHIR_ERR_MSG = "The body will contain a FHIR " +
