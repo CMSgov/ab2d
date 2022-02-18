88e456ca97e43475062d5a85732b8444efec5582
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-02-18 13:37:52 -0800

    Update create-ab2d-analyst.sql

--- common/src/main/resources/db/changelog/test_only/create-ab2d-analyst.sql
+++ common/src/main/resources/db/changelog/test_only/create-ab2d-analyst.sql
@@ -1,4 +1,11 @@
 ------------------------------
 -- Create ab2d_analyst user
 ------------------------------
-Create Role ab2d_analyst noinherit login password 'ab2d';
+DO
+$$
+    begin
+        if not exists(SELECT * FROM pg_user WHERE usename = 'ab2d_analyst') THEN
+            Create Role ab2d_analyst noinherit login password 'ab2d';
+        end if;
+    end
+$$
