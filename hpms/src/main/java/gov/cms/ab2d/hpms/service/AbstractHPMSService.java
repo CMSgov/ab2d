368b144c6a8ceb7303f7c6e7df5b509e07ad3068
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-03-02 09:36:10 -0800

    Revert changes that are needed for impl

--- hpms/src/main/java/gov/cms/ab2d/hpms/service/AbstractHPMSService.java
+++ hpms/src/main/java/gov/cms/ab2d/hpms/service/AbstractHPMSService.java
@@ -1,16 +1,31 @@
 package gov.cms.ab2d.hpms.service;
 
-import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 public class AbstractHPMSService {
 
     /*
      * In Production, this most likely will be an empty map which is defined as {} in the property which results in
-     * the collection actually not being initialized.
+     * the collection actually not being initialized.  Thus, added the explicit initialization to an empty collection
+     * so params is never null.
      */
+    @SuppressWarnings("FieldMayBeFinal")
+    @Value("#{${hpms.api.params}}")
+    private MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 
     protected URI buildFullURI(String baseURI) {
-        return UriComponentsBuilder.fromUriString(baseURI).build().toUri();
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(baseURI);
+
+        if (!params.isEmpty()) {
+            uriBuilder.queryParams(params);
+        }
+
+        return uriBuilder.build().toUri();
     }
 }
