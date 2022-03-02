cc2d587031c4f756be5ea3823bba5399f4e60bac
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-03-02 14:14:36 -0800

    Add test for auth mock

--- hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSMockedAuthTest.java
+++ hpms/src/test/java/gov/cms/ab2d/hpms/service/HPMSMockedAuthTest.java
@@ -17,7 +17,9 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpHeaders.COOKIE;
 
@@ -50,6 +52,14 @@ class HPMSMockedAuthTest {
             authService.buildAuthHeaders(headers);
             assertNotNull(headers.get(COOKIE));
             assertTrue(headers.get(COOKIE).stream().anyMatch(cookie -> cookie.contains("test")));
+        }
+    }
+
+    @Test
+    void authNoKey() {
+        try (MockedStatic<WebClient> webClientStatic = Mockito.mockStatic(WebClient.class)) {
+            client.authRequest(mockedWebClient, webClientStatic, new HPMSAuthResponse());
+            assertThrows(RuntimeException.class, () -> authService.buildAuthHeaders(new HttpHeaders()));
         }
     }
 
