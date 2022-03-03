304ed508dbf466c93e0b2ae7ae79459a7a83e4ce
nflearl <earl.nolan@semanticbits.com>
2022-03-03 11:13:40 -0800

    AB2D-4425: Tweaked SearchConfigTest so that it can run on different platforms.

--- worker/src/test/java/gov/cms/ab2d/worker/config/SearchConfigTest.java
+++ worker/src/test/java/gov/cms/ab2d/worker/config/SearchConfigTest.java
@@ -2,18 +2,24 @@ package gov.cms.ab2d.worker.config;
 
 import org.junit.jupiter.api.Test;
 
+import static java.io.File.separatorChar;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchConfigTest {
+    static final String EFS_MOUNT = separatorChar + "tmp" + separatorChar + "abc";
+    static final String EXPECTED_STREAM = EFS_MOUNT + separatorChar + "jobid" + separatorChar + "stream";
+    static final String FINISHED_STREAM = EFS_MOUNT + separatorChar + "jobid" + separatorChar + "finish";
+
     @Test
     void testSearchConfig() {
-        SearchConfig searchConfig = new SearchConfig("/tmp/abc", "stream", "finish", 0, 200, 2, 10);
+        SearchConfig searchConfig = new SearchConfig(EFS_MOUNT, "stream", "finish", 0, 200, 2, 10);
 
-        assertEquals("/tmp/abc/jobid/stream", searchConfig.getStreamingDir("jobid").getAbsolutePath());
-        assertEquals("/tmp/abc/jobid/finish", searchConfig.getFinishedDir("jobid").getAbsolutePath());
+        assertEquals(EXPECTED_STREAM, searchConfig.getStreamingDir("jobid").getPath());
+        assertEquals(FINISHED_STREAM, searchConfig.getFinishedDir("jobid").getPath());
         assertEquals("finish", searchConfig.getFinishedDir());
         assertEquals("stream", searchConfig.getStreamingDir());
-        assertEquals("/tmp/abc", searchConfig.getEfsMount());
+        assertEquals(EFS_MOUNT, searchConfig.getEfsMount());
         assertEquals(0, searchConfig.getBufferSize());
         assertEquals(200, searchConfig.getNdjsonRollOver());
         assertEquals(10, searchConfig.getNumberBenesPerBatch());
