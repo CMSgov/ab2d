b2add11fc786e08d3bd11246504b82d014c43bca
nflearl <earl.nolan@semanticbits.com>
2022-03-09 07:33:55 -0800

    Formatting and warning cleanup.

--- worker/src/main/java/gov/cms/ab2d/worker/service/ContractWorkerClient.java
+++ worker/src/main/java/gov/cms/ab2d/worker/service/ContractWorkerClient.java
@@ -3,7 +3,9 @@ package gov.cms.ab2d.worker.service;
 import gov.cms.ab2d.common.dto.ContractDTO;
 import gov.cms.ab2d.common.model.Contract;
 import gov.cms.ab2d.common.service.ContractService;
+
 import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ContractWorkerClient {
 
-    private ContractService contractService;
+    private final ContractService contractService;
 
     public ContractWorkerClient(ContractService contractService) {
         this.contractService = contractService;
@@ -19,10 +21,10 @@ public class ContractWorkerClient {
 
     public ContractDTO getContractByContractNumber(String contractNumber) {
         Optional<Contract> contractOptional = contractService.getContractByContractNumber(contractNumber);
-        if(contractOptional.isPresent())
-        {
+        if (contractOptional.isPresent()) {
             Contract contract = contractOptional.get();
-            return new ContractDTO(contract.getContractNumber(), contract.getContractName(), contract.getAttestedOn().toString(), contract.getContractType());
+            return new ContractDTO(contract.getContractNumber(), contract.getContractName(),
+                    contract.getAttestedOn().toString(), contract.getContractType());
         }
         return null;
     }
