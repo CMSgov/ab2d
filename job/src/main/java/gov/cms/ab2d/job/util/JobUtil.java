25dcb43640584d9ed3c7650381f546934264d183
nflearl <earl.nolan@semanticbits.com>
2022-04-22 11:14:56 -0700

    AB2D-4557: Attempt to fix Sonar Failure adding tests for JobUtil - none of the current tests ever covered the exception case and checking for job doneness really should not ever throw an exception.
    Removed try/catch.

--- job/src/main/java/gov/cms/ab2d/job/util/JobUtil.java
+++ job/src/main/java/gov/cms/ab2d/job/util/JobUtil.java
@@ -19,39 +19,32 @@ public class JobUtil {
      * @return
      */
     public static boolean isJobDone(Job job) {
-        try {
-            // Job is still in progress
-            if (job == null || job.getStatus() == null || job.getStatus() == JobStatus.IN_PROGRESS || job.getStatus() == JobStatus.SUBMITTED) {
-                return false;
-            }
-
-            // Job has finished but was not successful
-            if (job.getStatus() == JobStatus.CANCELLED || job.getStatus() == JobStatus.FAILED) {
-                return true;
-            }
-
-            // Job was successful - now did the client download the files or is it expired
-
-            // Job has expired, it's done.
-            if (job.getExpiresAt() != null && job.getExpiresAt().isBefore(OffsetDateTime.now())) {
-                return true;
-            }
-
-            // If it hasn't expired, look to see if all files have been downloaded, if so, it's done
-            List<JobOutput> jobOutputs = job.getJobOutputs();
-            if (jobOutputs == null || jobOutputs.isEmpty()) {
-                return false;
-            }
-            JobOutput aRemaining = jobOutputs.stream()
-                    .filter(c -> c.getError() == null || !c.getError())
-                    .filter(c -> !c.getDownloaded()).findAny().orElse(null);
-
-            return aRemaining == null;
-        } catch (Exception ex) {
-            // Logging should never break anything
-            String jobId = job.getJobUuid();
-            log.error("Unable to determine if job " + jobId + " is done", ex);
+        // Job is still in progress
+        if (job == null || job.getStatus() == null || job.getStatus() == JobStatus.IN_PROGRESS || job.getStatus() == JobStatus.SUBMITTED) {
             return false;
         }
+
+        // Job has finished but was not successful
+        if (job.getStatus() == JobStatus.CANCELLED || job.getStatus() == JobStatus.FAILED) {
+            return true;
+        }
+
+        // Job was successful - now did the client download the files or is it expired
+
+        // Job has expired, it's done.
+        if (job.getExpiresAt() != null && job.getExpiresAt().isBefore(OffsetDateTime.now())) {
+            return true;
+        }
+
+        // If it hasn't expired, look to see if all files have been downloaded, if so, it's done
+        List<JobOutput> jobOutputs = job.getJobOutputs();
+        if (jobOutputs == null || jobOutputs.isEmpty()) {
+            return false;
+        }
+        JobOutput aRemaining = jobOutputs.stream()
+                .filter(c -> c.getError() == null || !c.getError())
+                .filter(c -> !c.getDownloaded()).findAny().orElse(null);
+
+        return aRemaining == null;
     }
 }
