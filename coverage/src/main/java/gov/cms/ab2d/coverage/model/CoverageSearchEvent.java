131d1a63e21a533b670de7805dea1d8c6c3870d7
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-05-04 09:11:13 -0700

    remove unused import

--- coverage/src/main/java/gov/cms/ab2d/coverage/model/CoverageSearchEvent.java
+++ coverage/src/main/java/gov/cms/ab2d/coverage/model/CoverageSearchEvent.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
