eb884a2fc19d6fa7b28d0a2631dd88f3a5d24396
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-05-04 10:17:20 -0700

    AB2D-4557 event schema (#1003)
    * event scema creation
    
    * remove sensitive code
    
    * Add event schema
    
    * Update event-service-database-creation.sql
    
    * Update sql to fix common liqibase tests
    
    * rename file
    
    * move sql code to commons
    
    * clean code
    
    * Update event-service-database-migration.sql
    
    * clean up code
    
    * remove unneeded changes for review purposes and changes to sql script
    
    * Remove foriegn key restraint
    
    * revert event_bene_coverage_search_status_change changes
    
    * remove unused import

--- eventlogger/src/test/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ eventlogger/src/test/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -2,9 +2,6 @@ package gov.cms.ab2d.eventlogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.eventlogger"})
 public class SpringBootApp {
