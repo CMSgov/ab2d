19e8f8478348bf43985fc609a88803289568ca2b
sharshar-sb <laila.sharshar@semanticbits.com>
2022-06-24 07:36:43 -1000

    Cleanup and documenting interfaces

--- properties/src/main/java/gov/cms/ab2d/properties/model/Properties.java
+++ properties/src/main/java/gov/cms/ab2d/properties/model/Properties.java
@@ -2,6 +2,8 @@ package gov.cms.ab2d.properties.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,17 +11,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "properties", schema = "property")
-public class Properties extends TimestampBase {
+public class Properties {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="property.property_sequence")
     private Long id;
+
     @NotNull
     private String key;
+
     @NotNull
     private String value;
+
+    @CreationTimestamp
+    private OffsetDateTime created;
+
+    @UpdateTimestamp
+    private OffsetDateTime modified;
 }
