f1c8e593e19720348b973ef5dd7f1a063e9da1ca
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-07-19 16:00:48 -0700

    Update coverage

--- coverage/src/test/java/gov/cms/ab2d/coverage/service/CoverageServiceImplTest.java
+++ coverage/src/test/java/gov/cms/ab2d/coverage/service/CoverageServiceImplTest.java
@@ -19,6 +19,7 @@ import gov.cms.ab2d.coverage.repository.CoveragePeriodRepository;
 import gov.cms.ab2d.coverage.repository.CoverageSearchEventRepository;
 import gov.cms.ab2d.coverage.repository.CoverageSearchRepository;
 import gov.cms.ab2d.coverage.repository.CoverageServiceRepository;
+import gov.cms.ab2d.coverage.util.AB2DCoverageLocalstackContainer;
 import gov.cms.ab2d.coverage.util.AB2DCoveragePostgressqlContainer;
 import gov.cms.ab2d.coverage.util.Coverage;
 import gov.cms.ab2d.coverage.util.CoverageDataSetup;
@@ -99,6 +100,9 @@ class CoverageServiceImplTest {
 
     @Container
     private static final PostgreSQLContainer postgreSQLContainer= new AB2DCoveragePostgressqlContainer();
+
+    @Container
+    private static final AB2DCoverageLocalstackContainer localstackContainer = new AB2DCoverageLocalstackContainer();
 
     @Autowired
     CoveragePeriodRepository coveragePeriodRepo;
