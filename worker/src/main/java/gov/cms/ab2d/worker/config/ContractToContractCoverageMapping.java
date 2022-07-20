66c36469fc0758442dd9cc970db31f2f1adc555d
sb-keane <keane.mcgough@semanticbits.com>
2022-07-20 14:13:35 -0500

    added logging for debuggin

--- worker/src/main/java/gov/cms/ab2d/worker/config/ContractToContractCoverageMapping.java
+++ worker/src/main/java/gov/cms/ab2d/worker/config/ContractToContractCoverageMapping.java
@@ -4,12 +4,15 @@ import gov.cms.ab2d.common.dto.ContractDTO;
 import gov.cms.ab2d.common.model.Contract;
 import gov.cms.ab2d.coverage.model.ContractForCoverageDTO;
 import javax.annotation.PostConstruct;
+
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ContractToContractCoverageMapping {
 
     private static ModelMapper modelMapper;
@@ -49,6 +52,8 @@ public class ContractToContractCoverageMapping {
     }
 
     public ContractForCoverageDTO map(Contract contract) {
-        return modelMapper.map(contract, ContractForCoverageDTO.class);
+        ContractForCoverageDTO a = modelMapper.map(contract, ContractForCoverageDTO.class);
+        log.info("Attested from DB {} VS mapped {}", contract.getAttestedOn() , a.getAttestedOn());
+        return a;
     }
 }
