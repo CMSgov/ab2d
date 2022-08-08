777063b4e25bcf692cc159bf978bd674fa57ce80
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-08-08 15:19:52 -0700

    Update SpringBootApp.java

--- eventlogger/src/test/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ eventlogger/src/test/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -2,7 +2,6 @@ package gov.cms.ab2d.eventlogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient.clients"})
 public class SpringBootApp {
