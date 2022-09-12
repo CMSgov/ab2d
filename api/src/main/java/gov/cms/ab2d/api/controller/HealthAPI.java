fd7d1ca114ec40fa1dd724bb85001cebe28dccb1
sb-keane <keane.mcgough@semanticbits.com>
2022-09-12 11:04:07 -0500

    removed unused import

--- api/src/main/java/gov/cms/ab2d/api/controller/HealthAPI.java
+++ api/src/main/java/gov/cms/ab2d/api/controller/HealthAPI.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
