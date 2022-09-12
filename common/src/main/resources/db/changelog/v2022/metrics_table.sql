b7fb1fbd77ea0f6be9f52919381d3f082dc9229c
sb-keane <keane.mcgough@semanticbits.com>
2022-09-12 09:37:32 -0500

    added standard columns to metric event table

--- common/src/main/resources/db/changelog/v2022/metrics_table.sql
+++ common/src/main/resources/db/changelog/v2022/metrics_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS event.event_metrics
 (
     id            BIGSERIAL PRIMARY KEY,
-    service       varchar(32)  not null,
-    time_of_event timestamp not null
+    service       varchar(32) not null,
+    time_of_event timestamp   not null,
+    awsId         varchar(255),
+    environment   varchar(255),
+    job_id        varchar(255)
 )
