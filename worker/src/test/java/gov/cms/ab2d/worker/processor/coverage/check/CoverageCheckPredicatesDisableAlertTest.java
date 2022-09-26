98e674d2f8974ff1335aa89aef1aa407793a2928
sb-keane <keane.mcgough@semanticbits.com>
2022-09-26 16:08:49 -0500

    more tests

--- worker/src/test/java/gov/cms/ab2d/worker/processor/coverage/check/CoverageCheckPredicatesDisableAlertTest.java
+++ worker/src/test/java/gov/cms/ab2d/worker/processor/coverage/check/CoverageCheckPredicatesDisableAlertTest.java
@@ -5,14 +5,18 @@ import gov.cms.ab2d.common.model.Contract;
 import gov.cms.ab2d.coverage.model.CoverageCount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 
+import java.lang.reflect.Method;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -34,6 +38,33 @@ class CoverageCheckPredicatesDisableAlertTest {
 
         assertTrue(issues.stream()
                 .noneMatch(issue -> issue.equals("S3147,2021,12")));
+    }
+
+    @DisplayName("Coverage ignored")
+    @Test
+    void whenCoveragePeriodsMissing_ignore() throws NoSuchMethodException {
+        CoveragePresentCheck check = new CoveragePresentCheck(null, null, null);
+        Method method = Arrays.stream(CoveragePresentCheck.class.getDeclaredMethods())
+                .filter(m -> "ignoreMissing".equals(m.getName()))
+                .findFirst()
+                .orElse(null);
+        method.setAccessible(true);
+        assertSame(ReflectionUtils.invokeMethod(method, check, "S3147", 2021, 12), Boolean.TRUE);
+    }
+
+    @DisplayName("Coverage ignored")
+    @Test
+    void whenCoveragePeriodsMissing_log() throws NoSuchMethodException {
+        ContractDTO contractDTO = new ContractDTO("S3147", "Test",
+                OffsetDateTime.of(2021, 12, 30, 1, 1, 1, 1, ZoneOffset.UTC)
+                        .minus(2, ChronoUnit.YEARS), Contract.ContractType.NORMAL);
+        CoveragePresentCheck check = new CoveragePresentCheck(null, null, null);
+        Method method = Arrays.stream(CoveragePresentCheck.class.getDeclaredMethods())
+                .filter(m -> "logIssue".equals(m.getName()))
+                .findFirst()
+                .orElse(null);
+        method.setAccessible(true);
+        assertSame(ReflectionUtils.invokeMethod(method, check, contractDTO, 2021, 12, null), null);
     }
 
 }
