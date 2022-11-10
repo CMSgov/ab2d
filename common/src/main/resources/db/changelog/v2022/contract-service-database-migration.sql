dc59008f98d847db31140f7981aad9d80a2a08f0
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-11-10 14:40:00 -0800

    Update contract-service-database-migration.sql

--- common/src/main/resources/db/changelog/v2022/contract-service-database-migration.sql
+++ common/src/main/resources/db/changelog/v2022/contract-service-database-migration.sql
@@ -8,8 +8,9 @@ INSERT INTO contract_tmp (SELECT * FROM public.contract);
 -- Create new table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS contract.contract (like public.contract including all);
 
--- Insert any missing values into the properties table. If this is run a second time, it will not duplicate the values
+-- Insert any missing values into the contract table. If this is run a second time, it will not duplicate the values
 INSERT INTO contract.contract (id, contract_number, contract_name, attested_on, created, modified, hpms_parent_org_id, hpms_parent_org_name, hpms_org_marketing_name, update_mode, contract_type)
+
 SELECT t.id, t.contract_number, t.contract_name, t.attested_on, t.created, t.modified, t.hpms_parent_org_id, t.hpms_parent_org_name, t.hpms_org_marketing_name, t.update_mode, t.contract_type
 FROM contract_tmp t
          LEFT JOIN contract.contract p ON p.id = t.id WHERE p.id is null;
