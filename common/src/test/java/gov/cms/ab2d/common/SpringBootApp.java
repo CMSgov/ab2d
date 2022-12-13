13da2b922c4e7a478548cda0be5653ab6f7fe1e4
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-13 11:49:17 -0800

    Update SpringBootApp.java

--- common/src/test/java/gov/cms/ab2d/common/SpringBootApp.java
+++ common/src/test/java/gov/cms/ab2d/common/SpringBootApp.java
@@ -3,12 +3,14 @@ package gov.cms.ab2d.common;
 import gov.cms.ab2d.common.util.AB2DSQSMockConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.eventclient.clients"})
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.eventclient.clients", "gov.cms.ab2d.contracts"})
 @PropertySource("classpath:application.common.properties")
 @Import(AB2DSQSMockConfig.class)
+@EntityScan(basePackages = {"gov.cms.ab2d.common.model", "gov.cms.ab2d.job.model", "gov.cms.ab2d.properties.model", "gov.cms.ab2d.contracts"})
 public class SpringBootApp {
 
     public static void main(String [] args) {
