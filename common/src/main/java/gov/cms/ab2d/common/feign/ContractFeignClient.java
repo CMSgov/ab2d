32989e01cf8f0c586bb8274d119e8e5e7f140123
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-13 08:55:04 -0800

    Checkstyle fix

--- common/src/main/java/gov/cms/ab2d/common/feign/ContractFeignClient.java
+++ common/src/main/java/gov/cms/ab2d/common/feign/ContractFeignClient.java
@@ -3,5 +3,6 @@ package gov.cms.ab2d.common.feign;
 import gov.cms.ab2d.contracts.model.ContractAPI;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "contract",  url = "${contract.base.url}")
-public interface ContractFeignClient extends ContractAPI {}
+@FeignClient(name = "contract", url = "${contract.base.url}")
+public interface ContractFeignClient extends ContractAPI {
+}
