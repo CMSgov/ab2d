4a0e025d1241594bd4035a4b8f8cf7414f6f8677
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-14 09:41:56 -0800

    Add contractFeign into ContractService

--- common/src/main/java/gov/cms/ab2d/common/service/ContractServiceImpl.java
+++ common/src/main/java/gov/cms/ab2d/common/service/ContractServiceImpl.java
@@ -1,43 +1,77 @@
 package gov.cms.ab2d.common.service;
 
-import gov.cms.ab2d.contracts.model.Contract;
+import gov.cms.ab2d.common.feign.ContractFeignClient;
 import gov.cms.ab2d.common.repository.ContractRepository;
-import lombok.AllArgsConstructor;
+import gov.cms.ab2d.contracts.model.Contract;
+import gov.cms.ab2d.contracts.model.ContractDTO;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
-@AllArgsConstructor
 @Service
 @Transactional
 public class ContractServiceImpl implements ContractService {
 
     private final ContractRepository contractRepository;
 
+    private final ContractFeignClient contractFeignClient;
+
+    private final boolean contractServiceEnabled;
+
+    public ContractServiceImpl(ContractRepository contractRepository, ContractFeignClient contractFeignClient,
+                               @Value("${contract.service.enabled}") boolean contractServiceEnabled) {
+        this.contractRepository = contractRepository;
+        this.contractFeignClient = contractFeignClient;
+        this.contractServiceEnabled = contractServiceEnabled;
+    }
+
+
     public List<Contract> getAllAttestedContracts() {
         // There are about 100 contracts including test contracts so this call has minimal cost.
         // This call is only made once a day as well.
+        if (contractServiceEnabled)
+            return contractFeignClient.getContracts(null).stream()
+                    .map(this::dtoToContract).toList();
+
         return contractRepository.findAll()
                 .stream().filter(Contract::hasAttestation).collect(toList());
     }
 
     public Optional<Contract> getContractByContractNumber(String contractNumber) {
+        if (contractServiceEnabled)
+            return Optional.of(dtoToContract(contractFeignClient.getContractByNumber(contractNumber).get(0)));
+
         return contractRepository.findContractByContractNumber(contractNumber);
     }
 
     @Override
     public void updateContract(Contract contract) {
-        contractRepository.save(contract);
+        if (contractServiceEnabled)
+            contractFeignClient.updateContract(contract.toDTO());
+        else
+            contractRepository.save(contract);
+
     }
 
     @Override
     public Contract getContractByContractId(Long contractId) {
-        Optional<Contract> optionalContract = contractRepository.findById(contractId);
-        return optionalContract.orElse(null);
+        if (contractServiceEnabled) {
+            return dtoToContract(contractFeignClient.getContracts(contractId).get(0));
+        }
+        return contractRepository.findById(contractId).orElse(null);
+    }
+
+    //TODO replace Contract with ContractDTO
+    private Contract dtoToContract(ContractDTO contractDTO) {
+        Contract contract = new Contract(contractDTO.getContractNumber(), contractDTO.getContractName(), null, null, null);
+        contract.setAttestedOn(contract.getAttestedOn());
+        contract.setContractType(contractDTO.getContractType());
+        return contract;
     }
 
 }
--- worker/src/main/java/gov/cms/ab2d/worker/SpringBootApp.java
+++ worker/src/main/java/gov/cms/ab2d/worker/SpringBootApp.java
@@ -1,10 +1,12 @@
 package gov.cms.ab2d.worker;
 
+import gov.cms.ab2d.common.feign.ContractFeignClient;
 import gov.cms.ab2d.worker.bfdhealthcheck.BFDHealthCheckQuartzSetup;
 import gov.cms.ab2d.worker.stuckjob.StuckJobQuartzSetup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -25,6 +27,7 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableRetry
 @PropertySource("classpath:application.common.properties")
 @Import({StuckJobQuartzSetup.class, BFDHealthCheckQuartzSetup.class})
+@EnableFeignClients(clients = {ContractFeignClient.class})
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
