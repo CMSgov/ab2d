1ae01e13642aa59f47f6015f15443c7603d7d703
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2023-01-26 13:03:14 -0800

    remove hpms

--- api/src/main/java/gov/cms/ab2d/api/SpringBootApp.java
+++ api/src/main/java/gov/cms/ab2d/api/SpringBootApp.java
@@ -2,23 +2,19 @@ package gov.cms.ab2d.api;
 
 import gov.cms.ab2d.api.config.MDCFilter;
 import gov.cms.ab2d.common.feign.ContractFeignClient;
-import gov.cms.ab2d.hpms.quartz.HPMSIngestQuartzSetup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.job", "gov.cms.ab2d.api",
-        "gov.cms.ab2d.hpms", "gov.cms.ab2d.eventclient.clients", "gov.cms.ab2d.contracts"})
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.common", "gov.cms.ab2d.job", "gov.cms.ab2d.api", "gov.cms.ab2d.eventclient.clients", "gov.cms.ab2d.contracts"})
 @EntityScan(basePackages = {"gov.cms.ab2d.common.model", "gov.cms.ab2d.job.model", "gov.cms.ab2d.contracts"})
 @EnableJpaRepositories({"gov.cms.ab2d.common.repository", "gov.cms.ab2d.job.repository"})
 @PropertySource("classpath:application.common.properties")
-@Import({HPMSIngestQuartzSetup.class})
 @EnableFeignClients(clients = {ContractFeignClient.class})
 public class SpringBootApp {
 
