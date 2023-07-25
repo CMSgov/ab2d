39f72483b34debb9f00b55fb83644f61090c7c85
Sean Fern <seanfern@navapbc.com>
2023-07-25 17:11:13 -0400

    Switch to io.awspring.cloud for import

--- common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
+++ common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
@@ -2,14 +2,14 @@ package gov.cms.ab2d.common.util;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import gov.cms.ab2d.eventclient.clients.SQSEventClient;
+import io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration;
+import io.awspring.cloud.autoconfigure.messaging.MessagingAutoConfiguration;
+import io.awspring.cloud.messaging.config.SimpleMessageListenerContainerFactory;
+import io.awspring.cloud.messaging.listener.QueueMessageHandler;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration;
-import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
-import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import static org.mockito.Mockito.mock;
