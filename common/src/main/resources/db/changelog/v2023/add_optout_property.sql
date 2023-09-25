34cc321051545eb575213a0e320d5eea9f4316fb
smirnovaae <annasmirnova@navapbc.com>
2023-09-25 13:18:17 -0700

    AB2D-5434 Update Properties service to use own schema

--- common/src/main/resources/db/changelog/v2023/add_optout_property.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO property.properties(id, key, value, created, modified)
-VALUES (nextval('hibernate_sequence'), 'OptOutOn', 'false', current_timestamp, current_timestamp);
-
---rollback DELETE FROM properties WHERE key = 'OptOutOn';
