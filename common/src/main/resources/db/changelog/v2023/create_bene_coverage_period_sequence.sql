3b72df141e9354a4856adc6d83ee135333487645
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-02 15:21:49 -0400

    add bene_coverage_period_seq migration

---
+++ common/src/main/resources/db/changelog/v2023/create_bene_coverage_period_sequence.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE IF NOT EXISTS public.bene_coverage_period_seq START 1;
+
+select setval('public.bene_coverage_period_seq', (SELECT MAX(id) FROM public.bene_coverage_period));
+ALTER TABLE public.bene_coverage_period ALTER COLUMN id SET DEFAULT nextval('public.bene_coverage_period_seq');
