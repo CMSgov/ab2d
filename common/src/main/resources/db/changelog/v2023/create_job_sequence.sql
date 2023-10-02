759dce106e3f79a795adb966743e395431f6587b
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-02 13:42:04 -0400

    add create_job_sequence db migration

---
+++ common/src/main/resources/db/changelog/v2023/create_job_sequence.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE IF NOT EXISTS public.job_seq START 1;
+
+select setval('public.job_seq', (SELECT MAX(id) FROM public.job));
+ALTER TABLE public.job ALTER COLUMN id SET DEFAULT nextval('public.job_seq');
