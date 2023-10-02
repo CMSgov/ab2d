51138bf4e3979749ef9b89f1bd03494500016b0b
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-02 16:10:37 -0400

    add db migration create_event_bene_coverage_search_status_change_seq

---
+++ common/src/main/resources/db/changelog/v2023/create_event_bene_coverage_search_status_change_sequence.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE IF NOT EXISTS public.event_bene_coverage_search_status_change_seq START 1;
+
+select setval('public.event_bene_coverage_search_status_change_seq', (SELECT MAX(id) FROM public.event_bene_coverage_search_status_change));
+ALTER TABLE public.event_bene_coverage_search_status_change ALTER COLUMN id SET DEFAULT nextval('public.event_bene_coverage_search_status_change_seq');
