7c96259de2aa91d01b1099965bc6b7c4b930d8eb
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-02 15:37:51 -0400

    add coverage_search_seq db migration

---
+++ common/src/main/resources/db/changelog/v2023/create_coverage_search_sequence.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE IF NOT EXISTS public.coverage_search_seq START 1;
+
+select setval('public.coverage_search_seq', (SELECT MAX(id) FROM public.coverage_search));
+ALTER TABLE public.coverage_search ALTER COLUMN id SET DEFAULT nextval('public.coverage_search_seq');
