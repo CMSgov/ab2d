2ceeee06ca89d72d71db120a5a3c2133f70c7b27
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-02 13:09:09 -0400

    add create_user_account_seq to database changelog

---
+++ common/src/main/resources/db/changelog/v2023/create_user_account_sequence.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE IF NOT EXISTS public.user_account_seq START 1;
+
+select setval('public.user_account_seq', (SELECT MAX(id) FROM public.user_account));
+ALTER TABLE public.user_account ALTER COLUMN id SET DEFAULT nextval('public.user_account_seq');
