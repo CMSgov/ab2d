3804b31cdf0bdb7dbbf6b655b316f06d25549d14
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-06 16:03:12 -0400

    test revert db sequence increment change

--- common/src/main/resources/db/changelog/v2023/create_bene_coverage_period_sequence.sql
+++ common/src/main/resources/db/changelog/v2023/create_bene_coverage_period_sequence.sql
@@ -1,4 +1,5 @@
-CREATE SEQUENCE IF NOT EXISTS public.bene_coverage_period_seq START 1 INCREMENT BY 50;
+-- CREATE SEQUENCE IF NOT EXISTS public.bene_coverage_period_seq START 1 INCREMENT BY 50;
+CREATE SEQUENCE IF NOT EXISTS public.bene_coverage_period_seq START 1;
 
 select setval('public.bene_coverage_period_seq', (SELECT MAX(id) FROM public.bene_coverage_period));
 ALTER TABLE public.bene_coverage_period ALTER COLUMN id SET DEFAULT nextval('public.bene_coverage_period_seq');
