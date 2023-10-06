22960f63561e83821a2ae61581742a4dd69f2658
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-06 13:30:14 -0400

    add increment by 50 to all sequences (hibernate 6 change)

--- common/src/main/resources/db/changelog/v2023/create_coverage_search_sequence.sql
+++ common/src/main/resources/db/changelog/v2023/create_coverage_search_sequence.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE IF NOT EXISTS public.coverage_search_seq START 1;
+CREATE SEQUENCE IF NOT EXISTS public.coverage_search_seq START 1 INCREMENT BY 50;
 
 select setval('public.coverage_search_seq', (SELECT MAX(id) FROM public.coverage_search));
 ALTER TABLE public.coverage_search ALTER COLUMN id SET DEFAULT nextval('public.coverage_search_seq');
--- common/src/main/resources/db/changelog/v2023/create_user_account_sequence.sql
+++ common/src/main/resources/db/changelog/v2023/create_user_account_sequence.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE IF NOT EXISTS public.user_account_seq START 1;
+CREATE SEQUENCE IF NOT EXISTS public.user_account_seq START 1 INCREMENT BY 50;
 
 select setval('public.user_account_seq', (SELECT MAX(id) FROM public.user_account));
 ALTER TABLE public.user_account ALTER COLUMN id SET DEFAULT nextval('public.user_account_seq');
