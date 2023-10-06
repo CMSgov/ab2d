6ecdc794f4ce9a7d0291f89d5444974d4e96f671
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-06 16:03:12 -0400

    test revert db sequence increment change

--- common/src/main/resources/db/changelog/v2023/create_event_bene_coverage_search_status_change_sequence.sql
+++ common/src/main/resources/db/changelog/v2023/create_event_bene_coverage_search_status_change_sequence.sql
@@ -1,4 +1,5 @@
-CREATE SEQUENCE IF NOT EXISTS public.event_bene_coverage_search_status_change_seq START 1 INCREMENT BY 50;
+-- CREATE SEQUENCE IF NOT EXISTS public.event_bene_coverage_search_status_change_seq START 1 INCREMENT BY 50;
+CREATE SEQUENCE IF NOT EXISTS public.event_bene_coverage_search_status_change_seq START 1;
 
 select setval('public.event_bene_coverage_search_status_change_seq', (SELECT MAX(id) FROM public.event_bene_coverage_search_status_change));
 ALTER TABLE public.event_bene_coverage_search_status_change ALTER COLUMN id SET DEFAULT nextval('public.event_bene_coverage_search_status_change_seq');
--- common/src/main/resources/db/changelog/v2023/create_job_output_sequence.sql
+++ common/src/main/resources/db/changelog/v2023/create_job_output_sequence.sql
@@ -1,4 +1,5 @@
-CREATE SEQUENCE IF NOT EXISTS public.job_output_seq START 1 INCREMENT BY 50;
+-- CREATE SEQUENCE IF NOT EXISTS public.job_output_seq START 1 INCREMENT BY 50;
+CREATE SEQUENCE IF NOT EXISTS public.job_output_seq START 1;
 
 select setval('public.job_output_seq', (SELECT MAX(id) FROM public.job_output));
 ALTER TABLE public.job_output ALTER COLUMN id SET DEFAULT nextval('public.job_output_seq');
