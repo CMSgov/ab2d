861836ec528e491a43212a2475d0f6fed1caf1d8
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-10-23 13:07:00 -0400

    cleanup now unused imports and dependencies

--- api/src/main/java/gov/cms/ab2d/api/security/SecurityConfig.java
+++ api/src/main/java/gov/cms/ab2d/api/security/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
--- common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
+++ common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
@@ -5,9 +5,6 @@ import gov.cms.ab2d.eventclient.clients.SQSEventClient;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-// import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
-// import org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +13,6 @@ import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 @EnableAutoConfiguration
-// @EnableAutoConfiguration(exclude = {MessagingAutoConfiguration.class, ContextStackAutoConfiguration.class})
 public class AB2DSQSMockConfig {
 
   static {
