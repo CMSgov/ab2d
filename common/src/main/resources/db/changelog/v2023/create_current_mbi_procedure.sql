bc23dd25f92373582b1b45577f7583dcca500e3a
smirnovaae <annasmirnova@navapbc.com>
2023-11-06 11:56:24 -0800

    AB2D-5782 Database changes for AttributionDataShare Lambda

---
+++ common/src/main/resources/db/changelog/v2023/create_current_mbi_procedure.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE PROCEDURE update_current_mbi()
+LANGUAGE plpgsql
+AS $$
+begin
+INSERT INTO current_mbi
+SELECT DISTINCT current_mbi from coverage_anthem_united_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_bcbs_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_centene_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_cigna1_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_cigna2_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_cvs_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_centene_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_humana_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_misc_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_mutual_dean_clear_cambia_rite_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_united1_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_united_2023
+UNION DISTINCT
+SELECT DISTINCT current_mbi from coverage_default
+UNION DISTINCT
+SELECT DISTINCT current_mbi from sandbox_2023
+ON CONFLICT DO NOTHING;
+end;
+$$;
