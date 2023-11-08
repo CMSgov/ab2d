b4f20b68b642306b2701ad8f15d61f9329e8af79
smirnovaae <annasmirnova@navapbc.com>
2023-11-08 09:58:04 -0800

    addressing pr comment

--- common/src/main/resources/db/changelog/v2023/create_current_mbi_procedure.sql
+++ common/src/main/resources/db/changelog/v2023/create_current_mbi_procedure.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE update_current_mbi()
+CREATE OR REPLACE PROCEDURE update_current_mbi_2023()
 LANGUAGE plpgsql
 AS $$
 begin
@@ -33,3 +33,40 @@ SELECT DISTINCT current_mbi from sandbox_2023
 ON CONFLICT DO NOTHING;
 end;
 $$;
+--
+-- CREATE OR REPLACE PROCEDURE update_current_mbi_2024()
+--     LANGUAGE plpgsql
+-- AS $$
+-- begin
+--     INSERT INTO current_mbi
+--     SELECT DISTINCT current_mbi from coverage_anthem_united_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_bcbs_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_centene_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_cigna1_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_cigna2_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_cvs_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_centene_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_humana_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_misc_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_mutual_dean_clear_cambia_rite_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_united1_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_united_2024
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from coverage_default
+--     UNION DISTINCT
+--     SELECT DISTINCT current_mbi from sandbox_2024
+--     ON CONFLICT DO NOTHING;
+-- end;
+-- $$;
+--
