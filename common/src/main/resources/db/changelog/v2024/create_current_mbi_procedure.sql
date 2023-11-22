729c925247ca167c6aa6e73e58ef479811707428
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2023-11-22 14:08:19 -0800

    AB2D-5819/Create cron job to run AttributionDataShare procedure on the database (#1296)
    * Creating the new partitions for the new coverage table
    Database changes for AttributionDataShare Lambda
    
    * Add pg_cron schedule
    
    * testing on dev

---
+++ common/src/main/resources/db/changelog/v2024/create_current_mbi_procedure.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE PROCEDURE update_current_mbi_2024()
+    LANGUAGE plpgsql
+AS $$
+begin
+    INSERT INTO current_mbi
+    SELECT DISTINCT current_mbi from coverage_anthem_united_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_bcbs_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_centene_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_cigna1_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_cigna2_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_cvs_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_centene_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_humana_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_misc_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_mutual_dean_clear_cambia_rite_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_united1_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_united_2024
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from coverage_default
+    UNION DISTINCT
+    SELECT DISTINCT current_mbi from sandbox_2024
+    ON CONFLICT DO NOTHING;
+end;
+$$;
