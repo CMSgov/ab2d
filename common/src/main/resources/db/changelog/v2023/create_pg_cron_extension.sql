0c9a4c2e3985c08130ed3e32575c060eebd4da68
smirnovaae <annasmirnova@navapbc.com>
2023-12-15 08:49:52 -0800

    add pg_cron

--- common/src/main/resources/db/changelog/v2023/create_pg_cron_extension.sql
+++ common/src/main/resources/db/changelog/v2023/create_pg_cron_extension.sql
@@ -1,4 +1,9 @@
 create extension if not exists pg_cron;
 
--- run job every night at 1:00 am
-SELECT cron.schedule ('update_current_mbis','*/2 * * *','call update_current_mbi()');
+-- run job every Tuesday at 7:00 am
+--SELECT cron.schedule ('update_current_mbis','0 7 * * 2','call update_current_mbi_2023()');
+
+-- job that runs every day at midnight to purge the cron.job_run_details table. The job keeps only the last seven days.
+--SELECT cron.schedule('0 0 * * *', $$DELETE
+--    FROM cron.job_run_details
+--    WHERE end_time < now() - interval '7 days'$$);
