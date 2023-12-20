b6537b97fffa92097fa71ff467eedc0f4fc0df39
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-12-20 15:31:59 -0500

    fix pg_cron schedule formating

--- common/src/main/resources/db/changelog/v2023/create_pg_cron_extension.sql
+++ common/src/main/resources/db/changelog/v2023/create_pg_cron_extension.sql
@@ -4,4 +4,4 @@ SELECT pg_reload_conf();
 CREATE EXTENSION if not exists pg_cron;
 
 -- run job every night at 1:00 am
-SELECT cron.schedule ('update_current_mbis','*/2 * * *','call update_current_mbi()');
+SELECT cron.schedule ('update_current_mbis','0 */2 * * *','call update_current_mbi()');
