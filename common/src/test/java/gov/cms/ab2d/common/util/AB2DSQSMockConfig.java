2a6da34ed960683530666f9a2961c861a45d32df
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-01-31 11:20:08 -0500

    test - comment out @Qualifier on AmazonSQS Bean

--- common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
+++ common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
@@ -4,6 +4,8 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import gov.cms.ab2d.eventclient.clients.SQSEventClient;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,7 @@ import io.awspring.cloud.messaging.listener.QueueMessageHandler;
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
+@EnableAutoConfiguration
 public class AB2DSQSMockConfig {
 
   static {
@@ -39,10 +42,11 @@ public class AB2DSQSMockConfig {
     return mock(QueueMessageHandler.class);
   }
 
-  @Bean("mockAmazonSQS")
-  @Primary
+  // @Bean("mockAmazonSQS")
+  // @Primary
   public AmazonSQSAsync amazonSQSAsync() {
-    return mock(AmazonSQSAsync.class);
+    // return mock(AmazonSQSAsync.class);
+    return amazonSQSAsync;
   }
 
   @Bean
