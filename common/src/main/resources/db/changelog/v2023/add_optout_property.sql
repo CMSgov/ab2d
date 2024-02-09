1ecb93ec1d7b363f04379ca7c575fe00d1fb6967
Sadibhatla <121067659+Sadibhatla@users.noreply.github.com>
2024-02-09 11:01:23 -0800

    Update db.changelog-master.yaml (#1311)
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * Update add_optout_property.sql
    
    * Update db.changelog-master.yaml
    
    * Update db.changelog-master.yaml
    
    * 5915 commit
    
    * 5915 commit

--- common/src/main/resources/db/changelog/v2023/add_optout_property.sql
+++ common/src/main/resources/db/changelog/v2023/add_optout_property.sql
@@ -1,4 +1,10 @@
-INSERT INTO property.properties(id, key, value, created, modified)
-VALUES (nextval('hibernate_sequence'), 'OptOutOn', 'false', current_timestamp, current_timestamp);
+INSERT INTO property.properties(
+	id, key, value, created, modified)
+SELECT nextval('hibernate_sequence'), 'OptOutOn', 'false', current_timestamp, current_timestamp
+WHERE
+NOT EXISTS (
+SELECT * FROM property.properties 
+                   WHERE key='OptOutOn'
+);
 
 --rollback DELETE FROM properties WHERE key = 'OptOutOn';
