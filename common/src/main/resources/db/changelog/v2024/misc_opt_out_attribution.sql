bec11c564fbd4345a1700d242716d99de5ef21ff
Sadibhatla <sravanti.adibhatla@gmail.com>
2024-03-20 17:27:37 -0700

    6007 and 6017-- changed liquibase files, deleted procs and boolean chk in code

--- common/src/main/resources/db/changelog/v2024/misc_opt_out_attribution.sql
+++ common/src/main/resources/db/changelog/v2024/misc_opt_out_attribution.sql
@@ -11,32 +11,39 @@ SELECT cov.contract,
        cov.opt_out_flag,
        cov.bene_coverage_period_id,
        cov.bene_coverage_search_event_id
-FROM coverage cov;
+FROM public.coverage cov;
 
-ALTER TABLE current_mbi ADD COLUMN IF NOT EXISTS opt_out_flag BOOLEAN ; --add new columns
-ALTER TABLE current_mbi ADD COLUMN IF NOT EXISTS effective_date TIMESTAMP;
+ALTER TABLE public.current_mbi ADD COLUMN IF NOT EXISTS opt_out_flag BOOLEAN ; --add new columns
+ALTER TABLE public.current_mbi ADD COLUMN IF NOT EXISTS effective_date TIMESTAMP;
 
-ALTER TABLE coverage ALTER COLUMN opt_out_flag DROP DEFAULT ; --drop default constraint
+ALTER TABLE public.coverage ALTER COLUMN opt_out_flag DROP DEFAULT ; --drop default constraint
+ALTER TABLE public.current_mbi ALTER COLUMN opt_out_flag DROP DEFAULT ;
 --update  current_mbi set opt_out_flag=NULL  RUN ONLY ONCE if table is created with default as "fault"
 
---/*******RUN ONLY ONCE WHEN TABLE IS EMPTY to pull historic data *****/
--- insert into public.current_mbi(
---     mbi, effective_date, opt_out_flag)
--- SELECT distinct current_mbi,effective_date,opt_out_flag
--- FROM coverage_view_with_mbi
--- WHERE contract  in (
---     SELECT DISTINCT(contract_number)
---     FROM job_view
---     WHERE contract_number not LIKE 'Z%'
--- )on conflict do nothing
--- /******* END ******/
-
---- /*****RUNS on schedule weekly --TODO Set pg_cron schedule and update proc name ****/
-Create or replace procedure update_current_mbi()
+--/*******RUN ONLY ONCE WHEN TABLE IS EMPTY without "effective_date> CURRENT_DATE - 1" to pull historic data *****/
+--TODO Set pg_cron schedule to get new PDP enrollments ****/
+Create or replace procedure public.insert_new_current_mbi()
 LANGUAGE plpgsql
 AS $$
 begin
-update current_mbi  set mbi =sub.current_mbi,effective_date=sub.effective_date,opt_out_flag=sub.opt_out_flag
+insert into public.current_mbi(
+    mbi, effective_date, opt_out_flag)
+SELECT distinct current_mbi,effective_date,opt_out_flag
+FROM coverage_view_with_mbi
+WHERE contract  in (
+    SELECT DISTINCT(contract_number)
+    FROM job_view
+    WHERE contract_number not LIKE 'Z%'
+) and effective_date> CURRENT_DATE - 1
+on conflict do nothing
+-- /******* END ******/
+
+--- /*****RUNS on schedule weekly --TODO Set pg_cron schedule and update proc name ****/
+Create or replace procedure public.update_current_mbi()
+LANGUAGE plpgsql
+AS $$
+begin
+update public.current_mbi  set mbi =sub.current_mbi,effective_date=sub.effective_date,opt_out_flag=sub.opt_out_flag
     from
 (SELECT distinct current_mbi,effective_date,opt_out_flag
 FROM coverage_view_with_mbi
@@ -49,5 +56,4 @@ end;
 $$;
 -- /******* END ******/
 
-Drop procedure update_current_mbi_2023();
-Drop procedure update_current_mbi_2024();
+
