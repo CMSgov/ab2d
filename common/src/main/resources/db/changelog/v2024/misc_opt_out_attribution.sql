9b7c4e9de3fae39f12995a128dee7292c73cb2ff
smirnovaae <annasmirnova@navapbc.com>
2024-04-04 11:17:46 -0700

    changed update query

--- common/src/main/resources/db/changelog/v2024/misc_opt_out_attribution.sql
+++ common/src/main/resources/db/changelog/v2024/misc_opt_out_attribution.sql
@@ -12,10 +12,12 @@ SELECT cov.contract,
 FROM public.coverage cov;
 
 ALTER TABLE public.current_mbi ADD COLUMN IF NOT EXISTS opt_out_flag BOOLEAN ; --add new columns
-ALTER TABLE public.current_mbi ADD COLUMN IF NOT EXISTS effective_date DATE;
+ALTER TABLE public.current_mbi ADD COLUMN IF NOT EXISTS effective_date TIMESTAMP;
 ALTER TABLE public.current_mbi ALTER COLUMN opt_out_flag DROP DEFAULT ;
 ALTER TABLE public.coverage DROP COLUMN opt_out_flag  ;
 ALTER TABLE public.coverage DROP COLUMN effective_date  ;
+
+ALTER TABLE current_mbi ALTER COLUMN effective_date TYPE DATE;
 
 
 --update  current_mbi set opt_out_flag=NULL  RUN ONLY ONCE if table is created with default as "fault"
