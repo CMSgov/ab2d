7d5246dd4ba2960c6b144bd377f268fa99dbc7e6
Sadibhatla <121067659+Sadibhatla@users.noreply.github.com>
2024-04-25 11:23:21 -0700

    Revert "[AB2D-5916] AWS Cloud Dependency Upgrade (#1312)" (#1324)
    This reverts commit 32d6d78859892b9eb165702787f45b31a4a92883.

--- common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
+++ common/src/test/java/gov/cms/ab2d/common/util/AB2DSQSMockConfig.java
@@ -1,22 +1,21 @@
 package gov.cms.ab2d.common.util;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import gov.cms.ab2d.eventclient.clients.SQSEventClient;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import io.awspring.cloud.messaging.config.SimpleMessageListenerContainerFactory;
-import io.awspring.cloud.messaging.listener.QueueMessageHandler;
-
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {MessagingAutoConfiguration.class, ContextStackAutoConfiguration.class})
 public class AB2DSQSMockConfig {
 
   static {
@@ -42,16 +41,8 @@ public class AB2DSQSMockConfig {
     return mock(QueueMessageHandler.class);
   }
 
-  // @Bean("mockAmazonSQS")
-  // @Primary
+  @Bean("mockAmazonSQS")
   public AmazonSQSAsync amazonSQSAsync() {
-    // return mock(AmazonSQSAsync.class);
-    return amazonSQSAsync;
-  }
-
-  @Bean
-  @Primary
-  public AWSCredentialsProvider awsCredentialsProvider() {
-    return new DefaultAWSCredentialsProviderChain();
+    return mock(AmazonSQSAsync.class);
   }
 }
