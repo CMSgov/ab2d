e581eead638830e6ff43e2b321aa7f16082bfe37
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-05-07 20:35:37 -0400

    add constant for OAuth rather than duplicating string literal in CapabilityStatementR4

--- api/src/main/java/gov/cms/ab2d/api/controller/v2/CapabilityStatementR4.java
+++ api/src/main/java/gov/cms/ab2d/api/controller/v2/CapabilityStatementR4.java
@@ -15,6 +15,7 @@ import static gov.cms.ab2d.api.controller.common.ApiText.APPLICATION_JSON;
 
 public class CapabilityStatementR4 {
     public static CapabilityStatement populateCS(String server) {
+        String OAUTH_STRING = "OAuth";
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         CapabilityStatement cs = new CapabilityStatement();
         cs.setPublisher("Centers for Medicare &amp; Medicaid Services");
@@ -59,10 +60,10 @@ public class CapabilityStatementR4 {
         CodeableConcept codeableConcept = new CodeableConcept();
         Coding coding = new Coding();
         coding.setSystem("http://hl7.org/fhir/ValueSet/restful-security-service");
-        coding.setCode("OAuth");
-        coding.setDisplay("OAuth");
+        coding.setCode(OAUTH_STRING);
+        coding.setDisplay(OAUTH_STRING);
         codeableConcept.setCoding(List.of(coding));
-        codeableConcept.setText("OAuth");
+        codeableConcept.setText(OAUTH_STRING);
         security.setService(List.of(codeableConcept));
         rest.setSecurity(security);
         List<CapabilityStatement.CapabilityStatementRestResourceOperationComponent> restComponents = new ArrayList<>();
