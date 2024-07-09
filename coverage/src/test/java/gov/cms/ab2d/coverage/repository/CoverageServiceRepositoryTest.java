1b2b68b5c721fbffa35860d74de34e12107c08c2
kai [they] <coilysiren@gmail.com>
2024-07-09 13:04:22 -0700

    [AB2D-6187] increase `coverage` test coverage (#1360)
    * CoverageServiceRepositoryTest
    
    * revert
    
    * diff reduction
    
    * testAsMembership
    
    * CoverageServiceRepositoryTest

--- coverage/src/test/java/gov/cms/ab2d/coverage/repository/CoverageServiceRepositoryTest.java
+++ coverage/src/test/java/gov/cms/ab2d/coverage/repository/CoverageServiceRepositoryTest.java
@@ -2,6 +2,9 @@ package gov.cms.ab2d.coverage.repository;
 
 import gov.cms.ab2d.common.feign.ContractFeignClient;
 import gov.cms.ab2d.common.properties.PropertiesService;
+import gov.cms.ab2d.coverage.model.ContractForCoverageDTO;
+import gov.cms.ab2d.coverage.model.CoverageMembership;
+import gov.cms.ab2d.coverage.model.CoveragePagingRequest;
 import gov.cms.ab2d.coverage.model.CoveragePeriod;
 import gov.cms.ab2d.coverage.model.CoverageSearch;
 import gov.cms.ab2d.coverage.model.CoverageSearchEvent;
@@ -22,17 +25,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.*;
 
 import static gov.cms.ab2d.common.util.PropertyConstants.OPT_OUT_ON;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 @SpringBootTest
@@ -105,10 +115,61 @@ class CoverageServiceRepositoryTest {
 
     @Test
     void yearsTest() throws NoSuchFieldException, IllegalAccessException {
-        Field X = CoverageServiceRepository.class.getDeclaredField("YEARS");
-        X.setAccessible(true);
-        Assertions.assertNotNull(X.get(null));
-        Assertions.assertTrue(X.get(null).toString().startsWith("[2020, 2021, 2022, 2023, 2024"));
+        Field field = CoverageServiceRepository.class.getDeclaredField("YEARS");
+        field.setAccessible(true);
+        Assertions.assertNotNull(field.get(null));
+        Assertions.assertTrue(field.get(null).toString().startsWith("[2020, 2021, 2022, 2023, 2024"));
+    }
+
+    @Test
+    void testVacuumCoverage() {
+        assertDoesNotThrow(() -> {
+            coverageServiceRepository.vacuumCoverage();
+        });
+
+        String vacuumCoverage = (String) ReflectionTestUtils.getField(coverageServiceRepository, "vacuumCoverage");
+        ReflectionTestUtils.setField(coverageServiceRepository, "vacuumCoverage", "broken");
+
+        assertThrows(RuntimeException.class, () -> {
+            coverageServiceRepository.vacuumCoverage();
+        });
+
+        ReflectionTestUtils.setField(coverageServiceRepository, "vacuumCoverage", vacuumCoverage);
+    }
+
+    @Test
+    void testAsMembership() throws Exception {
+        ResultSet rs = mock(ResultSet.class);
+        when(rs.getInt(4)).thenReturn(2000);
+        when(rs.getInt(5)).thenReturn(12);
+        when(rs.getLong(1)).thenReturn(9L);
+        when(rs.getString(2)).thenReturn("currentMbi");
+        when(rs.getString(3)).thenReturn("historic,mbi,string");
+
+        CoverageMembership coverageMembership = CoverageServiceRepository.asMembership(rs, 0);
+
+        assertEquals(2000, coverageMembership.getYear());
+        assertEquals(12, coverageMembership.getMonth());
+        assertEquals(9L, coverageMembership.getIdentifiers().getBeneficiaryId());
+        assertEquals("currentMbi", coverageMembership.getIdentifiers().getCurrentMbi());
+        assertEquals(
+            new LinkedHashSet<String>(Arrays.asList("historic", "mbi", "string")),
+            coverageMembership.getIdentifiers().getHistoricMbis()
+        );
+    }
+
+    @Test
+    void testPageCoverage() {
+        OffsetDateTime now = OffsetDateTime.of(1999, 1, 1, 1, 1, 1, 1, ZoneOffset.UTC);
+        ContractForCoverageDTO contract = new ContractForCoverageDTO(
+            "stuff", now, ContractForCoverageDTO.ContractType.NORMAL
+        );
+        CoveragePagingRequest page = new CoveragePagingRequest(1, 1L, contract, now);
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {coverageServiceRepository.pageCoverage(page);}
+        );
     }
 
     private Identifiers createIdentifier(Long suffix) {
