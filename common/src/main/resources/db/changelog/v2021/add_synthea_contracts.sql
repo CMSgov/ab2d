4218d277e171b3649cafcb7e77c6bb9929be8792
Kai Siren <kaisiren@navapbc.com>
2024-07-11 15:29:28 -0700

    PdpClientServiceTest

--- common/src/main/resources/db/changelog/v2021/add_synthea_contracts.sql
+++ common/src/main/resources/db/changelog/v2021/add_synthea_contracts.sql
@@ -12,6 +12,7 @@ insert into contract (id, contract_number, contract_name, attested_on, update_mo
 insert into contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1007', 'Z1007', '2020-03-01', 'TEST', NOW(), NOW());
 insert into contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1008', 'Z1008', '2020-03-01', 'TEST', NOW(), NOW());
 insert into contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1009', 'Z1009', '2020-03-01', 'TEST', NOW(), NOW());
+insert into contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1010', 'Z1010', '2020-03-01', 'TEST', NOW(), NOW());
 
 insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyx2w9Z0AntLE297', true, 1000, (select id from contract where contract_number='Z1001' limit 1), 'PDP-1001');
 insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jz0e1dyNfRMm6297', true, 1000, (select id from contract where contract_number='Z1002' limit 1), 'PDP-1002');
@@ -22,6 +23,7 @@ insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id,
 insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyzerqYlfXtsQ297', true, 1000, (select id from contract where contract_number='Z1007' limit 1), 'PDP-1007');
 insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyy4ou5c1MJ3N297', true, 1000, (select id from contract where contract_number='Z1008' limit 1), 'PDP-1008');
 insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyw49f9U19vvB297', true, 1000, (select id from contract where contract_number='Z1009' limit 1), 'PDP-1009');
+insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyw49f9U20vvB297', true, 1000, (select id from contract where contract_number='Z1010' limit 1), 'PDP-1010');
 
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyx2w9Z0AntLE297' limit 1), (select id from role where name='SPONSOR' limit 1));
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jz0e1dyNfRMm6297' limit 1), (select id from role where name='SPONSOR' limit 1));
@@ -32,9 +34,10 @@ insert into user_role(user_account_id, role_id) values ((select id from user_acc
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyzerqYlfXtsQ297' limit 1), (select id from role where name='SPONSOR' limit 1));
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyy4ou5c1MJ3N297' limit 1), (select id from role where name='SPONSOR' limit 1));
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyw49f9U19vvB297' limit 1), (select id from role where name='SPONSOR' limit 1));
+insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyw49f9U20vvB297' limit 1), (select id from role where name='ADMIN' limit 1));
 
 ALTER TABLE coverage DETACH PARTITION sandbox;
 ALTER TABLE coverage ATTACH PARTITION sandbox FOR VALUES IN
-    ('Z0000', 'Z0001', 'Z0002', 'Z0005', 'Z0010', 'Z1001', 'Z1002', 'Z1003', 'Z1004', 'Z1005', 'Z1006', 'Z1007', 'Z1008', 'Z1009');
+    ('Z0000', 'Z0001', 'Z0002', 'Z0005', 'Z0010', 'Z1001', 'Z1002', 'Z1003', 'Z1004', 'Z1005', 'Z1006', 'Z1007', 'Z1008', 'Z1009', 'Z1010');
 
 CREATE TABLE coverage_default PARTITION OF coverage DEFAULT;
