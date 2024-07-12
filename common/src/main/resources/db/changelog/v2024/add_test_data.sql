d0f21de9aaa6442cab8deb1deec76f8e2f4c581d
Kai Siren <kaisiren@navapbc.com>
2024-07-12 11:09:54 -0700

    fix migration

--- common/src/main/resources/db/changelog/v2024/add_test_data.sql
+++ common/src/main/resources/db/changelog/v2024/add_test_data.sql
@@ -3,11 +3,8 @@
 
 --changeset lsharshar:AB2D-3874 failOnError:true
 
-insert into contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1010', 'Z1010', '2020-03-01', 'TEST', NOW(), NOW());
+insert into Contract.contract (id, contract_number, contract_name, attested_on, update_mode, created, modified) values ((select nextval('hibernate_sequence')), 'Z1010', 'Z1010', '2020-03-01', 'TEST', NOW(), NOW());
 
-insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyw49f9U20vvB297', true, 1000, (select id from contract where contract_number='Z1010' limit 1), 'PDP-1010');
+insert into user_account(id, client_id, enabled, max_parallel_jobs, contract_id, organization) values((select nextval('hibernate_sequence')), '0oa9jyw49f9U20vvB297', true, 1000, (select id from Contract.contract where contract_number='Z1010' limit 1), 'PDP-1010');
 
 insert into user_role(user_account_id, role_id) values ((select id from user_account where client_id='0oa9jyw49f9U20vvB297' limit 1), (select id from role where name='ADMIN' limit 1));
-
-ALTER TABLE coverage ATTACH PARTITION sandbox FOR VALUES IN ('Z1010');
-
