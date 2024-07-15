f49f1dcc9f5b7213c4596776cbe6a0678459cbbb
smirnovaae <annasmirnova@navapbc.com>
2024-07-15 12:49:48 -0700

    add test

--- api/src/test/java/gov/cms/ab2d/api/controller/common/ApiCommonTest.java
+++ api/src/test/java/gov/cms/ab2d/api/controller/common/ApiCommonTest.java
@@ -1,16 +1,18 @@
 package gov.cms.ab2d.api.controller.common;
 
-import gov.cms.ab2d.contracts.model.Contract;
 import gov.cms.ab2d.common.model.PdpClient;
 import gov.cms.ab2d.common.service.ContractService;
+import gov.cms.ab2d.common.service.InvalidClientInputException;
 import gov.cms.ab2d.common.service.InvalidContractException;
 import gov.cms.ab2d.common.service.PdpClientService;
+import gov.cms.ab2d.contracts.model.Contract;
 import gov.cms.ab2d.fhir.FhirVersion;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.OffsetDateTime;
+
+import static gov.cms.ab2d.common.util.Constants.SINCE_EARLIEST_DATE_TIME;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -41,7 +43,7 @@ class ApiCommonTest {
     void unattestedCheck() {
         InvalidContractException ice = assertThrows(InvalidContractException.class, () ->
                 apiCommon.checkValidCreateJob(null, CONTRACT_NUMBER, null, null,
-        "resource_type", "jpg", FhirVersion.STU3));
+                        "resource_type", "jpg", FhirVersion.STU3));
         assertNotNull(ice);
         assertTrue(ice.getMessage().contains(CONTRACT_NUMBER + " is not attested."));
     }
@@ -54,6 +56,24 @@ class ApiCommonTest {
                         "resource_type", "jpg", FhirVersion.STU3));
         assertNotNull(ice);
         assertTrue(ice.getMessage().contains(bogusContractNumber + " not associated with internal id"));
+    }
+
+    @Test
+    void checkSinceTimeTest() {
+        assertDoesNotThrow(() -> apiCommon.checkSinceTime(SINCE_EARLIEST_DATE_TIME));
+        assertDoesNotThrow(() -> apiCommon.checkSinceTime(null));
+        assertThrows(InvalidClientInputException.class, () -> apiCommon.checkSinceTime(OffsetDateTime.now().plusMonths(1)));
+        assertThrows(InvalidClientInputException.class, () -> apiCommon.checkSinceTime(SINCE_EARLIEST_DATE_TIME.minusMonths(1)));
+    }
+
+    @Test
+    void checkUntilTimeTest() {
+        OffsetDateTime currentDate = OffsetDateTime.now();
+        assertDoesNotThrow(() -> apiCommon.checkUntilTime(SINCE_EARLIEST_DATE_TIME, currentDate, FhirVersion.R4));
+        assertDoesNotThrow(() -> apiCommon.checkUntilTime(SINCE_EARLIEST_DATE_TIME, null, FhirVersion.R4));
+        assertThrows(InvalidClientInputException.class, () -> apiCommon.checkUntilTime(SINCE_EARLIEST_DATE_TIME, currentDate, FhirVersion.STU3));
+        assertThrows(InvalidClientInputException.class, () -> apiCommon.checkUntilTime(currentDate, SINCE_EARLIEST_DATE_TIME, FhirVersion.R4));
+        assertThrows(InvalidClientInputException.class, () -> apiCommon.checkUntilTime(null, SINCE_EARLIEST_DATE_TIME.minusMonths(1), FhirVersion.R4));
     }
 
     private ApiCommon buildApiCommon(ContractService contractService) {
