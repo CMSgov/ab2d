ba73f1f567fa8d61a5dd1f027aa969053ff5dd00
kai [they] <coilysiren@gmail.com>
2024-07-22 11:30:39 -0700

    [AB2D-6190] increase `worker` test coverage (#1370)
    * AutoScalingServiceTest
    
    * JobHandlerTest
    
    * RoundRobinBlockingQueueTest
    
    * RoundRobinBlockingQueueTest
    
    * RoundRobinBlockingQueueTest

--- worker/src/test/java/gov/cms/ab2d/worker/config/JobHandlerTest.java
+++ worker/src/test/java/gov/cms/ab2d/worker/config/JobHandlerTest.java
@@ -3,12 +3,14 @@ package gov.cms.ab2d.worker.config;
 import gov.cms.ab2d.job.model.Job;
 import gov.cms.ab2d.job.model.JobStatus;
 import gov.cms.ab2d.common.service.FeatureEngagement;
+import gov.cms.ab2d.common.service.ResourceNotFoundException;
 import gov.cms.ab2d.worker.service.WorkerService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.integration.support.locks.LockRegistry;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 
 
@@ -18,6 +20,7 @@ import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,6 +54,25 @@ class JobHandlerTest {
         verify(workerService, times(0)).process(anyString());
     }
 
+    @Test
+    void testEmptyPayload() {
+
+        ReentrantLock lock = new ReentrantLock();
+        when(workerService.getEngagement()).thenReturn(FeatureEngagement.IN_GEAR);
+
+        JobHandler jobHandler = new JobHandler(lockRegistry, workerService);
+
+        List<Map<String, Object>> payload = List.of();
+
+        jobHandler.handleMessage(new GenericMessage<>(payload));
+
+        assertFalse(lock.isLocked());
+
+        verify(workerService, times(1)).getEngagement();
+        verify(workerService, times(0)).process(anyString());
+    }
+
+
     @DisplayName("Job is started if worker is set to in gear")
     @Test
     void processingTriggeredInGear() {
@@ -76,6 +98,32 @@ class JobHandlerTest {
 
         verify(workerService, times(1)).getEngagement();
         verify(workerService, times(1)).process(anyString());
+    }
+
+    @Test
+    void testResourceNotFoundException() {
+        Job submittedJob = new Job();
+        submittedJob.setStatus(JobStatus.IN_PROGRESS);
+
+        ReentrantLock lock = new ReentrantLock();
+        when(workerService.getEngagement()).thenReturn(FeatureEngagement.IN_GEAR);
+        when(lockRegistry.obtain(anyString())).thenReturn(lock);
+        when(workerService.process(anyString())).thenThrow(ResourceNotFoundException.class);
+
+        JobHandler jobHandler = new JobHandler(lockRegistry, workerService);
+
+        Map<String, Object> jobMap = new HashMap<>() {{
+            put("job_uuid", "DoesNotMatter");
+        }};
+        List<Map<String, Object>> payload = List.of(jobMap);
+        GenericMessage<List<Map<String, Object>>> message = new GenericMessage<>(payload);
+
+        assertThrows(
+            MessagingException.class,
+            () -> {
+                jobHandler.handleMessage(message);
+            }
+        );
     }
 
     @DisplayName("Handler attempts to start jobs until it finds one that it can start")
