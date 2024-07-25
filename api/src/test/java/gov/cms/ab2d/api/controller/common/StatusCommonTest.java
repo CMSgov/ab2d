1579207d4970858efe7cdc7b91bfe310b5b7eded
Kai Siren <kaisiren@navapbc.com>
2024-07-25 15:03:29 -0700

    StatusCommonTest

--- api/src/test/java/gov/cms/ab2d/api/controller/common/StatusCommonTest.java
+++ api/src/test/java/gov/cms/ab2d/api/controller/common/StatusCommonTest.java
@@ -1,12 +1,101 @@
 package gov.cms.ab2d.api.controller.common;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.OffsetDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import gov.cms.ab2d.api.controller.JobProcessingException;
+import gov.cms.ab2d.api.remote.JobClient;
+import gov.cms.ab2d.common.model.PdpClient;
+import gov.cms.ab2d.common.service.PdpClientService;
+import gov.cms.ab2d.eventclient.clients.SQSEventClient;
+import gov.cms.ab2d.job.dto.JobPollResult;
+import gov.cms.ab2d.job.model.JobStatus;
 
 class StatusCommonTest {
 
-  @Test
-  void test() {
+  StatusCommon statusCommon;
+  PdpClientService pdpClientService;
+  PdpClient pdpClient;
+  JobClient jobClient;
+  JobPollResult jobPollResult;
+  SQSEventClient eventLogger;
+  MockHttpServletRequest req;
 
+  @BeforeEach
+  void beforeEach() {
+    pdpClientService = mock(PdpClientService.class);
+    pdpClient = mock(PdpClient.class);
+    when(pdpClientService.getCurrentClient()).thenReturn(pdpClient);
+
+    jobClient = mock(JobClient.class);
+    jobPollResult = mock(JobPollResult.class);
+    when(jobClient.poll(anyBoolean(), any(), any(), anyInt())).thenReturn(jobPollResult);
+    when(jobPollResult.getExpiresAt()).thenReturn(OffsetDateTime.now());
+
+    eventLogger = mock(SQSEventClient.class);
+    statusCommon = new StatusCommon(pdpClientService, jobClient, eventLogger, 0);
+
+    req = new MockHttpServletRequest();
+  }
+
+  @Test
+  void testThrowFailedResponse() {
+    assertThrows(JobProcessingException.class, () -> {
+      statusCommon.throwFailedResponse("error!");
+    });
+  }
+
+  @Test
+  void testDoStatus1() {
+    when(jobPollResult.getStatus()).thenReturn(JobStatus.SUCCESSFUL);
+
+    assertNotNull(
+      statusCommon.doStatus("1234", req, "prefix")
+    );
+  }
+
+  @Test
+  void testDoStatus2() {
+    when(jobPollResult.getStatus()).thenReturn(JobStatus.SUBMITTED);
+    assertNotNull(
+      statusCommon.doStatus("1234", req, "prefix")
+    );
+  }
+
+  @Test
+  void testDoStatus3() {
+    when(jobPollResult.getStatus()).thenReturn(JobStatus.IN_PROGRESS);
+    assertNotNull(
+      statusCommon.doStatus("1234", req, "prefix")
+    );
+  }
+
+  @Test
+  void testDoStatus4() {
+    when(jobPollResult.getStatus()).thenReturn(JobStatus.FAILED);
+    assertThrows(JobProcessingException.class, () -> {
+      statusCommon.doStatus("1234", req, "prefix");
+    });
+  }
+
+  @Test
+  void testDoStatus5() {
+    when(jobPollResult.getStatus()).thenReturn(JobStatus.CANCELLED);
+    assertThrows(JobProcessingException.class, () -> {
+      statusCommon.doStatus("1234", req, "prefix");
+    });
   }
 
 }
