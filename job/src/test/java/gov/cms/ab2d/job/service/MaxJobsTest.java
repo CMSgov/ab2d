588155152e207406fc55ed6d6f1272198f9104e0
smirnovaae <annasmirnova@navapbc.com>
2024-08-14 14:46:29 -0700

    fix unit test

--- job/src/test/java/gov/cms/ab2d/job/service/MaxJobsTest.java
+++ job/src/test/java/gov/cms/ab2d/job/service/MaxJobsTest.java
@@ -79,7 +79,7 @@ class MaxJobsTest extends JobCleanup {
         Contract contract = contractServiceStub.getAllAttestedContracts().iterator().next();
         String organization = pdpClientService.getCurrentClient().getOrganization();
         startJobDTO = new StartJobDTO(contract.getContractNumber(), organization,
-                EOB, LOCAL_HOST, NDJSON_FIRE_CONTENT_TYPE, null, null, STU3);
+                EOB, LOCAL_HOST, FHIR_NDJSON_CONTENT_TYPE, null, STU3);
         for (int idx = 0; idx < MAX_JOBS_PER_CLIENT; idx++) {
             Job retJob = jobService.createJob(startJobDTO);
             assertNotNull(retJob);
