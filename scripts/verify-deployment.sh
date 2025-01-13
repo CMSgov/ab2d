8052b3ea98672e1814fd003bfbb605942abaa437
oluwolenpbc <127983913+oluwolenpbc@users.noreply.github.com>
2025-01-13 11:58:10 -0500

    paramaterize verify script

--- scripts/verify-deployment.sh
+++ scripts/verify-deployment.sh
@@ -4,7 +4,12 @@ set -x  # Enable verbose mode for debugging
 
 sleep 30
 
-BASIC_AUTH=$(aws secretsmanager get-secret-value --secret-id ab2d/ab2d-east-impl/jenkins-verify-api --query SecretString --output text) || { echo "Failed to retrieve secret"; exit 3; }
+#  ENv Parameters
+SECRET_ID="${1}"  
+BASE_URL="${2}"  
+EXPORT_URL="${3}"
+
+BASIC_AUTH=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --query SecretString --output text) || { echo "Failed to retrieve secret"; exit 3; }
 
 echo "Checking status"
 max_attempts=90
@@ -16,7 +21,7 @@ while [ "$STATE" != "401" ]; do
         echo "Max attempts reached timing out"
         exit 1
     fi
-    STATE=$(curl -sLk -w "%{http_code}" "https://impl.ab2d.cms.gov" -o /dev/null) || { echo "Curl request failed"; exit 3; }
+    STATE=$(curl -sLk -w "%{http_code}" "$BASE_URL" -o /dev/null) || { echo "Curl request failed"; exit 3; }
     echo "$STATE"
     if [ "$STATE" != "401" ]; then
         echo 'Unreachable, sleeping for 10 seconds . . .'
@@ -35,7 +40,7 @@ TOKEN=$(curl -s --location --request POST 'https://test.idp.idm.cms.gov/oauth2/a
 | jq -r '.access_token') || { echo "Token refresh failed"; exit 3; }
 
 echo "Starting PDP-100 job"
-JOB=$(curl -k -s --head --location --request GET 'https://impl.ab2d.cms.gov/api/v2/fhir/Patient/$export?_type=ExplanationOfBenefit&_since=2020-02-13T00:00:00.000-05:00&_outputFormat=application%2Ffhir%2Bndjson' \
+JOB=$(curl -k -s --head --location --request GET "$EXPORT_URL" \
 --header "Prefer: respond-async" \
 --header "Authorization: Bearer $TOKEN" \
 | awk -v FS=": " '/^content-location/{print $2}' | sed 's/content-location: //' | tr -d '\r') || { echo "Failed to start job"; exit 3; }
