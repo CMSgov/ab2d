ca190a96ad2f3fe7ed27654257ab5e8fbd397870
smirnovaae <annasmirnova@navapbc.com>
2025-02-14 10:51:56 -0800

    added sequence

--- common/src/main/java/gov/cms/ab2d/common/model/PdpClient.java
+++ common/src/main/java/gov/cms/ab2d/common/model/PdpClient.java
@@ -5,15 +5,8 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -34,7 +27,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class PdpClient extends TimestampBase implements UserDetails {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_account_seq")
+    @SequenceGenerator(name = "user_account_seq", sequenceName = "public.user_account_seq", allocationSize = 1)
     @EqualsAndHashCode.Include
     private Long id;
 
