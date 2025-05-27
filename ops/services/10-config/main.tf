964df1a24031a402264c38bed61ecb9f0ed5bb05
Michael J Burling <michael.burling@cms.hhs.gov>
2025-05-27 14:27:29 -0500

    PLT-1099: Introduce sops-based configuration

---
+++ ops/services/10-config/main.tf
@@ -0,0 +1,25 @@
+locals {
+  service      = "config"
+  default_tags = module.platform.default_tags
+}
+
+module "platform" {
+  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/platform?ref=tofu"
+  providers = { aws = aws, aws.secondary = aws.secondary }
+
+  app         = local.app
+  env         = terraform.workspace
+  root_module = "https://github.com/CMSgov/ab2d/tree/main/ops/services/01-config" #FIXME
+  service     = local.service
+}
+
+#TODO
+module "sops" {
+  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/sops?ref=PLT-1099"
+
+  platform = module.platform
+}
+
+output "edit" {
+  value = module.sops.sopsw
+}
