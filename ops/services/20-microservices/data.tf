2d29084a1b521fc79d21e1549d6fe129722e9c11
Michael J Burling <michael.burling@cms.hhs.gov>
2025-05-28 12:36:32 -0500

    AB2D-6692: Support Immutable Microservice Tags

---
+++ ops/services/20-microservices/data.tf
@@ -0,0 +1,71 @@
+data "aws_security_group" "api" {
+  name   = "${local.service_prefix}-api-sg"
+  vpc_id = local.vpc_id
+}
+
+data "aws_security_group" "worker" {
+  name   = "${local.service_prefix}-worker-sg"
+  vpc_id = local.vpc_id
+}
+
+data "aws_security_group" "lambda" {
+  name   = "${local.service_prefix}-microservice-lambda-sg"
+  vpc_id = local.vpc_id
+}
+
+data "aws_db_instance" "this" {
+  db_instance_identifier = local.service_prefix
+}
+
+data "aws_ssm_parameter" "database_name" { #TODO: this is derivable
+  name = "/ab2d/${local.env}/core/nonsensitive/database_name"
+}
+
+data "aws_ssm_parameter" "database_password" {
+  name = "/ab2d/${local.env}/core/sensitive/database_password"
+}
+
+data "aws_ssm_parameter" "database_user" {
+  name = "/ab2d/${local.env}/core/sensitive/database_user"
+}
+
+data "aws_ssm_parameter" "keystore_password" {
+  name = "/ab2d/${local.env}/core/sensitive/keystore_password"
+}
+
+data "aws_ssm_parameter" "keystore_location" { #TODO: this appears to be an invariant
+  name = "/ab2d/${local.env}/core/nonsensitive/keystore_location"
+}
+
+data "aws_ssm_parameter" "okta_jwt_issuer" {
+  name = "/ab2d/${local.env}/core/sensitive/okta_jwt_issuer"
+}
+
+data "aws_ssm_parameter" "slack_alert_webhooks" {
+  name = "/ab2d/${local.env}/common/sensitive/slack_alert_webhooks"
+}
+
+data "aws_ssm_parameter" "slack_trace_webhooks" {
+  name = "/ab2d/${local.env}/common/sensitive/slack_trace_webhooks"
+}
+
+###### Contracts Configuration #########
+data "aws_ssm_parameter" "hpms_url" {
+  name = "/ab2d/${local.env}/core/sensitive/hpms_url"
+}
+
+data "aws_ssm_parameter" "hpms_api_params" {
+  name = "/ab2d/${local.env}/core/sensitive/hpms_api_params"
+}
+
+data "aws_ssm_parameter" "hpms_auth_key_id" {
+  name = "/ab2d/${local.env}/core/sensitive/hpms_auth_key_id"
+}
+
+data "aws_ssm_parameter" "hpms_auth_key_secret" {
+  name = "/ab2d/${local.env}/core/sensitive/hpms_auth_key_secret"
+}
+
+data "aws_iam_role" "task_execution_role" {
+  name = "ab2d-${local.env}-microservice-task-definition-role"
+}
