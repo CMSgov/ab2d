f78afa3d9a5b8392d727e2c8fd30bd5a3c5bb431
Michael J Burling <michael.burling@cms.hhs.gov>
2025-05-30 15:04:35 -0500

    Use more modern sopsw

--- ops/services/10-config/main.tf
+++ ops/services/10-config/main.tf
@@ -13,7 +13,7 @@ locals {
 }
 
 module "platform" {
-  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/platform?ref=267771f3414c92e2f3090616587550e26bc41a47"
+  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/platform?ref=PLT-1099"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
   app         = local.app
@@ -23,7 +23,7 @@ module "platform" {
 }
 
 module "sops" {
-  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/sops?ref=267771f3414c92e2f3090616587550e26bc41a47"
+  source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/sops?ref=PLT-1099"
 
   platform = module.platform
 }
