daa30793edd166f14ddf3374a0dc5f114b1c5940
Michael J Burling <michael.burling@cms.hhs.gov>
2025-05-30 12:37:41 -0500

    Move required providers out of root tofu

--- ops/services/10-config/main.tf
+++ ops/services/10-config/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+}
+
 locals {
   service      = "config"
   default_tags = module.platform.default_tags
