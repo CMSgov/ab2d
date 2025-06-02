477b105c5601e9664e5181a3832c5d6db4dca55a
Michael J Burling <michael.burling@cms.hhs.gov>
2025-06-02 16:09:28 -0500

    Fix conflicting configuration in ecr image lookups

--- ops/services/20-microservices/data.tf
+++ ops/services/20-microservices/data.tf
@@ -32,17 +32,17 @@ data "aws_iam_role" "task_execution_role" {
 data "aws_ecr_image" "contracts" {
   repository_name = "ab2d-contracts"
   image_tag       = var.contracts_service_image_tag
-  most_recent     = true
+  most_recent     = var.contracts_service_image_tag == null ? true : null
 }
 
 data "aws_ecr_image" "events" {
   repository_name = "ab2d-events"
   image_tag       = var.events_service_image_tag
-  most_recent     = true
+  most_recent     = var.events_service_image_tag == null ? true : null
 }
 
 data "aws_ecr_image" "properties" {
   repository_name = "ab2d-properties"
   image_tag       = var.properties_service_image_tag
-  most_recent     = true
+  most_recent     = var.properties_service_image_tag == null ? true : null
 }
