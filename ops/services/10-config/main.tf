d222a9e0da55c40ce0aa8db69b6a813a9c5cab3a
Michael J Burling <michael.burling@cms.hhs.gov>
2025-06-02 12:45:33 -0500

    Begin standardizing root and platform usage

--- ops/services/10-config/main.tf
+++ ops/services/10-config/main.tf
@@ -7,19 +7,20 @@ terraform {
   }
 }
 
-locals {
-  service      = "config"
-  default_tags = module.platform.default_tags
-}
-
 module "platform" {
   source = "git::https://github.com/CMSgov/ab2d-bcda-dpc-platform.git//terraform/modules/platform?ref=PLT-1099"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
   app         = local.app
-  env         = terraform.workspace
+  env         = local.env
   root_module = "https://github.com/CMSgov/ab2d/tree/main/ops/services/10-config"
   service     = local.service
+}
+
+locals {
+  default_tags = module.platform.default_tags
+  env          = terraform.workspace
+  service      = "config"
 }
 
 module "sops" {
@@ -27,6 +28,7 @@ module "sops" {
 
   platform = module.platform
 }
+
 
 output "edit" {
   value = module.sops.sopsw
