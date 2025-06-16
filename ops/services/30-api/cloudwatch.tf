eeb47ac1cc74ea0a3739aa905dc81cdd3b439263
Michael J Burling <michael.burling@cms.hhs.gov>
2025-06-16 14:51:30 -0500

    Remove commented out ASG metrics

--- ops/services/30-api/cloudwatch.tf
+++ ops/services/30-api/cloudwatch.tf
@@ -1,19 +1,3 @@
-# resource "aws_cloudwatch_metric_alarm" "app_cpu_alarm" {
-#   alarm_name          = "${local.service_prefix}-app-cpu-alarm"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = "2"
-#   metric_name         = "CPUUtilization"
-#   namespace           = "AWS/EC2"
-#   period              = "300"
-#   statistic           = "Average"
-#   threshold           = "90.0"
-#   alarm_actions       = [local.cloudwatch_sns_topic]
-
-#   dimensions = {
-#     AutoScalingGroupName = aws_autoscaling_group.asg.name
-#   }
-# }
-
 resource "aws_cloudwatch_metric_alarm" "target_response_time" {
   alarm_name          = "${local.service_prefix}-target-response-time"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -45,21 +29,6 @@ resource "aws_cloudwatch_metric_alarm" "healthy_host_count" {
     TargetGroup  = aws_lb_target_group.ab2d_api.arn_suffix
   }
 }
-
-# resource "aws_cloudwatch_metric_alarm" "app_status_check_alarm" {
-#   alarm_name          = "${local.service_prefix}-app-status-check-alarm"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "StatusCheckFailed"
-#   namespace           = "AWS/EC2"
-#   period              = "300"
-#   statistic           = "Maximum"
-#   threshold           = "1.0"
-#   alarm_actions       = [local.cloudwatch_sns_topic]
-#   dimensions = {
-#     AutoScalingGroupName = aws_autoscaling_group.asg.name
-#   }
-# }
 
 resource "aws_cloudwatch_metric_alarm" "app_elb_http_code_elb_4xx_count" {
   alarm_name          = "${local.service_prefix}-app-elb-http-code-elb_4xx_count"
