bde920654b51f918d619f734e59f7f5eb7f894ed
Ben Hesford <benhesford@navapbc.com>
2025-06-30 10:22:33 -0700

    Clean up

--- common/src/main/resources/db/changelog/v2025/test_insert.sql
+++
@@ -1,2 +0,0 @@
--- Test insert with explicit schema
-INSERT INTO ab2d.role (id, name) VALUES ((select nextval('hibernate_sequence')), 'TEST1');
--- common/src/main/resources/db/changelog/v2025/test_insert_2.sql
+++
@@ -1,2 +0,0 @@
--- Test insert without explicit schema
-INSERT INTO role (id, name) VALUES ((select nextval('hibernate_sequence')), 'TEST2');
