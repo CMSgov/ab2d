18b43286d2cf3de7b756cb1b7af21f81e68fd2a4
Michael J Burling <michael.burling@cms.hhs.gov>
2025-06-30 19:30:32 -0500

    AB2D-6746: Encode account level test credentials (#1520)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/AB2D-6746
    
    ## 🛠 Changes
    Here, we're introducing a work-in-progress `00-bootstrap` terraservice
    module that serves AB2D at the AWS Account Level, encoding AB2D's
    account-level resources in ECR and SSM.
    
    We're also migrating from usage of the unmanaged ssm values for the PDP
    test credentials to those defined in `00-bootstrap`.
    
    ## ℹ️ Context
    Post greenfield migration.
    
    ## 🧪 Validation
    * manual dispatch of e2e-test-gf.yml ran successfully against `test`
    * manual dispatch of e2e-test-gf.yml ran successfully against `sandbox`

---
+++ ops/services/00-bootstrap/tofu.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+}
+
+locals {
+  app              = "ab2d"
+  established_envs = ["test", "prod"]
+  parent_env = coalesce(
+    var.parent_env,
+    contains(local.established_envs, terraform.workspace) ? terraform.workspace : null,
+    "invalid-parent-environment;do-better"
+  )
+
+  state_buckets = {
+    test    = "ab2d-test-tfstate-20250410134820763500000001"
+    prod    = "ab2d-prod-tfstate-20250411202936776600000001"
+  }
+}
+
+variable "region" {
+  default  = "us-east-1"
+  nullable = false
+}
+
+variable "secondary_region" {
+  default  = "us-west-2"
+  nullable = false
+}
+
+variable "parent_env" {
+  description = <<-EOF
+  The parent environment of the current solution. Will correspond with `terraform.workspace`".
+  Necessary on `tofu init` and `tofu workspace select` _only_. In all other situations, parent env
+  will be divined from `terraform.workspace`.
+  EOF
+  type        = string
+  nullable    = true
+  default     = null
+  validation {
+    condition     = var.parent_env == null || one([for x in local.established_envs : x if var.parent_env == x && x == terraform.workspace]) != null
+    error_message = "Invalid parent environment name."
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+provider "aws" {
+  alias = "secondary"
+
+  region = var.secondary_region
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket       = local.state_buckets[local.parent_env]
+    key          = "ops/services/${local.service}/tofu.tfstate"
+    region       = var.region
+    encrypt      = true
+    kms_key_id   = "alias/${local.app}-${local.parent_env}-tfstate-bucket"
+    use_lockfile = true
+  }
+}
