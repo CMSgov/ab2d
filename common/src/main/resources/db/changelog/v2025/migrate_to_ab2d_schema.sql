e626b602ceb2558f9541d748e6cc1e86c4582481
Ben Hesford <benhesford@navapbc.com>
2025-07-07 15:20:24 -0700

    WIP

--- common/src/main/resources/db/changelog/v2025/migrate_to_ab2d_schema.sql
+++ common/src/main/resources/db/changelog/v2025/migrate_to_ab2d_schema.sql
@@ -1,6 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS ab2d;
 ALTER ROLE cmsadmin SET search_path TO ab2d,public;
-SET search_path TO ab2d,public;
 
 -- Move tables except databasechangelog and databasechangeloglock under 'public'
 DO $$
