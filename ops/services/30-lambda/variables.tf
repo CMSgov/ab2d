94489733744b81a5c88e150b7bd94795b9b09be4
Michael J Burling <michael.burling@cms.hhs.gov>
2025-07-16 11:53:11 -0500

    tofu fmt on 30-lambda

--- ops/services/30-lambda/variables.tf
+++ ops/services/30-lambda/variables.tf
@@ -1,5 +1,5 @@
 variable "release_version" {
-  default = "1.1.0-aurora"
+  default     = "1.1.0-aurora"
   description = "Release version for common lambdas stored in artifactory"
-  type    = string
+  type        = string
 }
