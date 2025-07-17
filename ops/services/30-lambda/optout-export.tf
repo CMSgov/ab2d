5cf9a71bbf81f0ed1adb9eee3c39e09ec0ad22dd
Michael J Burling <michael.burling@cms.hhs.gov>
2025-07-17 08:08:37 -0500

    AB2D-6801: Manage optout lambda infrastructure

---
+++ ops/services/30-lambda/optout-export.tf
@@ -0,0 +1,179 @@
+locals {
+  export_schedule = lookup(
+    {
+      prod = "cron(0 1 ? * WED *)"
+      test = "cron(0 13 ? * * *)"
+  }, local.env, "")
+}
+
+resource "aws_iam_role" "export" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "lambda.amazonaws.com"
+          }
+        },
+        {
+          Action = [
+            "sts:TagSession",
+            "sts:AssumeRoleWithWebIdentity",
+          ]
+          Condition = {
+            StringEquals = {
+              "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            }
+            StringLike = {
+              "token.actions.githubusercontent.com:sub" = "repo:CMSgov/ab2d-lambdas:*"
+            }
+          }
+          Effect = "Allow"
+          Principal = {
+            Federated = "arn:aws:iam::${module.platform.aws_caller_identity.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          }
+        },
+        {
+          Action = [
+            "sts:TagSession",
+            "sts:AssumeRole",
+          ]
+          Effect = "Allow"
+          Principal = {
+            AWS = [for role in module.platform.kion_roles : role.arn]
+          }
+
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  force_detach_policies = false
+  max_session_duration  = 3600
+  name                  = "${local.service_prefix}-opt-out-export-function"
+  path                  = "/delegatedadmin/developer/"
+  permissions_boundary  = "arn:aws:iam::${module.platform.aws_caller_identity.account_id}:policy/cms-cloud-admin/developer-boundary-policy"
+
+  inline_policy {
+    name = "assume-bucket-role"
+    policy = jsonencode(
+      {
+        Statement = [
+          {
+            Action   = "sts:AssumeRole"
+            Effect   = "Allow"
+            Resource = module.platform.ssm.eft.bfd-bucket-role-arn.value
+          },
+        ]
+        Version = "2012-10-17"
+      }
+    )
+  }
+
+  inline_policy {
+    name = "default-function"
+    policy = jsonencode(
+      {
+        Statement = [
+          {
+            Action = [
+              "ssm:GetParameters",
+              "ssm:GetParameter",
+              "sqs:ReceiveMessage",
+              "sqs:GetQueueAttributes",
+              "sqs:DeleteMessage",
+              "logs:PutLogEvents",
+              "logs:CreateLogStream",
+              "logs:CreateLogGroup",
+              "kms:Encrypt",
+              "kms:Decrypt",
+              "ec2:DescribeNetworkInterfaces",
+              "ec2:DescribeAccountAttributes",
+              "ec2:DeleteNetworkInterface",
+              "ec2:CreateNetworkInterface",
+            ]
+            Effect   = "Allow"
+            Resource = "*"
+          },
+        ]
+        Version = "2012-10-17"
+      }
+    )
+  }
+}
+
+resource "aws_lambda_function" "export" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  filename                       = "${path.root}/optout.zip"
+  description                    = "Exports data files to a BFD bucket for opt-out"
+  function_name                  = "${local.service_prefix}-opt-out-export"
+  handler                        = "gov.cms.ab2d.attributiondatashare.AttributionDataShareHandler"
+  kms_key_arn                    = local.env_key_alias.target_key_arn
+  memory_size                    = 10240
+  timeout                        = 900
+  package_type                   = "Zip"
+  reserved_concurrent_executions = -1
+  role                           = aws_iam_role.export[0].arn
+  runtime                        = "java17"
+  skip_destroy                   = false
+  tags = {
+    code = "https://github.com/CMSgov/ab2d-lambdas/tree/main/optout"
+  }
+
+  environment {
+    variables = {
+      APP_NAME         = "${local.service_prefix}-opt-out-export"
+      DB_HOST          = "jdbc:postgresql://${local.ab2d_db_host}:${local.db_port}/${local.db_name}?sslmode=${local.ab2d_db_ssl_mode}%ApplicationName=${local.service_prefix}-opt-out-export"
+      ENV              = local.env
+      S3_UPLOAD_BUCKET = "bfd-${local.env}-eft"
+      S3_UPLOAD_PATH   = "bfdeft01/ab2d/out"
+    }
+  }
+
+  ephemeral_storage {
+    size = 512
+  }
+
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/${local.service_prefix}-opt-out-export"
+  }
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+  vpc_config {
+    security_group_ids = [local.db_sg_id, aws_security_group.database_lambda.id]
+    subnet_ids         = local.node_subnet_ids
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "export" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  name                = "${aws_lambda_function.export[0].function_name}-function"
+  description         = "Trigger ${aws_lambda_function.export[0].function_name} function"
+  schedule_expression = local.export_schedule
+}
+
+resource "aws_cloudwatch_event_target" "export" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  arn  = aws_lambda_function.export[0].arn
+  rule = aws_cloudwatch_event_rule.export[0].name
+}
+
+resource "aws_lambda_permission" "export" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  statement_id  = "AllowExecutionFromCloudWatchEvents"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.export[0].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.export[0].arn
+}
---
+++ ops/services/30-lambda/optout-import.tf
@@ -0,0 +1,223 @@
+resource "aws_iam_role" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "lambda.amazonaws.com"
+          }
+        },
+        {
+          Action = [
+            "sts:TagSession",
+            "sts:AssumeRoleWithWebIdentity",
+          ]
+          Condition = {
+            StringEquals = {
+              "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            }
+            StringLike = {
+              "token.actions.githubusercontent.com:sub" = "repo:CMSgov/ab2d-lambdas:*"
+            }
+          }
+          Effect = "Allow"
+          Principal = {
+            Federated = "arn:aws:iam::${module.platform.aws_caller_identity.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          }
+        },
+        {
+          Action = [
+            "sts:TagSession",
+            "sts:AssumeRole",
+          ]
+          Effect = "Allow"
+          Principal = {
+            AWS = [for role in module.platform.kion_roles : role.arn]
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  force_detach_policies = false
+  max_session_duration  = 3600
+  name                  = "${local.service_prefix}-opt-out-import-function"
+  path                  = "/delegatedadmin/developer/"
+  permissions_boundary  = "arn:aws:iam::${module.platform.aws_caller_identity.account_id}:policy/cms-cloud-admin/developer-boundary-policy"
+
+  inline_policy {
+    name = "assume-bucket-role"
+    policy = jsonencode(
+      {
+        Statement = [
+          {
+            Action   = "sts:AssumeRole"
+            Effect   = "Allow"
+            Resource = module.platform.ssm.eft.bfd-bucket-role-arn.value
+          },
+        ]
+        Version = "2012-10-17"
+      }
+    )
+  }
+  inline_policy {
+    name = "default-function"
+    policy = jsonencode(
+      {
+        Statement = [
+          {
+            Action = [
+              "ssm:GetParameters",
+              "ssm:GetParameter",
+              "sqs:ReceiveMessage",
+              "sqs:GetQueueAttributes",
+              "sqs:DeleteMessage",
+              "logs:PutLogEvents",
+              "logs:CreateLogStream",
+              "logs:CreateLogGroup",
+              "kms:Encrypt",
+              "kms:Decrypt",
+              "ec2:DescribeNetworkInterfaces",
+              "ec2:DescribeAccountAttributes",
+              "ec2:DeleteNetworkInterface",
+              "ec2:CreateNetworkInterface",
+            ]
+            Effect   = "Allow"
+            Resource = "*"
+          },
+        ]
+        Version = "2012-10-17"
+      }
+    )
+  }
+}
+
+resource "aws_lambda_function" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  filename = "${path.root}/optout.zip"
+  architectures = [
+    "x86_64",
+  ]
+  description                    = "Ingests the most recent beneficiary opt-out list from BFD"
+  function_name                  = "${local.service_prefix}-opt-out-import"
+  handler                        = "gov.cms.ab2d.optout.OptOutHandler"
+  kms_key_arn                    = local.env_key_alias.target_key_arn
+  memory_size                    = 1024
+  package_type                   = "Zip"
+  reserved_concurrent_executions = -1
+  role                           = aws_iam_role.import[0].arn
+  runtime                        = "java11"
+  skip_destroy                   = false
+
+  tags = {
+    code = "https://github.com/CMSgov/ab2d-lambdas/tree/main/optout"
+  }
+  timeout = 900
+
+  environment {
+    variables = {
+      APP_NAME = "${local.service_prefix}-opt-out-import"
+      DB_HOST  = "jdbc:postgresql://${local.ab2d_db_host}:${local.db_port}/${local.db_name}?sslmode=${local.ab2d_db_ssl_mode}%ApplicationName=${local.service_prefix}-opt-out-import"
+      ENV      = local.env
+    }
+  }
+
+  ephemeral_storage {
+    size = 512
+  }
+
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/${local.service_prefix}-opt-out-import"
+  }
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+  vpc_config {
+    security_group_ids = [local.db_sg_id, aws_security_group.database_lambda.id]
+    subnet_ids         = local.node_subnet_ids
+  }
+}
+
+resource "aws_sqs_queue" "dead_letter" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  name              = "${local.service_prefix}-opt-out-import-dead-letter"
+  kms_master_key_id = local.env_key_alias.target_key_arn
+}
+
+resource "aws_sqs_queue" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  name              = "${local.service_prefix}-opt-out-import"
+  kms_master_key_id = local.env_key_alias.target_key_arn
+
+  visibility_timeout_seconds = 900
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dead_letter[0].arn
+    maxReceiveCount     = 4
+  })
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  queue_url = aws_sqs_queue.dead_letter[0].id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue"
+    sourceQueueArns   = [aws_sqs_queue.import[0].arn]
+  })
+}
+
+data "aws_iam_policy_document" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  statement {
+    actions = ["sqs:SendMessage"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    resources = [aws_sqs_queue.import[0].arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [module.platform.ssm.eft.bfd-sns-topic-arn.value]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  queue_url = aws_sqs_queue.import[0].id
+  policy    = data.aws_iam_policy_document.import[0].json
+}
+
+resource "aws_sns_topic_subscription" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  endpoint  = aws_sqs_queue.import[0].arn
+  protocol  = "sqs"
+  topic_arn = module.platform.ssm.eft.bfd-sns-topic-arn.value
+}
+
+resource "aws_lambda_event_source_mapping" "import" {
+  count = contains(["prod", "test"], local.env) ? 1 : 0
+
+  event_source_arn = aws_sqs_queue.import[0].arn
+  function_name    = aws_lambda_function.import[0].function_name
+  batch_size       = 1
+  enabled          = true
+}
