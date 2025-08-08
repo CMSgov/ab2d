33deb54af0b4c028751dc7ab47555b1526151741
smirnovaae <annasmirnova@navapbc.com>
2025-08-08 13:42:23 -0700

    fix liquibase error

--- common/src/main/resources/db/changelog/v2025/add_cron_schedule.sql
+++
@@ -1,9 +0,0 @@
-TRUNCATE TABLE IF EXISTS cron.job;
-
-SELECT cron.schedule('0 7 * * *', 'CALL proc_insert_mbi_to_table(0,40);');
-SELECT cron.schedule('20 7 * * *', 'CALL proc_insert_mbi_to_table(41,20);');
-SELECT cron.schedule('0 8 * * *', 'CALL proc_insert_mbi_to_table(81,100);');
-SELECT cron.schedule('40 7 * * *', 'CALL proc_insert_mbi_to_table(61,12);');
-SELECT cron.schedule('10 8 * * *', 'CALL proc_insert_mbi_to_table(74,6);');
-
-SELECT cron.schedule('delete-job-run-details','0 12 * * *',$$DELETE FROM cron.job_run_details WHERE end_time < now() - interval '7 days'$$);
