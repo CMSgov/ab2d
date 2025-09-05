69adae392a0eab5d8405b0415cccb2e1e12bb617
Ben Hesford <benhesford@navapbc.com>
2025-09-05 15:09:22 -0700

    Update

--- common/src/test/java/gov/cms/ab2d/common/util/HealthCheckUtilTest.java
+++ common/src/test/java/gov/cms/ab2d/common/util/HealthCheckUtilTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import javax.sql.DataSource;
 
