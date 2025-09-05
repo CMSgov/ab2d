5a573cf76677a1b12e545c27f0510c6e019b6f1d
Ben Hesford <benhesford@navapbc.com>
2025-09-05 15:22:38 -0700

    Remove unused class

--- common/src/main/java/gov/cms/ab2d/common/properties/PropertiesClient.java
+++
@@ -1,8 +0,0 @@
-package gov.cms.ab2d.common.properties;
-
-import gov.cms.ab2d.common.model.Property;
-
-public interface PropertiesClient {
-    Property getProperty(String key);
-    Property setProperty(String key, String value);
-}
