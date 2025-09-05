4502f6902e080906b9d0dcc17f8bc8a65f0120c0
Ben Hesford <benhesford@navapbc.com>
2025-09-05 10:13:48 -0700

    Update

--- common/src/test/java/gov/cms/ab2d/common/health/LoggingAvailableTest.java
+++ common/src/test/java/gov/cms/ab2d/common/health/LoggingAvailableTest.java
@@ -1,13 +1,28 @@
 package gov.cms.ab2d.common.health;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith({MockitoExtension.class, OutputCaptureExtension.class})
 class LoggingAvailableTest {
+
+    @Mock
+    org.slf4j.Logger mockLogger;
 
     @Test
     void canLog() {
         assertTrue(LoggingAvailable.canLog());
+    }
+
+    @Test
+    void canNotLog() {
+        when(mockLogger.isErrorEnabled()).thenReturn(false);
+        assertFalse(LoggingAvailable.canLog(mockLogger));
     }
 }
