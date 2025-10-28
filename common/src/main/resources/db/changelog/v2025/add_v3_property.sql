c28615b1591bdf612c076ea7c64561a2aa34fd92
smirnovaae <annasmirnova@navapbc.com>
2025-10-28 16:44:10 -0700

    fix

--- common/src/main/resources/db/changelog/v2025/add_v3_property.sql
+++ common/src/main/resources/db/changelog/v2025/add_v3_property.sql
@@ -1,7 +1,4 @@
 INSERT INTO property.properties (id, key, value, created, modified)
-SELECT nextval('hibernate_sequence'), 'v3.on', 'false', current_timestamp, current_timestamp
-    WHERE NOT EXISTS (
-    SELECT 1
-    FROM property.properties
-    WHERE key = 'v3.on'
-);
+SELECT (SELECT COALESCE(MAX(id),0)+1 FROM property.properties),
+       'v3.on', 'false', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM property.properties WHERE key = 'v3.on');
