e81a91782564140f478cf8c3d46252b4e2de68c1
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-04 07:17:07 -0700

    redirect port 80 to port 443

--- ops/services/20-microservices/main.tf
+++ ops/services/20-microservices/main.tf
@@ -143,9 +143,24 @@ resource "aws_ssm_parameter" "internal_lb" {
   type  = "String"
 }
 
-resource "aws_lb_listener" "internal_lb" {
+resource "aws_lb_listener" "http_to_https_redirect" {
   load_balancer_arn = aws_lb.internal_lb.arn
   port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "internal_lb" {
+  load_balancer_arn = aws_lb.internal_lb.arn
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
 
