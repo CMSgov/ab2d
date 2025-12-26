1fb57b97aaf2802abd03b165dc94c6dcc7f60b66
Ben Hesford <benhesford@navapbc.com>
2025-12-26 16:42:42 -0700

    Fix

--- coverage/src/main/java/gov/cms/ab2d/coverage/service/CoverageV3ServiceImpl.java
+++ coverage/src/main/java/gov/cms/ab2d/coverage/service/CoverageV3ServiceImpl.java
@@ -97,14 +97,28 @@ public class CoverageV3ServiceImpl implements CoverageV3Service {
         val sql2 =
         """
             select count(distinct patient_id) from (
-                select * from v3.coverage_v3 
+                select * from v3.coverage_v3
                     where contract = :contract and (year,month) in (:yearMonthRecords)
                 union
-                select * from  v3.coverage_v3_historical 
+                select * from  v3.coverage_v3_historical
                     where contract = :contract and (year, month) in (:yearMonthRecords)
-            ) 
+            )
         """;
 
+        val sql2WithOptOut =
+        """
+           select count(distinct patient_id) from 
+           (
+               select * from v3.coverage_v3
+                   where contract = :contract and (year,month) in (:yearMonthRecords)
+               union
+               select * from  v3.coverage_v3_historical
+                   where contract = :contract and (year, month) in (:yearMonthRecords)
+           ) as union_results 
+           join current_mbi on union_results.current_mbi = current_mbi.mbi
+           where current_mbi is not null
+           and share_data is not false
+        """;
 
         List<Object[]> yearMonthRecordsObjects = new ArrayList<>();
         for (YearMonthRecord yearMonthRecord : yearMonthRecords) {
@@ -116,11 +130,19 @@ public class CoverageV3ServiceImpl implements CoverageV3Service {
                 .addValue("yearMonthRecords", yearMonthRecordsObjects);
 
 
-        NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+        final NamedParameterJdbcTemplate template1 = new NamedParameterJdbcTemplate(dataSource);
 
-        return template.queryForList(sql2, parameters, Integer.class)
+        int count1 = template1.queryForList(sql2, parameters, Integer.class)
                 .stream().findFirst().orElseThrow(() -> new RuntimeException("no coverage information found for any " +
                         "of the coverage periods provided"));
+        log.info("Count #1: {}", count1);
+
+        final NamedParameterJdbcTemplate template2 = new NamedParameterJdbcTemplate(dataSource);
+        int count2 = template2.queryForList(sql2, parameters, Integer.class)
+                .stream().findFirst().orElseThrow(() -> new RuntimeException("no coverage information found for any " +
+                        "of the coverage periods provided"));
+        log.info("Count #2: {}", count2);
+        return count1;
 
         /*
         val sql1 = buildQueryWithPlaceholders(COVERAGE_V3_TABLE, yearMonthRecords.size());
