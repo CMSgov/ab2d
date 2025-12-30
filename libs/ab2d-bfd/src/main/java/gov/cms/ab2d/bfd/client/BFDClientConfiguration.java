003c51c3828451dfa923646b683d7e01d4c13bf9
juliareynolds-nava <juliareynolds@navapbc.com>
2025-12-30 13:32:13 -0700

    Get cert data from ecs container secrets.

--- libs/ab2d-bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
+++ libs/ab2d-bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDClientConfiguration.java
@@ -14,11 +14,24 @@ import org.springframework.context.annotation.PropertySource;
 import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.security.*;
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.cert.CertificateException;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
+
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 /**
  * Credits: most of the code in this class has been copied over from https://github.com/CMSgov/dpc-app
@@ -28,11 +41,16 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class BFDClientConfiguration {
 
-    @Value("${bfd.keystore.location}")
-    private String keystorePath;
+    @Value("${bfd.keystore.private_key}")
+    private String ssm_private_key;
+
+    @Value("${bfd.keystore.certificate}")
+    private String ssm_certificate;
 
     @Value("${bfd.keystore.password}")
     private String keystorePassword;
+
+    private String keystoreAlias = "AB2D_BFD_KEYSTORE";
 
     @Value("${bfd.connectionTimeout}")
     private int connectionTimeout;
@@ -53,28 +71,116 @@ public class BFDClientConfiguration {
     private int connectionTTL;
 
     /**
-     * Get http client alloweed to connect to BFD domain only. The domain is limited by Mutual TLS cert verification.
-     * @return the HTTP client
+     * Creates a KeyStore from private key and certificate stored in the ecs container secrets.
+     *
+     * @param privateKeyParam SSM parameter name containing the private key
+     * @param certificateParam SSM parameter name containing the certificate
+     * @param keystorePassword Password to protect the keystore
+     * @param keyAlias Alias for the key entry in the keystore
+     * @return Configured KeyStore object
+     * @throws Exception if keystore creation fails
      */
+    public static KeyStore createKeyStore(
+            String privateKeyParam,
+            String certificateParam,
+            String keystorePassword,
+            String keyAlias) throws Exception {
+
+        try (SsmClient ssmClient = getSSMClient()) {
+
+            // Retrieve private key from container environment
+            String privateKeyData = System.getenv(privateKeyParam);
+            PrivateKey privateKey = parsePrivateKey(privateKeyData);
+
+            // Retrieve certificate from container environment
+            String certificateData = System.getenv(certificateParam);
+            Certificate certificate = parseCertificate(certificateData);
+
+            // Create KeyStore
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            keyStore.load(null, null); // Initialize empty keystore
+
+            // Add private key and certificate chain to keystore
+            Certificate[] certChain = new Certificate[]{certificate};
+            keyStore.setKeyEntry(
+                    keyAlias,
+                    privateKey,
+                    keystorePassword.toCharArray(),
+                    certChain
+            );
+
+            return keyStore;
+        }
+    }
+
+    /**
+     * Parses a private key from PEM or PKCS12 format.
+     * Supports RSA, EC, and other key types.
+     */
+    private static PrivateKey parsePrivateKey(String keyData) throws Exception {
+        // Remove PEM headers and whitespace
+        String cleanedData = keyData
+                .replaceAll("-----BEGIN PRIVATE KEY-----", "")
+                .replaceAll("-----END PRIVATE KEY-----", "")
+                .replaceAll("-----BEGIN RSA PRIVATE KEY-----", "")
+                .replaceAll("-----END RSA PRIVATE KEY-----", "")
+                .replaceAll("-----BEGIN EC PRIVATE KEY-----", "")
+                .replaceAll("-----END EC PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] keyBytes = Base64.getDecoder().decode(cleanedData);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+
+        // Try different key algorithms
+        String[] algorithms = {"RSA", "EC", "DSA"};
+        Exception lastException = null;
+
+        for (String algorithm : algorithms) {
+            try {
+                KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+                return keyFactory.generatePrivate(keySpec);
+            } catch (Exception e) {
+                lastException = e;
+            }
+        }
+
+        throw new Exception("Unable to parse private key with any supported algorithm", lastException);
+    }
+
+    /**
+     * Parses a certificate from PEM format.
+     */
+    private static Certificate parseCertificate(String certificateData) throws Exception {
+        String cleanedData = certificateData
+                .replaceAll("-----BEGIN CERTIFICATE-----", "")
+                .replaceAll("-----END CERTIFICATE-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] certBytes = Base64.getDecoder().decode(cleanedData);
+
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(certBytes);
+
+        return certFactory.generateCertificate(inputStream);
+    }
+
     @Bean
     public HttpClient bfdHttpClient() {
-
         try {
-            File keyStoreFile = new File(keystorePath);
-            if (!keyStoreFile.exists()) {
-                URL resource = BFDClientConfiguration.class.getResource(keystorePath);
-                if (resource != null) {
-                    keyStoreFile = new File(resource.toURI());
-                }
+            KeyStore keyStore = createKeyStore(
+                    this.ssm_private_key,
+                    this.ssm_certificate,
+                    this.keystorePassword,
+                    this.keystoreAlias
+            );
+
+            if (keyStore) {
+                throw new BeanInstantiationException(HttpClient.class, "Keystore does not exist");
             }
 
-            if (!keyStoreFile.exists()) {
-                throw new BeanInstantiationException(HttpClient.class, "Keystore file does not exist");
-            }
-
-            return buildMutualTlsClient(keyStoreFile, keystorePassword.toCharArray());
+            return buildMutualTlsClient(keyStore, keystorePassword.toCharArray());
         } catch (URISyntaxException fnf) {
-            throw new BeanInstantiationException(HttpClient.class, "Keystore file does not exist");
+            throw new BeanInstantiationException(HttpClient.class, "Keystore does not exist");
         }
     }
 
@@ -86,16 +192,15 @@ public class BFDClientConfiguration {
      * @param keyStorePass password for keystore (default: "changeit")
      * @return {@link HttpClient} compatible with HAPI FHIR TLS client
      */
-    private HttpClient buildMutualTlsClient(File keystoreFile, char[] keyStorePass) {
+    private HttpClient buildMutualTlsClient(KeyStore keyStore, char[] keyStorePass) {
         final SSLContext sslContext;
 
         try {
             // BlueButton FHIR servers have a self-signed cert and require a client cert
-            sslContext = SSLContexts.custom()
-                    .loadKeyMaterial(keystoreFile, keyStorePass, keyStorePass)
-                    .loadTrustMaterial(keystoreFile, keyStorePass)
-                    .build();
-
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(keyStore, keyStorePass);
+            sslContext.init(kmf.getKeyManagers(), null, null);
         } catch (IOException | CertificateException | KeyManagementException | NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException ex) {
             log.error(ex.getMessage());
             throw new BeanInstantiationException(KeyStore.class, ex.getMessage());
