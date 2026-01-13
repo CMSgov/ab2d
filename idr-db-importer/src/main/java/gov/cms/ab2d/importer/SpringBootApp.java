34e44c70d7f38cd848b6a68562118a16767f2266
smirnovaae <annasmirnova@navapbc.com>
2026-01-13 09:39:10 -0800

    wip

--- idr-db-importer/src/main/java/gov/cms/ab2d/importer/SpringBootApp.java
+++ idr-db-importer/src/main/java/gov/cms/ab2d/importer/SpringBootApp.java
@@ -1,8 +1,9 @@
 package gov.cms.ab2d.importer;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
@@ -11,27 +12,29 @@ import org.springframework.retry.annotation.EnableRetry;
 @Slf4j
 @RequiredArgsConstructor
 @EnableRetry
-public class SpringBootApp {
+public class SpringBootApp implements ApplicationRunner {
 
-	private final CoverageV3S3Importer importer;
+    private final CoverageV3S3Importer importer;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
 
-	@PostConstruct
-	public void runAfterStartup() {
-		log.info("Starting coverage_v3 S3 -> Aurora import task");
-		int exitCode = 1;
-		try {
-			importer.runOnce();
-			exitCode = 0;
-			log.info("Import task completed successfully");
-		} catch (Exception e) {
-			log.error("Import task failed", e);
-		} finally {
-			System.exit(exitCode);
-		}
-	}
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("IDR S3 import ECS task started");
+
+        int exitCode = 1;
+        try {
+            importer.runOnce();
+            exitCode = 0;
+            log.info("ECS task completed successfully");
+        } catch (Exception e) {
+            log.error("IDR S3 import ECS task failed", e);
+        } finally {
+            System.exit(exitCode);
+        }
+    }
 }
 
