5aa971ab43aade282bf3dc5a3a7c045e52b1db2e
smirnovaae <annasmirnova@navapbc.com>
2026-02-09 12:39:54 -0800

    fix test

--- libs/ab2d-bfd/src/test/java/gov/cms/ab2d/bfd/client/BFDClientConfigurationTest.java
+++ libs/ab2d-bfd/src/test/java/gov/cms/ab2d/bfd/client/BFDClientConfigurationTest.java
@@ -3,6 +3,7 @@ package gov.cms.ab2d.bfd.client;
 import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -108,17 +109,16 @@ class BFDClientConfigurationTest {
                 .getMessage().contains("unable to find valid certification path to requested target"));
     }
 
-//    @DisplayName("Certs with public authority are not trusted unless they are explicitly listed in the truststore")
-//    @Test
-//    void doNotTrustPublicCerts() throws IOException {
-//        HttpGet httpget = new HttpGet("https://www.verisign.com/");
-//        try {
-//            var resp = httpClient.execute(httpget);
-//            assertTrue(resp.getStatusLine().getStatusCode() < 500);
-//        } finally {
-//            httpget.releaseConnection();
-//        }
-//    }
+    @DisplayName("Certs with public authority are not trusted unless they are explicitly listed in the truststore")
+    @Test
+    void doNotTrustPublicCerts() {
+        HttpGet httpget = new HttpGet("https://www.verisign.com/");
+        try {
+            assertThrows(javax.net.ssl.SSLHandshakeException.class, () -> httpClient.execute(httpget));
+        } finally {
+            httpget.releaseConnection();
+        }
+    }
 
 
     @DisplayName("Missing keystore file throws bean instantiation exception")
