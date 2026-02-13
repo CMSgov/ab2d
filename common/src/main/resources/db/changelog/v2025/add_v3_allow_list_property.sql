2bf57850da1b7d1afcc35f0c45c7b3cf8a6b1f32
Ben Hesford <benhesford@navapbc.com>
2026-02-13 12:59:11 -0700

    Update V3 whitelisted contracts

--- common/src/main/resources/db/changelog/v2025/add_v3_allow_list_property.sql
+++ common/src/main/resources/db/changelog/v2025/add_v3_allow_list_property.sql
@@ -2,3 +2,7 @@ INSERT INTO property.properties (id, key, value, created, modified)
 SELECT (SELECT COALESCE(MAX(id),0)+1 FROM property.properties),
        'v3.whitelisted.contracts', '', now(), now()
     WHERE NOT EXISTS (SELECT 1 FROM property.properties WHERE key = 'v3.whitelisted.contracts');
+
+UPDATE property.properties
+SET value='Z0001,Z0002,Z0003,Z0004,Z0005,Z0006,Z0007,Z0008,Z0009,Z0010'
+WHERE key='v3.whitelisted.contracts';
