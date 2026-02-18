dbc08029fd44464b437192a68a5d07959e8690d0
Ben Hesford <benhesford@navapbc.com>
2026-02-18 14:54:38 -0700

    Update test

--- api/src/test/java/gov/cms/ab2d/api/controller/v1/FileDownloadAPIV1Test.java
+++ api/src/test/java/gov/cms/ab2d/api/controller/v1/FileDownloadAPIV1Test.java
@@ -1,19 +1,24 @@
 package gov.cms.ab2d.api.controller.v1;
 
-import com.okta.jwt.AccessTokenVerifier;
 import gov.cms.ab2d.api.controller.ErrorHandler;
-import gov.cms.ab2d.api.controller.common.ApiCommon;
 import gov.cms.ab2d.api.controller.common.FileDownloadCommon;
-import gov.cms.ab2d.api.security.JwtConfig;
-import gov.cms.ab2d.common.model.PdpClient;
-import gov.cms.ab2d.common.service.PdpClientService;
-import gov.cms.ab2d.eventclient.clients.SQSEventClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @WebMvcTest(FileDownloadAPIV1.class)
 @ContextConfiguration(classes = {FileDownloadAPIV1.class})
@@ -22,29 +27,19 @@ public class FileDownloadAPIV1Test {
 	@Autowired
 	private MockMvc mockMvc;
 
-//	@MockitoBean
-//	SQSEventClient sqsEventClient;
-//
-//	@MockitoBean
-//	ApiCommon apiCommon;
-//
 	@MockitoBean
 	FileDownloadCommon fileDownloadCommon;
 
-//	@MockitoBean
-//	AccessTokenVerifier accessTokenVerifier;
-//
-//	@MockitoBean
-//	PdpClientService pdpClientService;
-//
-//	@MockitoBean
-//	JwtConfig jwtConfig;
-//
 	@MockitoBean
 	ErrorHandler errorHandler;
 
 	@Test
-	void blah() {
+	@WithMockUser
+	void blah() throws Exception {
+		when(fileDownloadCommon.downloadFile(eq("1234"), eq("test.json"), any(), any())).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+		mockMvc.perform(get("/api/v1/fhir/Job/1234/file/test.json"))
+				.andExpect(status().isOk());
 
 	}
 
