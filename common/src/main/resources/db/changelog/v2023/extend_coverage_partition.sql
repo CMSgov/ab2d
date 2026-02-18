3ed2b423b386d05636498d67fa619d9872032d68
keeyanghoreshi-rgb <keeyanghoreshi@navapbc.com>
2026-02-18 11:40:54 -0500

    build fixes and speedup
    # Conflicts:
    #	worker/src/test/resources/application.properties
    
    # Conflicts:
    #	common/src/main/resources/db/changelog/test_only/coverage-fake-partitions.sql

--- common/src/main/resources/db/changelog/v2023/extend_coverage_partition.sql
+++ common/src/main/resources/db/changelog/v2023/extend_coverage_partition.sql
@@ -57,3 +57,39 @@ create table  IF NOT EXISTS coverage_misc_2024 partition of coverage_misc
     for values in (2024);
 create table  IF NOT EXISTS coverage_misc_2025 partition of coverage_misc
     for values in (2025);
+
+create table IF NOT EXISTS sandbox_2026 partition of sandbox
+    for values in (2026);
+
+create table IF NOT EXISTS coverage_anthem_united_2026 partition of coverage_anthem_united
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_bcbs_2026 partition of coverage_bcbs
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_centene_2026 partition of coverage_centene
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_cigna1_2026 partition of coverage_cigna1
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_cigna2_2026 partition of coverage_cigna2
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_cvs_2026 partition of coverage_cvs
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_humana_2026 partition of coverage_humana
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_united1_2026 partition of coverage_united1
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_united_2026 partition of coverage_united2
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_mutual_dean_clear_cambia_rite_2026 partition of coverage_mutual_dean_clear_cambia_rite
+    for values in (2026);
+
+create table  IF NOT EXISTS coverage_misc_2026 partition of coverage_misc
+    for values in (2026);
