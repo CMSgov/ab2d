e0551e2fd6c5edb0a27d45d8f63efe3db6d8385f
Ben Hesford <benhesford@navapbc.com>
2026-02-19 11:03:38 -0700

    Remove commented out BFD URLs

--- ops/services/30-worker/main.tf
+++ ops/services/30-worker/main.tf
@@ -46,23 +46,6 @@ locals {
     "sandbox" = "ab2d-sbx-sandbox"
   }, local.parent_env, local.env)
 
-/*
-  bfd_url = lookup({
-    dev     = "https://test.fhir.bfd.cmscloud.local"
-    test    = "https://test.fhir.bfd.cmscloud.local"
-    sandbox = "https://sandbox.fhir.bfd.cmscloud.local"
-    prod    = "https://prod.fhir.bfd.cmscloud.local"
-  }, local.parent_env)
-
-  bfd_url_v3 = lookup({
-    dev     = "https://test.fhirv3.bfd.cmscloud.local"
-    test    = "https://test.fhirv3.bfd.cmscloud.local"
-    sandbox = "https://sandbox.fhirv3.bfd.cmscloud.local"
-    prod    = "https://prod.fhirv3.bfd.cmscloud.local"
-  }, local.parent_env)
-*/
-
-
   bfd_url = lookup({
     prod = "https://prod.fhir.bfd.cmscloud.local"
   }, local.parent_env, "https://prod-sbx.fhir.bfd.cmscloud.local")
