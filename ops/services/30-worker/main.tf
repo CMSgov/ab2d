872cf9a1aad14c2c821cf536a18ce6dcc3b1c2aa
Ben Hesford <benhesford@navapbc.com>
2026-02-18 17:52:55 -0700

    Use prod-sbx

--- ops/services/30-worker/main.tf
+++ ops/services/30-worker/main.tf
@@ -46,7 +46,7 @@ locals {
     "sandbox" = "ab2d-sbx-sandbox"
   }, local.parent_env, local.env)
 
-
+/*
   bfd_url = lookup({
     dev     = "https://test.fhir.bfd.cmscloud.local"
     test    = "https://test.fhir.bfd.cmscloud.local"
@@ -60,9 +60,9 @@ locals {
     sandbox = "https://sandbox.fhirv3.bfd.cmscloud.local"
     prod    = "https://prod.fhirv3.bfd.cmscloud.local"
   }, local.parent_env)
+*/
 
 
-/*
   bfd_url = lookup({
     prod = "https://prod.fhir.bfd.cmscloud.local"
   }, local.parent_env, "https://prod-sbx.fhir.bfd.cmscloud.local")
@@ -70,7 +70,7 @@ locals {
   bfd_url_v3 = lookup({
     prod = "https://prod.fhirv3.bfd.cmscloud.local"
   }, local.parent_env, "https://sandbox.fhirv3.bfd.cmscloud.local")
-*/
+
 
   ab2d_efs_mount                = "/mnt/efs"
   aws_region                    = module.platform.primary_region.name
