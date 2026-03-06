45fe8e6eced2f125d86a3e45c8d6d4772de9f67c
smirnovaae <annasmirnova@navapbc.com>
2026-03-06 15:38:32 -0800

    removed count preventing anything being created in prod

--- ops/services/10-core/main.tf
+++ ops/services/10-core/main.tf
@@ -325,7 +325,6 @@ resource "aws_security_group" "attribution" {
 }
 
 resource "aws_security_group" "idr_endpoint" {
- // count = module.platform.parent_env == "prod" ? 1 : 0
 
   name        = "${local.service_prefix}-idr-endpoint"
   description = "For the PrivateLink endpoint for IDR Snowflake"
@@ -334,9 +333,8 @@ resource "aws_security_group" "idr_endpoint" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "idr_endpoint_http" {
-//  count = module.platform.parent_env == "prod" ? 1 : 0
 
-  security_group_id = aws_security_group.idr_endpoint[0].id
+  security_group_id = aws_security_group.idr_endpoint.id
 
   referenced_security_group_id = aws_security_group.attribution.id
   from_port                    = 80
@@ -345,9 +343,8 @@ resource "aws_vpc_security_group_ingress_rule" "idr_endpoint_http" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "idr_endpoint_https" {
-//  count = module.platform.parent_env == "prod" ? 1 : 0
 
-  security_group_id = aws_security_group.idr_endpoint[0].id
+  security_group_id = aws_security_group.idr_endpoint.id
 
   referenced_security_group_id = aws_security_group.attribution.id
   from_port                    = 443
