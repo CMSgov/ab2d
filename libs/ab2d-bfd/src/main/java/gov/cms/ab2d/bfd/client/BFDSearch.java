4ffa39e4c82738b3736b095ec1e31a5ef13093a5
Colby Seyferth <colbyseyferth@navapbc.com>
2026-03-10 23:33:28 -0700

    remove unused imports

--- libs/ab2d-bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDSearch.java
+++ libs/ab2d-bfd/src/main/java/gov/cms/ab2d/bfd/client/BFDSearch.java
@@ -1,11 +1,7 @@
 package gov.cms.ab2d.bfd.client;
 
-import gov.cms.ab2d.fhir.FhirVersion;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
-
 import java.io.IOException;
-import java.time.OffsetDateTime;
-import java.util.List;
 
 public interface BFDSearch {
     IBaseBundle searchEOB(BFDSearchDTO searchDTO) throws IOException;
