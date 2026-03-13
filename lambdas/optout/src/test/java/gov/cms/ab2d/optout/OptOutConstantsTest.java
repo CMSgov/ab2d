8788fbdbdc7300e401d35581a0e948896e48c163
Michael J Burling <michael.burling@cms.hhs.gov>
2026-03-13 10:46:15 -0500

    Update other hard-coded bucket references 🙁

--- lambdas/optout/src/test/java/gov/cms/ab2d/optout/OptOutConstantsTest.java
+++ lambdas/optout/src/test/java/gov/cms/ab2d/optout/OptOutConstantsTest.java
@@ -5,7 +5,7 @@ public class OptOutConstantsTest {
     public static final String TEST_FILE_NAME = "optOutDummy.txt";
     public static final String MULTIPLE_MBIS_TEST_FILE_NAME = "multipleMBIsOptOutDummy.txt";
     public static final String TEST_BUCKET_PATH = "bfdeft01/ab2d";
-    public static final String TEST_BFD_BUCKET_NAME = "bfd-test-eft";
+    public static final String TEST_BFD_BUCKET_NAME = "bfd-test-bene-prefs-ab2d20260225231712701800000001";
 
     private OptOutConstantsTest() {
     }
