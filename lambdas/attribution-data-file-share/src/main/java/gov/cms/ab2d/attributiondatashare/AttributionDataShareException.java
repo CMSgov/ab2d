918521702f2cadd78d61be2e54df73004602bbc8
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2026-03-26 10:43:33 -0700

    AB2D-7189 Remove alignment and confirmation files sent to NGD (#1742)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/AB2D-7189
    
    ## 🛠 Changes
    
    - Deleted code for opt-out-export Lambda
    - Deleted workflows and terraform 
    
    ## ℹ️ Context
    
    Due to the new connection for beneficiary data-sharing preferences with
    IDR, we are no longer sending bene preference sharing change
    confirmation files to NGD.
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->

--- lambdas/attribution-data-file-share/src/main/java/gov/cms/ab2d/attributiondatashare/AttributionDataShareException.java
+++
@@ -1,7 +0,0 @@
-package gov.cms.ab2d.attributiondatashare;
-
-public class AttributionDataShareException extends RuntimeException {
-    public AttributionDataShareException(String message, Exception ex) {
-        super(message, ex);
-    }
-}
