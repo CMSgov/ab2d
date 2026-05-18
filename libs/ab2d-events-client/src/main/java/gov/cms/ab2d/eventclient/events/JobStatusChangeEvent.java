ca9701f0cd083f1e912a079b4bdfb7ed83847d86
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2026-05-18 11:29:10 -0700

    AB2D-7241 Modify job-related Slack alerts (#1780)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/AB2D-7241
    
    ## 🛠 Changes
    
    Added ab2d version in slack alerts
    Follow up for  https://github.com/CMSgov/ab2d/pull/1777
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->

--- libs/ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/events/JobStatusChangeEvent.java
+++ libs/ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/events/JobStatusChangeEvent.java
@@ -20,6 +20,7 @@ public class JobStatusChangeEvent extends LoggableEvent {
     private OffsetDateTime until;
     private List<String> serviceDates;
     private String resourceTypes;
+    private String version;
 
     public JobStatusChangeEvent() { }
 
@@ -32,13 +33,14 @@ public class JobStatusChangeEvent extends LoggableEvent {
 
     public JobStatusChangeEvent(String organization, String jobId, String oldStatus, String newStatus, String description,
                                 String contractNumber, OffsetDateTime since, OffsetDateTime until,
-                                List<String> serviceDates, String resourceTypes) {
+                                List<String> serviceDates, String resourceTypes, String version) {
         this(organization, jobId, oldStatus, newStatus, description);
         this.contractNumber = contractNumber;
         this.since = since;
         this.until = until;
         this.serviceDates = serviceDates;
         this.resourceTypes = resourceTypes;
+        this.version = version;
     }
 
     @Override
@@ -70,6 +72,9 @@ public class JobStatusChangeEvent extends LoggableEvent {
             sb.append("\n");
         }
 
+        if (version != null && !version.isBlank()) {
+            sb.append("Version: ").append(version).append("\n");
+        }
         sb.append("Job ID: ").append(getJobId()).append("\n");
 
         DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
