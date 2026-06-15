87f6a269cf6fbe845fc614f8f3dc1f06b5f2a5a9
keeyanghoreshi-rgb <keeyanghoreshi@navapbc.com>
2026-06-15 17:34:35 -0400

    update import

--- libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
+++ libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
@@ -3,9 +3,9 @@ package gov.cms.ab2d.snsclient.config;
 import gov.cms.ab2d.eventclient.config.Ab2dEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
