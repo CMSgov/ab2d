3691635d4ce6ab0e9232c5a645d46d7252484ac5
keeyanghoreshi-rgb <keeyanghoreshi@navapbc.com>
2026-06-15 20:52:28 -0400

    update excludes

--- libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
+++ libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
@@ -3,18 +3,9 @@ package gov.cms.ab2d.snsclient.config;
 import gov.cms.ab2d.eventclient.config.Ab2dEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(
-        scanBasePackages = {"gov.cms.ab2d.snsclient"},
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                DataSourceTransactionManagerAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class}
-)
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.snsclient"})
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
