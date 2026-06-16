90fcc0868af210d4e05c9787ff6e999ef18c1116
keeyanghoreshi-rgb <keeyanghoreshi@navapbc.com>
2026-06-15 23:21:16 -0400

    revert excludes

--- libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
+++ libs/ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SpringBootApp.java
@@ -5,7 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.snsclient"})
+@SpringBootApplication(
+        scanBasePackages = {"gov.cms.ab2d.snsclient"},
+        excludeName = {
+                "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration",
+                "org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration",
+                "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
+        }
+)
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
