3960a6b9a2c57903281bd8314d7f80ed6ef7e6cb
smirnovaae <annasmirnova@navapbc.com>
2026-06-17 10:55:24 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/gfreeman/AB2D-7301' into AB2D-7302/datadog-integration""
    This reverts commit e7ac5733490f6abd1615b8b92c68db0dc0692f3e.

--- ops/services/30-api/iam.tf
+++ ops/services/30-api/iam.tf
@@ -1,3 +1,28 @@
+# -------------------------------------------------------
+# Additional Task Policies —
+# -------------------------------------------------------
 data "aws_iam_role" "api" {
   name = "${local.service_prefix}-${local.service}"
+}
+
+resource "aws_iam_policy" "api" {
+  name        = "ab2d-${module.platform.env}-api-additional-policy"
+  description = "Additional IAM permissions for the AB2D API module beyond the base service and platform modules"
+  policy      = data.aws_iam_policy_document.api.json
+}
+
+data "aws_iam_policy_document" "api" {
+  statement {
+    sid    = "CloudWatchMetricsAccess"
+    effect = "Allow"
+    actions = [
+      "cloudwatch:PutMetricData"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "cloudwatch:namespace"
+      values   = ["ab2d/${module.platform.env}/api"]
+    }
+  }
 }
